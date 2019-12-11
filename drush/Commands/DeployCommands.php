de61de30cd62c2c155dbb0737ab2b7051f6baef1
Rob Bayliss <rob@lastcallmedia.com>
2019-12-11 18:46:49 -0500

    Move to a class without a validation handler

--- drush/Commands/DeployCommands.php
+++ drush/Commands/DeployCommands.php
@@ -391,24 +391,4 @@ EOT;
     }
   }
 
-  /**
-   * Invalidate by cache tags.
-   *
-   * Note: This is a temporary backport of a Drush 10 feature.
-   * Remove when we go to Drush 10.
-   *
-   * @command cache:tags
-   * @param string $tags A comma delimited list of cache tags to clear.
-   * @aliases ct
-   * @bootstrap full
-   * @usage drush cache:tag node:12,user:4
-   *   Purge content associated with two cache tags.
-   */
-  public function tags($tags)
-  {
-    $tags = StringUtils::csvToArray($tags);
-    Cache::invalidateTags($tags);
-    $this->logger()->success(dt("Invalidated tag(s): !list.", ['!list' => implode(' ', $tags)]));
-  }
-
 }
