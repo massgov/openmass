c460892b2364e5e69222ddd4d52e03093e48f95d
Moshe Weitzman <weitzman@tejasa.com>
2020-01-14 13:12:14 -0500

    Add debugging.

--- .circleci/circle_trigger.sh
+++ .circleci/circle_trigger.sh
@@ -12,10 +12,11 @@ else
 fi
 
 DATA="{ \"branch\": \"$CIRCLE_BRANCH\", \"parameters\": { $PARAMETERS } }"
+URL="${CIRCLE_API}/v2/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline"
 echo "Triggering pipeline with data:"
+echo -e "  $URL"
 echo -e "  $DATA"
 
-URL="${CIRCLE_API}/v2/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline"
 HTTP_RESPONSE=$(curl -s -u ${CIRCLE_TOKEN}: -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" $URL)
 
 if [ "$HTTP_RESPONSE" -ge "200" ] && [ "$HTTP_RESPONSE" -lt "300" ]; then
