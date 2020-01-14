a3321ac9ddf6b83395355cad8e5165e46d000b1d
Moshe Weitzman <weitzman@tejasa.com>
2020-01-14 15:07:35 -0500

    Refactor trigger.sh v2

--- .circleci/circle_trigger.sh
+++ .circleci/circle_trigger.sh
@@ -7,28 +7,27 @@ PARAMETERS='"post-trigger": true'
 PARAMETERS+=', "webhook": false'
 
 if [ -z "$CIRCLE_PR_NUMBER" ]; then
-  echo -e "Skip trigger since this is a fork"
-  exit 0;
-fi
+  # This is an internal PR. Trigger additional testing.
+  DATA="{ \"branch\": \"$CIRCLE_BRANCH\", \"parameters\": { $PARAMETERS } }"
+  URL="${CIRCLE_API}/v2/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline"
+  echo "Triggering pipeline with data:"
+  echo -e "URL:  $URL"
+  echo -e "DATA:  $DATA"
 
-# This is an internal PR. Triggering additional testing.
-DATA="{ \"branch\": \"$CIRCLE_BRANCH\", \"parameters\": { $PARAMETERS } }"
-URL="${CIRCLE_API}/v2/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline"
-echo "Triggering pipeline with data:"
-echo -e "URL:  $URL"
-echo -e "DATA:  $DATA"
+  HTTP_RESPONSE=$(curl -s -u ${CIRCLE_PERSONAL_TOKEN}: -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" $URL)
 
-HTTP_RESPONSE=$(curl -s -u ${CIRCLE_PERSONAL_TOKEN}: -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" $URL)
-
-if [ "$HTTP_RESPONSE" -ge "200" ] && [ "$HTTP_RESPONSE" -lt "300" ]; then
-    echo "API call succeeded."
-    echo "Response:"
-    cat response.txt
+  if [ "$HTTP_RESPONSE" -ge "200" ] && [ "$HTTP_RESPONSE" -lt "300" ]; then
+      echo "API call succeeded."
+      echo "Response:"
+      cat response.txt
+  else
+      echo -e "\e[93mReceived status code: ${HTTP_RESPONSE}\e[0m"
+      echo "Response:"
+      cat response.txt
+      exit 1
+  fi
 else
-    echo -e "\e[93mReceived status code: ${HTTP_RESPONSE}\e[0m"
-    echo "Response:"
-    cat response.txt
-    exit 1
-fi
+  echo -e "Skip trigger since this is a fork."
+  exit 0;
 
 
