465b2a4b9fbd2d6fb6a78ca84855bbee8ff08a8c
Rob Bayliss <rob@lastcallmedia.com>
2020-02-03 14:09:34 -0500

    Also update ma:deploy with new fetch process

--- drush/Commands/DeployCommands.php
+++ drush/Commands/DeployCommands.php
@@ -128,17 +128,12 @@ class DeployCommands extends DrushCommands implements SiteAliasManagerAwareInter
       // This section resembles ma-refresh-local --db-prep-only. We don't call that
       // since we can't easily make Cloud API calls from Acquia servers, and we
       // don't need to sanitize here.
-      $process = Drush::drush($self, 'ma:latest-backup-url', ['prod']);
-      $process->mustRun();
-      $url = $process->getOutput();
-      $this->logger()->success('Backup URL retrieved.');
 
       // Download the latest backup.
       // Directory set by https://jira.mass.gov/browse/DP-12823.
       $tmp =  Path::join('/mnt/tmp', $_SERVER['REQUEST_TIME'] . '-db-backup.sql.gz');
-      $bash = ['wget', '-q', '--continue', trim($url), "--output-document=$tmp"];
-      $process = Drush::siteProcess($targetRecord, $bash);
-      $process->mustRun($process->showRealtime());
+      $process = Drush::drush($self, 'ma:latest-backup-download', ['prod', $tmp]);
+      $process->mustRun();
       $this->logger()->success('Database downloaded from backup.');
 
       // Drop all tables.
