7db38a2c4eef2ea9f5269b013f30681e88445e37
Rob Bayliss <rob@lastcallmedia.com>
2020-02-18 16:43:28 -0500

    Fix query for aliases

--- .circleci/nightcrawler/fetch_urls.js
+++ .circleci/nightcrawler/fetch_urls.js
@@ -82,7 +82,7 @@ function runQuery(alias, query) {
  * @return Promise<Array<String>>
  */
 function fetchNodeUrls(alias, type, sampleSize) {
-  var query = "SELECT alias, CONCAT('/node/', nid) AS entity FROM node_field_data n INNER JOIN url_alias u ON CONCAT('/node/', n.nid) = u.source WHERE status = 1 AND type = '" + type + "' LIMIT " + sampleSize;
+  var query = "SELECT alias, CONCAT('/node/', nid) AS entity FROM node_field_data n INNER JOIN path_alias u ON CONCAT('/node/', n.nid) = u.path WHERE n.status = 1 AND type = '" + type + "' LIMIT " + sampleSize;
 
   return runQuery(alias, query).then(function(rows) {
     return rows.map(function(row) {
