ddc145d67e3bbffce4a309c4b9aee8a9564798bf
Tom Fleming <tom@lastcallmedia.com>
2020-05-14 13:33:29 -0400

    Added GovernmentBenefitsInfo and QuarantineGuidelines based on recommended properties from Google - DP-18678

---
+++ docroot/modules/custom/mass_schema_metatag/mass_schema_special_announcement/src/Plugin/metatag/Tag/SchemaSpecialAnnouncementQuarantineGuidelines.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\mass_schema_special_announcement\Plugin\metatag\Tag;
+
+use Drupal\schema_metatag\Plugin\metatag\Tag\SchemaUrlWebContentBase;
+
+/**
+ * Provides a plugin for the 'schema_special_announcement_quarantine_guidelines' meta tag.
+ *
+ * - 'id' should be a globally unique id.
+ * - 'name' should match the Schema.org element name.
+ * - 'group' should match the id of the group that defines the Schema.org type.
+ *
+ * @MetatagTag(
+ *   id = "schema_special_announcement_quarantine_guidelines",
+ *   label = @Translation("quarantineGuidelines"),
+ *   description = @Translation("Url or webcontent for quarantineGuidelines."),
+ *   name = "quarantineGuidelines",
+ *   group = "schema_special_announcement",
+ *   weight = 11,
+ *   type = "string",
+ *   secure = FALSE,
+ *   multiple = FALSE
+ * )
+ */
+class SchemaSpecialAnnouncementQuarantineGuidelines extends SchemaUrlWebContentBase {
+
+}
