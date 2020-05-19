791c89fe8ef16f586b3267fba3cbe379c35c6a82
Tom Fleming <tom@lastcallmedia.com>
2020-05-19 10:46:29 -0400

    DP-18678 Add special announcement metadata (#314)
    * Added patch from d.o issue 3120412: Add Schema support for special announcements - DP-18647
    
    * Added changelog for DP-18647
    
    * Renamed changelog from spike issue to story issue - DP-18678
    
    * Updated composer to include special announcement patch - DP-18678
    
    * Enabled schema_special_announcement - DP-18678
    
    * Added special announcement metatag to content types - DP-18678
    
    * Fixed changelog to include type - DP-18678
    
    * Moved and modified d.o. patch 3120412-12 to use in mass_schema_metatag - DP-18678
    
    * Added State option to SpatialCoverage for SpecialAnnouncement metatag - DP-18678
    
    * Removed composer patch for SpecialAnnouncement - DP-18678
    
    * Added GovernmentBenefitsInfo and QuarantineGuidelines based on recommended properties from Google - DP-18678
    
    * Added GovernmentBenefitsInfo content - DP-18678
    
    * Fixed errors in special announcement code - DP-18678
    
    * Cleaned up code for SpatialCoverage - DP-18678
    
    * Revert "Updated composer to include special announcement patch - DP-18678"
    
    This reverts commit f8a0ac59758ca0257a5659440254056756739751.
    
    * Changed changelog message - DP-18678
    
    * Made recommended CodeSniffer changes - DP-18678
    
    * Removed content types other than Location from SpecialAnnouncement metatag settings - DP-18678
    
    * Added ID to special announcements - DP-18678
    
    * Added default schema id for special announcements - DP-18678
    
    * Added recommended metatag_groups_alter hook based on other schema metatag modules - DP-18678
    
    * Changed schema field labels to match schema.org names like other schema fields - DP-18678
    
    * Added test for special announcement metadata to run against the location content type - DP-18678
    
    * Revert "Removed content types other than Location from SpecialAnnouncement metatag settings - DP-18678"
    
    This reverts commit 72c60d143186a5cd9142ef272d52953a15153687.
    
    * Updated short comment on tag files - DP-18678
    
    * Removed Url support for metatags that support both URL and WebContent - DP-18678
    
    * Added tests for remaining metatags - DP-18678
    
    * Cleaned up tag ordering, removed unnecessary seralize calls - DP-18678
    
    * Cleaned up test metadata - DP-18678
    
    * Corrected module name in comment - DP-18678

---
+++ docroot/modules/custom/mass_schema_metatag/mass_schema_special_announcement/src/Plugin/metatag/Tag/SchemaSpecialAnnouncementSpatialCoverage.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\mass_schema_special_announcement\Plugin\metatag\Tag;
+
+use Drupal\schema_metatag\Plugin\metatag\Tag\SchemaPlaceBase;
+
+/**
+ * Provides a plugin for the 'SpatialCoverage' meta tag.
+ *
+ * @MetatagTag(
+ *   id = "schema_special_announcement_spatial_coverage",
+ *   label = @Translation("spatialCoverage"),
+ *   description = @Translation("What geographical area does the announcement cover?"),
+ *   name = "spatialCoverage",
+ *   group = "schema_special_announcement",
+ *   weight = 11,
+ *   type = "label",
+ *   secure = FALSE,
+ *   multiple = TRUE,
+ * )
+ */
+class SchemaSpecialAnnouncementSpatialCoverage extends SchemaPlaceBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function form(array $element = []) {
+    $form = parent::form($element);
+
+    $form['@type']['#options']['State'] = $this->t('State');
+
+    return $form;
+  }
+
+}
