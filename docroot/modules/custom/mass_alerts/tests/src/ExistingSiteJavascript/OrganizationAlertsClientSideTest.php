48b3366f6c73cd8ed5a8e12042a90514dba3f7ca
danstafford <danstafford@users.noreply.github.com>
2020-06-15 16:03:26 -0400

    DP-18846 Alert system tests (#359)
    * Alert by-org test working
    
    * Add specifc page and editor role alert tests
    
    * Move editor and org node to test setup
    
    * Add editor role organization alert test
    
    * Got old site_side alert working again
    
    * Remove setup and teardown from org and specific alert tests
    
    * Add emergencyAlertPublisher role alerts tests
    
    * PHPCS fix
    
    * Add changelog for DP-18846
    
    * Up PHP memory limit in docker enviroment spun up by circle
    
    * Upped docker php memory to 2GB
    
    * Set environemnt to 2GB too
    
    * Commented out 2gb for a test
    
    * Adjust memory limit again
    
    * Adjust php memory limit
    
    * Try setting memory_limit on phpunit command
    
    * Remove docker memory limit
    
    * No limit phpunit memory limit for testing
    
    * Change memory limit to 2048M
    
    * Try 3072M
    
    * Debug phpunit cmd
    
    * Change memory in settings.php
    
    * Set php memory limit at setup and teardown of tests
    
    * Swap randomString for randomMachineName to eliminate weird chars
    
    * Remove memory_limit tests in setup and teardown
    
    * Increase memory limit when on CircleCi in settings.php
    
    * Reduce memory limit from 1024 to 768
    
    * Swap visit function for drupalGet to generate browser_output during test
    
    * Remove typo
    
    Co-authored-by: Dan Stafford <dan@lastcallmedia.com>

---
+++ docroot/modules/custom/mass_alerts/tests/src/ExistingSiteJavascript/OrganizationAlertsClientSideTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\Tests\mass_alerts\ExistingSiteJavascript;
+
+use Drupal\mass_content_moderation\MassModeration;
+use Drupal\paragraphs\Entity\Paragraph;
+use weitzman\DrupalTestTraits\ExistingSiteWebDriverTestBase;
+
+/**
+ * Test 'By Organization' Alerts.
+ */
+class OrganizationAlertsClientSideTest extends ExistingSiteWebDriverTestBase {
+
+  /**
+   * Test pages have organization alert displaying.
+   *
+   * @throws \Behat\Mink\Exception\ResponseTextException
+   */
+  public function testPagesHaveOrgAlert() {
+
+    $alert_message = $this->randomMachineName();
+
+    $org_node = $this->createNode([
+      'type' => 'org_page',
+      'title' => $this->randomMachineName(),
+      'status' => 1,
+      'moderation_state' => MassModeration::PUBLISHED,
+    ]);
+
+    $news_node = $this->createNode([
+      'type' => 'news',
+      'title' => $this->randomMachineName(),
+      'status' => 1,
+      'moderation_state' => MassModeration::PUBLISHED,
+      'field_organizations' => ['target_id' => $org_node->id()],
+    ]);
+
+    $this->createNode([
+      'type' => 'alert',
+      'title' => $this->randomMachineName(),
+      'field_alert_display' => 'by_organization',
+      'moderation_state' => MassModeration::PUBLISHED,
+      'status' => 1,
+      'field_alert' => Paragraph::create([
+        'type' => 'emergency_alert',
+        'field_emergency_alert_message' => $alert_message,
+      ]),
+      'field_target_organization' => ['target_id' => $org_node->id()],
+    ]);
+
+    $assert_session = $this->assertSession();
+
+    $this->drupalGet('node/' . $org_node->id());
+    $assert_session->pageTextContains($org_node->getTitle());
+    $assert_session->waitForElement('css', '.ma__header-alert__message');
+    $assert_session->pageTextContains($alert_message);
+
+    $this->drupalGet('node/' . $news_node->id());
+    $assert_session->pageTextContains($news_node->getTitle());
+    $assert_session->waitForElement('css', '.ma__header-alert__message');
+    $assert_session->pageTextContains($alert_message);
+  }
+
+}
