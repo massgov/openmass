637c0aa80d70d62e91c42b149b2e0f677b2814f3
Moshe Weitzman <weitzman@tejasa.com>
2020-08-14 14:26:41 -0400

    Bring back unpubRemindersTest. Move a few lines to validate job

--- docroot/modules/custom/mass_unpublish_reminders/tests/ExistingSite/UnpublishRemindersTest.php
+++ docroot/modules/custom/mass_unpublish_reminders/tests/ExistingSite/UnpublishRemindersTest.php
@@ -36,7 +36,6 @@ class UnpublishRemindersTest extends ExistingSiteBase {
    * {@inheritdoc}
    */
   protected function setUp() {
-    $this->markTestSkipped('Too slow');
     parent::setUp();
     $this->startMailCollection();
     $this->clearQueue('mass_unpublish_reminders_queue');
