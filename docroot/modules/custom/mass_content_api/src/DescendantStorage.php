0b47f22511831921dc3d3beb8a8e1f9eb15c27df
Moshe Weitzman <weitzman@tejasa.com>
2020-10-07 17:29:57 -0400

    Remove debug loggings.

--- docroot/modules/custom/mass_content_api/src/DescendantStorage.php
+++ docroot/modules/custom/mass_content_api/src/DescendantStorage.php
@@ -25,7 +25,8 @@ class DescendantStorage implements DescendantStorageInterface {
   private $database;
   private $table;
   private $debugTable;
-  private $debug = TRUE;
+  // If enabling, also fix bug where debug is a giant text string, too large for its column.
+  private $debug = FALSE;
 
   /**
    * Constructor.
