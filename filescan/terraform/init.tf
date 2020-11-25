5c9635b34608d42a41482e5a953d24696e5314da
Tom Fleming <tom@lastcallmedia.com>
2020-11-25 15:58:40 -0500

    Corrected bucket name - DP-20492

--- filescan/terraform/init.tf
+++ filescan/terraform/init.tf
@@ -1,17 +1,17 @@
-// Configure the AWS provider.
 provider "aws" {
   region = "us-east-1"
 }
 
-// Setup remote state storage in an S3 bucket, and inform terraform about
-// the bucket that is used so we can reference it from other places.
-
-// Configure the Terraform backend to store state in S3.
 terraform {
   backend "s3" {
-    bucket         = "terraform.secure.digital.mass.gov"
-    key            = "terraform/state/massgov.filescan.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform"
+    bucket               = "application-configurations"
+    key                  = "terraform/state/massgov.filescan.tfstate"
+    workspace_key_prefix = "terraform/state/workspaces"
+    region               = "us-east-1"
+    dynamodb_table       = "terraform"
   }
+}
+
+locals {
+  state_bucket = "application-configurations"
 }
