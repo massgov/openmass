5ea677db5f26c13666b769959391ef46720e875b
Tom Fleming <tom@lastcallmedia.com>
2020-11-25 16:45:57 -0500

    Added default values - DP-20492

--- filescan/terraform/variables.tf
+++ filescan/terraform/variables.tf
@@ -1,11 +1,14 @@
 variable "av_definitions_bucket_name" {
   type = string
+  default = "clamav-definitions.digital.mass.gov"
 }
 
 variable "chamber_namespace" {
   type = string
+  default = "tf/massgov-filescan-deployment"
 }
 
 variable "tags" {
   type = map(string)
+  default = {}
 }
