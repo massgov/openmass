f492f5b993168a0fdfd5beff4b6721c23db90048
Tom Fleming <tom@lastcallmedia.com>
2021-02-10 17:42:17 -0500

    Moved over to setResponseNotCacheable - DP-20580

--- docroot/modules/custom/mass_admin_pages/src/EventSubscriber/CookieSubscriber.php
+++ docroot/modules/custom/mass_admin_pages/src/EventSubscriber/CookieSubscriber.php
@@ -4,16 +4,9 @@ namespace Drupal\mass_admin_pages\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Drupal\Core\EventSubscriber\FinishResponseSubscriber;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-use Drupal\Core\Cache\Context\CacheContextsManager;
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Language\LanguageManagerInterface;
-use Drupal\Core\PageCache\RequestPolicyInterface;
-use Drupal\Core\PageCache\ResponsePolicyInterface;
 use Symfony\Component\HttpFoundation\Cookie;
-use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
-use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Response subscriber to add cookies.
@@ -23,31 +16,19 @@ class CookieSubscriber extends FinishResponseSubscriber implements EventSubscrib
   /**
    * {@inheritdoc}
    */
-  public function __construct(LanguageManagerInterface $language_manager, ConfigFactoryInterface $config_factory, RequestPolicyInterface $request_policy, ResponsePolicyInterface $response_policy, CacheContextsManager $cache_contexts_manager, $http_response_debug_cacheability_headers = FALSE) {
-    parent::__construct($language_manager, $config_factory, $request_policy, $response_policy, $cache_contexts_manager, $http_response_debug_cacheability_headers);
-  }
+  protected function setResponseNotCacheable(Response $response, Request $request) {
+    parent::setResponseNotCacheable($response, $request);
 
-  /**
-   * {@inheritdoc}
-   */
-  public static function getSubscribedEvents() {
-    $events[KernelEvents::RESPONSE][] = ['checkForNodeEdit'];
-    return $events;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function checkForNodeEdit(FilterResponseEvent $event) {
     $server_name = explode('.', gethostname());
     if (\Drupal::currentUser()->isAuthenticated()) {
-      if ($event->getRequest()->attributes->get('_route') == 'entity.node.edit_form') {
+      $response->headers->clearCookie('ah_app_server', '/');
+
+      // Set cookie to stick file uploads to one server when on edit pages to
+      // address issues with uploading files via the WYSIWYG.
+      // See https://support.acquia.com/hc/en-us/articles/360004147834-Pinning-to-a-web-server-without-using-the-hosts-file#defineacookie
+      if ($request->attributes->get('_route') == 'entity.node.edit_form') {
         $cookie = new Cookie('ah_app_server', rawurlencode($server_name[0]), REQUEST_TIME + 86400, '/');
-        $event->getResponse()->headers->setCookie($cookie);
-      }
-      else {
-        $cookie = new Cookie('ah_app_server', '', REQUEST_TIME - 3600, '/');
-        $event->getResponse()->headers->setCookie($cookie);
+        $response->headers->setCookie($cookie);
       }
     }
   }
