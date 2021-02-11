ce55eac8fa2499153f4162d6c772d7cdd548b688
Tom Fleming <tom@lastcallmedia.com>
2021-02-11 13:50:23 -0500

    Changed route to entity browser dialog - DP-21191

--- docroot/modules/custom/mass_admin_pages/src/EventSubscriber/CookieSubscriber.php
+++ docroot/modules/custom/mass_admin_pages/src/EventSubscriber/CookieSubscriber.php
@@ -38,7 +38,7 @@ class CookieSubscriber extends FinishResponseSubscriber implements EventSubscrib
       // Set cookie to stick file uploads to one server when on edit pages to
       // address issues with uploading files via the WYSIWYG.
       // See https://support.acquia.com/hc/en-us/articles/360004147834-Pinning-to-a-web-server-without-using-the-hosts-file#defineacookie
-      if ($request->attributes->get('_route') == 'entity.node.edit_form') {
+      if ($request->attributes->get('_route') == 'entity_browser.browse_files') {
         $cookie = new Cookie('ah_app_server', rawurlencode($server_name[0]), REQUEST_TIME + 86400, '/');
         $response->headers->setCookie($cookie);
       }
