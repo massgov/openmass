5532666fa393c852df1e24ab2b9eae2a74b999af
Tom Fleming <tom@lastcallmedia.com>
2021-02-22 15:34:26 -0500

    Cleaned up resizer scripts to fix eslint errors - DP-20892

--- docroot/themes/custom/mass_theme/overrides/js/iframe_resizer_iframe.js
+++ docroot/themes/custom/mass_theme/overrides/js/iframe_resizer_iframe.js
@@ -1,5 +1,10 @@
-/*
+/**
+ * @file
+ * Iframe resizer iframe script.
+ * @see https://jacobfilipp.com/iframe-height-autoresize-crossdomain/
+ */
 
+/*
 MIT License
 
 Copyright (c) 2019 Jacob Filipp
@@ -25,29 +30,32 @@ SOFTWARE.
 
 
 // Add this script into the page that will appear <b>inside an iFrame</b>
-// This code monitors the page for changes in size. When change is detected, it sends send the latest size to the parent page using postMessage
+// This code monitors the page for changes in size. When change is detected, it sends send the latest size to the parent page using postMessage.
+var iframeDimensions_Old = '';
 
+// Determine height of content on this page.
+function getMyHeight() {
+  'use strict';
 
-// determine height of content on this page
-function getMyHeight()
-{
-  //https://stackoverflow.com/a/11864824
-  return Math.max( document.body.scrollHeight, document.documentElement.scrollHeight)
+  // https://stackoverflow.com/a/11864824
+  return Math.max(document.body.scrollHeight, document.documentElement.scrollHeight);
 }
 
 
-// send the latest page dimensions to the parent page on which this iframe is embedded
-function sendDimensionsToParent()
-{
+// Send the latest page dimensions to the parent page on which this iframe is embedded.
+function sendDimensionsToParent() {
+  'use strict';
+
   var iframeDimensions_New = {
-    'width': window.innerWidth, //supported from IE9 onwards
-    'height': getMyHeight()
+    // Supported from IE9 onwards.
+    width: window.innerWidth,
+    height: getMyHeight()
   };
 
-  if( (iframeDimensions_New.width != iframeDimensions_Old.width) || (iframeDimensions_New.height != iframeDimensions_Old.height) )  // if old width is not equal new width, or old height is not equal new height, then...
-  {
+  // If old width is not equal new width, or old height is not equal new height, then...
+  if ((iframeDimensions_New.width !== iframeDimensions_Old.width) || (iframeDimensions_New.height !== iframeDimensions_Old.height)) {
 
-    window.parent.postMessage(iframeDimensions_New, "*");
+    window.parent.postMessage(iframeDimensions_New, '*');
     iframeDimensions_Old = iframeDimensions_New;
 
   }
@@ -55,24 +63,24 @@ function sendDimensionsToParent()
 }
 
 
-// on load - send the page dimensions. (we do this on load because then all images have loaded...)
-window.addEventListener( 'load', function(){
-
+// On load - send the page dimensions. (we do this on load because then all images have loaded...)
+window.addEventListener('load', function () {
+  'use strict';
 
   iframeDimensions_Old = {
-    'width': window.innerWidth, //supported from IE9 onwards
-    'height': getMyHeight()
+    // Supported from IE9 onwards.
+    width: window.innerWidth,
+    height: getMyHeight()
   };
 
-  window.parent.postMessage(iframeDimensions_Old, "*"); //send our dimensions once, initially - so the iFrame is initialized to the correct size
+  // Send our dimensions once, initially - so the iFrame is initialized to the correct size.
+  window.parent.postMessage(iframeDimensions_Old, '*');
 
-
-  if( window.MutationObserver ) // if mutationobserver is supported by this browser
-  {
-    //https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
-
+  // If mutationobserver is supported by this browser.
+  if (window.MutationObserver) {
+    // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver.
     var observer = new MutationObserver(sendDimensionsToParent);
-    config = {
+    var config = {
       attributes: true,
       attributeOldValue: false,
       characterData: true,
@@ -82,14 +90,12 @@ window.addEventListener( 'load', function(){
     };
 
     observer.observe(document.body, config);
-
   }
-  else // if mutationobserver is NOT supported
-  {
-    //check for changes on a timed interval, every 1/3 of a second
+  // If mutationobserver is NOT supported.
+  else {
+    // Check for changes on a timed interval, every 1/3 of a second.
     window.setInterval(sendDimensionsToParent, 300);
   }
 
-
-});   // end of window.onload
-
+// End of window.onload.
+});
