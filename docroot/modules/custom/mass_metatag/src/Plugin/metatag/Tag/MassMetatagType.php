af54be53011dbc7e9f6a40c865feaa45706a6c67
Yukiko Gannett <yukikogannett@gmail.com>
2021-03-16 13:41:28 -0400

    set my_type value to be single with the field option

--- docroot/modules/custom/mass_metatag/src/Plugin/metatag/Tag/MassMetatagType.php
+++ docroot/modules/custom/mass_metatag/src/Plugin/metatag/Tag/MassMetatagType.php
@@ -43,52 +43,18 @@ class MassMetatagType extends MetaNameBase {
         if ($node->hasField($term_ref_name) && $data_type = $node->get($term_ref_name)) {
           $items = $data_type->referencedEntities();
           if (!empty($items)) {
-
-
             $type_name = $items[0]->get('name')->getString();
 
             if ($type_name === 'Data resource') {
               $outputValue = 'data-resource';
             }
-
-            // $data_resources = $node->get('field_data_resource_type');
-            // $tags = [];
-
-            // field_data_resource_type has a value + data type = Data resource.
-            // if (!$data_resources->isEmpty() && strpos($type_name, 'resource') !== FALSE) {
-
-            //   // Values for mg_resource_type[NEW]
-            //   // foreach ($data_resources->referencedEntities() as $k => $resource) {
-            //   //   $tags[$k] = $resource->get('field_dataresource_metatag')->getString();
-            //   // }
-            //   // $outputValue = ', ' . implode(', ', $tags);
-
-            //   $outputValue = '1. DATA TYPE: ' . $data_type->getString() . ' TYPE NAME: ' . $type_name;
-            // }
-
-            // elseif ($data_resources->isEmpty() && strpos($type_name, 'resource') !== FALSE) {
-            //   // Type = Data resource + 'field_data_resource_type' = unchecked.
-            //   $outputValue = '2. DATA TYPE: ' . $data_type . ' TYPE NAME: ' . $type_name;
-            // }
-
-            // if (!$items[0]->get('field_details_datatype_metatag')->isEmpty()) {
             else {
               // Anything but 'Data resource'.
-              $outputValue = 'ENTITY TERM: ' . $items[0]->get('field_details_datatype_metatag')->getString() . ' DATA TYPE: ' . $type_name = $items[0]->get('name')->getString();
+              $outputValue = $items[0]->get('field_details_datatype_metatag')->getString();
             }
-            // $outputValue DOESN'T GET PASSED.
-            // THIS CONDISTION DOESN'T GET TRUE TO PASS THE VARIABLE.
           }
-          else {
-            // $outputValue = "puppy";
-          }
-          // $outputValue = "frog";
         }
-        // Add the type value to the initial mg_type value, field_binder_binder_type.
-        $element['#attributes']['content'] = $outputValue;
-        // $element['#attributes']['content'] = $element['#attributes']['content'] . $outputValue;
       }
-      // $outputValue = "orange";
       $element['#attributes']['content'] = $outputValue;
     }
 
