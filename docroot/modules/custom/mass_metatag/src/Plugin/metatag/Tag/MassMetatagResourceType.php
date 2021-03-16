2d8e36897eef787cdcd7046c366fd53cb27d3547
Yukiko Gannett <yukikogannett@gmail.com>
2021-03-16 15:04:29 -0400

    fix lint error

--- docroot/modules/custom/mass_metatag/src/Plugin/metatag/Tag/MassMetatagResourceType.php
+++ docroot/modules/custom/mass_metatag/src/Plugin/metatag/Tag/MassMetatagResourceType.php
@@ -34,41 +34,41 @@ class MassMetatagResourceType extends MetaNameBase {
 
     $node = \Drupal::routeMatch()->getParameter('node');
 
-      if ($node instanceof NodeInterface && $node->bundle() == 'binder') {
-        if ($node->get('field_binder_data_type')) {
-          $items = $node->get('field_binder_data_type')->referencedEntities();
-          if (!empty($items)) {
-            // Check Binder data type, field_binder_data_type to determin if mg_resource_type is neccessary or not.
-            if ($items[0]->get('name')->getString() === 'Data resource') {
+    if ($node instanceof NodeInterface && $node->bundle() == 'binder') {
+      if ($node->get('field_binder_data_type')) {
+        $items = $node->get('field_binder_data_type')->referencedEntities();
+        if (!empty($items)) {
+          // Check Binder data type, field_binder_data_type to determin if mg_resource_type is neccessary or not.
+          if ($items[0]->get('name')->getString() === 'Data resource') {
 
-              $data_resources = $node->get('field_data_resource_type');
-              $types = [];
-              $outputValue = '';
+            $data_resources = $node->get('field_data_resource_type');
+            $types = [];
+            $outputValue = '';
 
-              // field_data_resource_type has a value + data type = Data resource.
-              if (!$data_resources->isEmpty()) {
+            // field_data_resource_type has a value + data type = Data resource.
+            if (!$data_resources->isEmpty()) {
 
-                // Values for mg_resource_type[NEW]
-                foreach ($data_resources->referencedEntities() as $k => $resource) {
-                  $types[$k] = $resource->get('field_dataresource_metatag')->getString();
-                }
-                $outputValue = implode(', ', $types);
+              // Values for mg_resource_type[NEW]
+              foreach ($data_resources->referencedEntities() as $k => $resource) {
+                $types[$k] = $resource->get('field_dataresource_metatag')->getString();
               }
-              else {
-                $outputValue = "null";
-              }
-
-              $element = [
-                '#tag' => 'meta',
-                '#attributes' => [
-                  'name' => 'mg_resource_type',
-                  'content' => $outputValue,
-                ]
-              ];
+              $outputValue = implode(', ', $types);
             }
+            else {
+              $outputValue = "null";
+            }
+
+            $element = [
+              '#tag' => 'meta',
+              '#attributes' => [
+                'name' => 'mg_resource_type',
+                'content' => $outputValue,
+              ]
+            ];
           }
         }
       }
+    }
     return $element;
   }
 }
