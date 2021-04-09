119b51b23ccf786553cefd7670a567b6ad1f454f
April Sides <april.sides@lullabot.com>
2021-04-09 16:26:48 -0400

    DP-19365: D9 upgrade packages - Views (#648)
    * DP-19365: Updating the address module to 1.9.0
    
    * DP-19365: Updating the draggableviews module to 2.0
    
    * DP-19365: Updating the flag module to 4.0.0-beta2
    
    * DP-19365: Updating metatag module to 1.16.0
    
    * DP-19365: Updating paragraphs module to 1.12.0
    
    * DP-19365: Updating the pathauto module to 1.8.0
    
    * DP-19365: Updating the views_bulk_operations module to 3.11.0
    
    * DP-19365: Updating the token module to 1.9.0
    
    * DP-19365: Updating the entity_reference_revisions module to 1.9.0
    
    * DP-19365: Added changelog file
    
    * DP-19365: Updating mass_metatag PHPUnit tests from metatag module updates
    
    * DP-19365: Updating the schema_metatag module to 1.8.0
    
    * DP-19365: Refactoring schema parsing logic to identify all schemas for a given page
    
    * DP-19365: Fixing code standards in refactored code

--- docroot/modules/custom/mass_metatag/tests/src/ExistingSite/DecisionMetadataTest.php
+++ docroot/modules/custom/mass_metatag/tests/src/ExistingSite/DecisionMetadataTest.php
@@ -104,7 +104,7 @@ class DecisionMetadataTest extends MetadataTestCase {
         '@type' => 'AboutPage',
         '@id' => $url . '#decision',
         'about' => 'TestOverview',
-        'releasedEvent' => 'Mon, 12/31/2012 - 12:00',
+        'releasedEvent' => '<time datetime="2012-12-31T12:00:00Z">Mon, 12/31/2012 - 12:00</time>',
         'significantLink' => [
           file_create_url('public://test.txt'),
         ],
