702eb98c72cb07ca2a3e6e22f949956b89808269
Yukiko Gannett <yukikogannett@gmail.com>
2021-04-20 10:56:05 -0400

    DP-20281 Translation support (#553)
    * enable language module
    
    * add sample languages under Administration > Configuration > Regional and language > Languages
    
    * Add language field to alerts, curated list, form, info details, news, promo pages and service details
    
    * Position the language field below org. field in content types
    
    * configure lang. field to exclude locked languages
    
    * add English version field
    
    * position English version field below Language field in news
    
    * add conditional logic to english version field
    
    * Adjust the language field style to match others'
    
    * add mass_translations to modules
    
    * add English version field to media
    
    * set up test config of mass_translations
    
    * add Translations tab
    
    * set position of translations tab
    
    * rename route
    
    * enable mass_translations module
    
    * disable language field from content types with no english version field
    
    * Removed local settings - DP-20281
    
    * Excluding menu_link_content from sanitization - DP-20281
    
    * Added view for language selection - DP-20281
    
    * Added working controller showing links to other language versions - DP-20281
    
    * Added hreflang links - DP-20281
    
    * Set media translations parameter to media entities - DP-20281
    
    * Added english version reference view to other content types - DP-20281
    
    * Updated English version fields, removed confusing title/help text - DP-20281
    
    * Added Translations tab, made split controller with shared markup and language fetcher - DP-20281
    
    * Added changelog - DP-20281
    
    * Fixed CS issues - DP-20281
    
    * Added languages based on language terms used - DP-20281
    
    * Added language selector to documents - DP-20281
    
    * Swapped language entity reference for language module selector - DP-20281
    
    * Set conditional field for language selector - DP-20281
    
    * Added language filter for documents and content views - DP-20281
    
    * Hid old language field on documents, tried a different language detection method - DP-20281
    
    * changed English version variable declaraton - DP-20281
    
    * Removed unused interfaces - DP-20281
    
    * temp commit of deploy/tests
    
    * Removed other language detection methods, allow URLs to be language agnostic - DP-20281
    
    * Added access check for Translations tab - DP-20281
    
    * Added help text - DP-20281
    
    * Moved alias alter to mass_translations - DP-20281
    
    * Uncommented deploy - DP-20281
    
    * Fixed deploy hook - DP-20281
    
    * Added check - DP-20281
    
    * Fix PHPCS error - DP-20281
    
    * Updated deploy hook - DP-20281
    
    * Simplified deploy hook - DP-20281
    
    * Revert "Excluding menu_link_content from sanitization - DP-20281"
    
    This reverts commit 0a73db59cc60bce86f31a9440efb42f47ee88f8e.
    
    * Moved English version field to below Language field on Documents - DP-20281
    
    * Removed published filter from english version views - DP-20281
    
    * Limited language filter values in content and document filters - DP-20281
    
    * Removed not applicable/satisfied options from Documents language selector - DP-20281
    
    * Fixed form page autocomplete - DP-20281
    
    * Added tests for translation links - DP-20281
    
    * Added media_presave bypass to resolve deploy issues when bulk updating media entities with pointers to missing files - DP-20281
    
    * Update docroot/modules/custom/mass_translations/mass_translations.module
    
    * Added media test - DP-20281
    
    * Updated node translation test - DP-20281
    
    * Fixed namespace on tests
    
    * Added checking for Translations tabs on nodes and documents - DP-20281
    
    * Added note about hiding confusing media help text - DP-20281
    
    Co-authored-by: Tom Fleming <tom@lastcallmedia.com>

---
+++ docroot/modules/custom/mass_translations/tests/ExistingSite/DocumentTranslationTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Drupal\Tests\mass_translations\ExistingSite;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\file\Entity\File;
+use Drupal\user\Entity\User;
+use weitzman\DrupalTestTraits\Entity\MediaCreationTrait;
+use weitzman\DrupalTestTraits\ExistingSiteBase;
+use weitzman\LoginTrait\LoginTrait;
+
+/**
+ * Service Details translation tests.
+ */
+class DocumentTranslationTest extends ExistingSiteBase {
+
+  use MediaCreationTrait;
+  use LoginTrait;
+
+  private $editor;
+  private $orgNode;
+  private $file;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $user = User::create(['name' => $this->randomMachineName()]);
+    $user->addRole('editor');
+    $user->activate();
+    $user->save();
+    $this->editor = $user;
+
+    $this->orgNode = $this->createNode([
+      'type' => 'org_page',
+      'title' => $this->randomMachineName(),
+      'status' => 1,
+      'moderation_state' => 'published',
+    ]);
+
+    // Create a "Llama" media item.
+    file_put_contents('public://llama-43.txt', 'Test');
+    $this->file = File::create([
+      'uri' => 'public://llama-43.txt',
+    ]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getMedia(): EntityInterface {
+    $media = $this->createMedia([
+      'bundle' => 'document',
+      'title' => 'Test Document',
+      'field_organizations' => [$this->orgNode],
+      'field_upload_file' => [$this->file],
+      'moderation_state' => 'published',
+    ]);
+
+    $langcodes = \Drupal::languageManager()->getLanguages();
+    unset($langcodes['en']);
+    $langcode = array_rand($langcodes);
+    $translation = $this->createMedia([
+      'bundle' => 'document',
+      'title' => 'Test Document',
+      'field_organizations' => [$this->orgNode],
+      'field_upload_file' => [$this->file],
+      'field_media_english_version' => [$media],
+      'moderation_state' => 'published',
+      'langcode' => $langcode,
+    ]);
+
+    return $translation;
+  }
+
+  /**
+   * Retrieve the href value for translated link.
+   */
+  public function testHasTranslation() {
+    $this->drupalLogin($this->editor);
+    $entity = $this->getMedia();
+    $this->drupalGet($entity->toUrl());
+    $this->assertEquals(200, $this->getSession()->getStatusCode(), 'Entity page was loadable');
+    $page = $this->getSession()->getPage();
+    $element = $page->find('css', '.ma__listing-table__container')->getText();
+    $this->assertContains($entity->language()->getName(), $element, 'Language not found');
+    $tabs = $page->find('css', '.primary-tabs')->getText();
+    $this->assertContains('Translations', $tabs, 'No Translations tab was found');
+  }
+
+}
