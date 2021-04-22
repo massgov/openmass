7ec2699c7842527d75696898367d37419033467d
April Sides <weekbeforenext@gmail.com>
2021-04-22 18:18:53 -0400

    DP-21634: Reducing the batch size for the language_terms deploy hook again

--- docroot/modules/custom/mass_translations/mass_translations.deploy.php
+++ docroot/modules/custom/mass_translations/mass_translations.deploy.php
@@ -28,7 +28,7 @@ function mass_translations_deploy_language_terms(&$sandbox) {
     $sandbox['max'] = $count->count()->execute();
   }
 
-  $batch_size = 100;
+  $batch_size = 50;
 
   $mids = $query->condition('mid', $sandbox['current'], '>')
     ->sort('mid')
