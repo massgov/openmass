51ac9609cd60d9215bf79cb301b32b0b9cdd37a8
Minghua Sun <clairesunstudio@gmail.com>
2021-04-29 11:00:21 -0400

    lint fixes

--- docroot/themes/custom/mass_theme/overrides/js/iframe_resizer_iframe.js
+++ docroot/themes/custom/mass_theme/overrides/js/iframe_resizer_iframe.js
@@ -1,4 +1,4 @@
-/* Instructions 
+/* Instructions
 **
 ** Mass.gov CDN: https://mass.gov/themes/custom/mass_theme/overrides/js/iframe_resizer_iframe.js
 ** Add this script into the iframe source code before the closing body tag:
@@ -33,67 +33,65 @@ SOFTWARE.
 
 
 // determine height of content on this page
-function getMyHeight()
-{
-	//https://stackoverflow.com/a/11864824
-	return Math.max( document.body.scrollHeight, document.documentElement.scrollHeight)
+function getMyHeight() {
+  // https://stackoverflow.com/a/11864824
+  return Math.max(document.body.scrollHeight, document.documentElement.scrollHeight);
 }
 
 
 // send the latest page dimensions to the parent page on which this iframe is embedded
-function sendDimensionsToParent()
-{
-	var iframeDimensions_New = {
-				'width': window.innerWidth, //supported from IE9 onwards
-				'height': getMyHeight()
-			};
+function sendDimensionsToParent() {
+  var iframeDimensions_New = {
+    width: window.innerWidth, // supported from IE9 onwards
+    height: getMyHeight()
+  };
 
-	if( (iframeDimensions_New.width != iframeDimensions_Old.width) || (iframeDimensions_New.height != iframeDimensions_Old.height) )  // if old width is not equal new width, or old height is not equal new height, then...
-	{
+  if ((iframeDimensions_New.width != iframeDimensions_Old.width) || (iframeDimensions_New.height != iframeDimensions_Old.height)) // if old width is not equal new width, or old height is not equal new height, then...
+  {
 
-		window.parent.postMessage(iframeDimensions_New, "*");
-		iframeDimensions_Old = iframeDimensions_New;
+    window.parent.postMessage(iframeDimensions_New, '*');
+    iframeDimensions_Old = iframeDimensions_New;
 
-	}
+  }
 
 }
 
 
 // on load - send the page dimensions. (we do this on load because then all images have loaded...)
-window.addEventListener( 'load', function(){
+window.addEventListener('load', function () {
   // For debugging purposes
-	console.log('loaded iframe JS: ' + document.URL)
+  console.log('loaded iframe JS: ' + document.URL);
 
-	iframeDimensions_Old = {
-						'width': window.innerWidth, //supported from IE9 onwards
-						'height': getMyHeight()
-					};
+  iframeDimensions_Old = {
+    width: window.innerWidth, // supported from IE9 onwards
+    height: getMyHeight()
+  };
 
-	window.parent.postMessage(iframeDimensions_Old, "*"); //send our dimensions once, initially - so the iFrame is initialized to the correct size
+  window.parent.postMessage(iframeDimensions_Old, '*'); // send our dimensions once, initially - so the iFrame is initialized to the correct size
 
 
-	if( window.MutationObserver ) // if mutationobserver is supported by this browser
-	{
-		//https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+  if (window.MutationObserver) // if mutationobserver is supported by this browser
+  {
+    // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
 
-		var observer = new MutationObserver(sendDimensionsToParent);
-		config = {
+    var observer = new MutationObserver(sendDimensionsToParent);
+    config = {
 			  attributes: true,
 			  attributeOldValue: false,
 			  characterData: true,
 			  characterDataOldValue: false,
 			  childList: true,
 			  subtree: true
-		};
+    };
 
-		observer.observe(document.body, config);
+    observer.observe(document.body, config);
 
-	}
-	else // if mutationobserver is NOT supported
-	{
-		//check for changes on a timed interval, every 1/3 of a second
-		window.setInterval(sendDimensionsToParent, 300);
-	}
+  }
+  else // if mutationobserver is NOT supported
+  {
+    // check for changes on a timed interval, every 1/3 of a second
+    window.setInterval(sendDimensionsToParent, 300);
+  }
 
 
-});   // end of window.onload
+}); // end of window.onload
--- docroot/themes/custom/mass_theme/overrides/js/iframe_resizer_parent.js
+++ docroot/themes/custom/mass_theme/overrides/js/iframe_resizer_parent.js
@@ -32,24 +32,26 @@ SOFTWARE.
 // event.data - the object that the iframe sent us
 // event.origin - the URL from which the message came
 // event.source - a reference to the 'window' object that sent the message
-function gotResizeMessage(event)
-{
-	var matches = document.querySelectorAll('iframe'); // iterate through all iFrames on page
-	for (i=0; i<matches.length; i++)
-	{
-		if( matches[i].contentWindow == event.source ) // found the iFrame that sent us a message
-		{
-			//matches[i].width = Number( event.data.width )	 <--- we do not do anything with the page width for now
-			matches[i].height = Number( event.data.height )
+function gotResizeMessage(event) {
+  'use strict';
+  var matches = document.querySelectorAll('iframe'); // iterate through all iFrames on page
+  var i = 0;
+  for (; i < matches.length; i++) {
+    // found the iFrame that sent us a message
+    if (matches[i].contentWindow === event.source) {
+      // matches[i].width = Number( event.data.width )	 <--- we do not do anything with the page width for now
+      matches[i].height = Number(event.data.height);
 
-			return 1;
-		}
-	}
+      return 1;
+    }
+  }
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-  console.log('loaded parent JS')
+document.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+  // eslint-disable-next-line no-console
+  console.log('loaded parent JS');
 
-	window.addEventListener("message", gotResizeMessage, false)
+  window.addEventListener('message', gotResizeMessage, false);
 
-}); //on DOM ready
+}); // on DOM ready
