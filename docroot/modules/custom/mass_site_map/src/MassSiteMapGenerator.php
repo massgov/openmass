0d4392bd08b3f9ca37f3e4159e7ffe37b1b58202
Matt Kleve <matt.kleve@lullabot.com>
2021-05-14 14:23:06 -0600

    Initial commit WIP

--- docroot/modules/custom/mass_site_map/src/MassSiteMapGenerator.php
+++ docroot/modules/custom/mass_site_map/src/MassSiteMapGenerator.php
@@ -2,14 +2,14 @@
 
 namespace Drupal\mass_site_map;
 
-use Drupal\simple_sitemap\SitemapGenerator;
+use Drupal\simple_sitemap\Plugin\simple_sitemap\SitemapGenerator\SitemapWriter;
 
 /**
  * Class MassSiteMapGenerator.
  *
  * @package Drupal\mass_site_map
  */
-class MassSiteMapGenerator extends SitemapGenerator {
+class MassSiteMapGenerator extends SitemapWriter {
 
   /**
    * Generates and returns a sitemap chunk.
@@ -21,11 +21,11 @@ class MassSiteMapGenerator extends SitemapGenerator {
    *   Sitemap chunk
    */
   protected function generateSitemapChunk(array $links) {
-    $this->writer->openMemory();
-    $this->writer->setIndent(TRUE);
-    $this->writer->startDocument(self::XML_VERSION, self::ENCODING);
-    $this->writer->writeComment(self::GENERATED_BY);
-    $this->writer->startElement('urlset');
+    $this->openMemory();
+    $this->setIndent(TRUE);
+    $this->startDocument(self::XML_VERSION, self::ENCODING);
+    $this->writeComment(self::GENERATED_BY);
+    $this->startElement('urlset');
 
     // Add attributes to document.
     if (!$this->isHreflangSitemap()) {
@@ -33,7 +33,7 @@ class MassSiteMapGenerator extends SitemapGenerator {
     }
     $this->moduleHandler->alter('simple_sitemap_attributes', self::$attributes);
     foreach (self::$attributes as $name => $value) {
-      $this->writer->writeAttribute($name, $value);
+      $this->writeAttribute($name, $value);
     }
 
     // Add URLs to document.
@@ -41,70 +41,70 @@ class MassSiteMapGenerator extends SitemapGenerator {
     foreach ($links as $link) {
 
       // Add each translation variant URL as location to the sitemap.
-      $this->writer->startElement('url');
-      $this->writer->writeElement('loc', $link['url']);
+      $this->startElement('url');
+      $this->writeElement('loc', $link['url']);
 
       // If more than one language is enabled, add all translation variant URLs
       // as alternate links to this location turning the sitemap into a hreflang
       // sitemap.
       if (isset($link['alternate_urls']) && $this->isHreflangSitemap()) {
         foreach ($link['alternate_urls'] as $language_id => $alternate_url) {
-          $this->writer->startElement('xhtml:link');
-          $this->writer->writeAttribute('rel', 'alternate');
-          $this->writer->writeAttribute('hreflang', $language_id);
-          $this->writer->writeAttribute('href', $alternate_url);
-          $this->writer->endElement();
+          $this->startElement('xhtml:link');
+          $this->writeAttribute('rel', 'alternate');
+          $this->writeAttribute('hreflang', $language_id);
+          $this->writeAttribute('href', $alternate_url);
+          $this->endElement();
         }
       }
 
       // Add lastmod if any.
       if (isset($link['lastmod'])) {
-        $this->writer->writeElement('lastmod', $link['lastmod']);
+        $this->writeElement('lastmod', $link['lastmod']);
       }
 
       // Add changefreq if any.
       if (isset($link['changefreq'])) {
-        $this->writer->writeElement('changefreq', $link['changefreq']);
+        $this->writeElement('changefreq', $link['changefreq']);
       }
 
       // Add priority if any.
       if (isset($link['priority'])) {
-        $this->writer->writeElement('priority', $link['priority']);
+        $this->writeElement('priority', $link['priority']);
       }
 
       // Add images if any.
       if (!empty($link['images'])) {
         foreach ($link['images'] as $image) {
-          $this->writer->startElement('image:image');
-          $this->writer->writeElement('image:loc', $image['path']);
-          $this->writer->endElement();
+          $this->startElement('image:image');
+          $this->writeElement('image:loc', $image['path']);
+          $this->endElement();
         }
       }
 
       // PageMap support.
       if (!empty($link['pagemap'])) {
-        $this->writer->startElement('PageMap');
-        $this->writer->writeAttribute('xmlns', 'http://www.google.com/schemas/sitemap-pagemap/1.0');
+        $this->startElement('PageMap');
+        $this->writeAttribute('xmlns', 'http://www.google.com/schemas/sitemap-pagemap/1.0');
         if (!empty($link['pagemap']['metatags'])) {
-          $this->writer->startElement('DataObject');
-          $this->writer->writeAttribute('type', 'metatags');
+          $this->startElement('DataObject');
+          $this->writeAttribute('type', 'metatags');
           foreach ($link['pagemap']['metatags'] as $tag) {
-            $this->writer->startElement('Attribute');
-            $this->writer->writeAttribute('name', $tag['name']);
-            $this->writer->text($tag['value']);
-            $this->writer->endElement();
+            $this->startElement('Attribute');
+            $this->writeAttribute('name', $tag['name']);
+            $this->text($tag['value']);
+            $this->endElement();
           }
-          $this->writer->endElement();
+          $this->endElement();
         }
-        $this->writer->endElement();
+        $this->endElement();
       }
 
-      $this->writer->endElement();
+      $this->endElement();
     }
-    $this->writer->endElement();
-    $this->writer->endDocument();
+    $this->endElement();
+    $this->endDocument();
 
-    return $this->writer->outputMemory();
+    return $this->outputMemory();
   }
 
 }
