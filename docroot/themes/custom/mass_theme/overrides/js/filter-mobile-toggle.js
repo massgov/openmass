886ecbf19ea3a6c1c8abe4715a0669dcabd11a89
Minghua Sun <clairesunstudio@gmail.com>
2021-05-19 17:23:51 -0400

    add toggle button and files

---
+++ docroot/themes/custom/mass_theme/overrides/js/filter-mobile-toggle.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * Support views with filters 
+ */
+
+/* views-view--data-listing */
+
+(function () {
+  'use strict';
+
+  const toggleButton = document.getElementById('filter-toggle');
+  console.log(toggleButton);
+})();
