8ae16317492511b7984fb998429ce145a8296a5c
Minghua Sun <clairesunstudio@gmail.com>
2021-05-20 11:05:56 -0400

    fix css and add toggle JS

--- docroot/themes/custom/mass_theme/overrides/js/filter-mobile-toggle.js
+++ docroot/themes/custom/mass_theme/overrides/js/filter-mobile-toggle.js
@@ -8,6 +8,19 @@
 (function () {
   'use strict';
 
-  const toggleButton = document.getElementById('filter-toggle');
-  console.log(toggleButton);
+  let expanded = false;
+  const button = document.getElementById('filter-toggle');
+  const filters = document.getElementById('filters')
+  const toggleButton = (exp) => {
+    filters.style.display = exp ? 'block' : 'none';
+    button.innerHTML = exp ? 'Hide Filters' : 'Show Filters';
+    button.setAttribute('aria-expanded', expanded)
+  }
+  toggleButton();
+  button.onclick = () => {
+    console.log('button clicked');
+    expanded = !expanded;
+    toggleButton(expanded);
+  };
+  
 })();
