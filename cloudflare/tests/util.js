2197783f95c0a63e806259ff5de1eb903bac3d0c
Moshe Weitzman <weitzman@tejasa.com>
2021-06-01 15:06:45 -0400

    DP-17765 Update Cloudflare configuration to stop sending legacy prefixes to legacy server (#773)
    * Remove legacy redirect handling from /cloudflare
    
    * Add changelog
    
    * Changelog

--- cloudflare/tests/util.js
+++ cloudflare/tests/util.js
@@ -2,29 +2,11 @@
 import {URL} from 'url'
 import {
   isEditUrl,
-  isLegacyUrl,
   isFileRedirect
 } from '../src/util';
 import makeServiceWorkerMock from "service-worker-mock";
 
 const mockContext = makeServiceWorkerMock();
-
-describe('isLegacyUrl', function() {
-  const tests = [
-    ['https://www.mass.gov/anf', true],
-    ['https://www.mass.gov/abc', false],
-    ['https://www.mass.gov/alerts', false],
-    ['https://www.mass.gov/alerts/foo', false],
-    ['https://www.mass.gov/alert', true],
-    ['https://www.mass.gov/alert/foo', true],
-  ]
-
-  tests.forEach(function(test) {
-    it(`Should treat ${test[0]} as a ${test[1] ? 'legacy' : 'drupal'} url`, function() {
-      expect(isLegacyUrl(new URL(test[0]))).toEqual(test[1]);
-    });
-  });
-});
 
 describe('isEditUrl', function() {
   const tests = [
