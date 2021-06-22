91039de48b0a43729d060c39041dcf0b4710a189
Diego Diestra <ddiestras@gmail.com>
2021-06-22 18:38:29 -0500

    Update tests

--- docroot/modules/custom/mass_alerts/tests/src/ExistingSiteJavascript/SiteWideAlertsClientSideTest.php
+++ docroot/modules/custom/mass_alerts/tests/src/ExistingSiteJavascript/SiteWideAlertsClientSideTest.php
@@ -61,7 +61,6 @@ class SiteWideAlertsClientSideTest extends ExistingSiteWebDriverTestBase {
 
     $jsWebAssert = $this->assertSession();
 
-
     $jsWebAssert->drupalGet('/alerts/sitewide');
     $jsWebAssert->statusCodeEquals(200);
     $jsWebAssert->pageTextContains($node->getTitle());
