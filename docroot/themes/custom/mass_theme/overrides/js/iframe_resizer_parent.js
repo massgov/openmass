5807f2ce822bc4c967193327720978a186890c16
Tom Fleming <tom@lastcallmedia.com>
2021-06-28 10:35:35 -0400

    DP-20892 Responsive iframe height (#605)
    * Added and configured form display of iframe height config field - DP-20892
    
    * Made height field conditional - DP-20892
    
    * Removed height config from paragraph display - DP-20892
    
    * Added iframe resize scripts - DP-20892
    
    * Modified iframe paragraph to assign non-numeric height value to iframe when set to full - DP-20892
    
    * Added changelog - DP-20892
    
    * Cleaned up resizer scripts to fix eslint errors - DP-20892
    
    * Revert "Cleaned up resizer scripts to fix eslint errors - DP-20892"
    
    This reverts commit 5532666fa393c852df1e24ab2b9eae2a74b999af.
    
    * Revert "Revert "Cleaned up resizer scripts to fix eslint errors - DP-20892""
    
    This reverts commit 3ec20f571d46e4c19882e9ec6865193272376210.
    
    * Pushing up more cleaned up version for testing
    
    * Update docroot/themes/custom/mass_theme/overrides/js/iframe_resizer_iframe.js
    
    Co-authored-by: Minghua Sun <clairesunstudio@gmail.com>
    
    * Update docroot/themes/custom/mass_theme/overrides/js/iframe_resizer_parent.js
    
    Co-authored-by: Minghua Sun <clairesunstudio@gmail.com>
    
    * Fixed Twig template - DP-20892
    
    * Updated help text - DP-20892
    
    * Cleaned up parent script - DP-20892
    
    * Fixed script link - DP-20892
    
    * Fixed conditional field logic for existing content - DP-20892
    
    * Moved help text to 100% field - DP-20892
    
    * Added 100% iframe confirmation field to show conditionally - DP-20892
    
    * Updated conditional fields JS, moved setting full height value to preprocess - DP-20892
    
    * Added library - DP-20892
    
    * Addressed some issues affecting paragraph value output
    
    * Fixed setting auto height and library - DP-20892
    
    * Additional adjustments to iframe display and 100% confirmation - DP-20892
    
    * Fixed height setting for iframes - DP-20892
    
    * Added simplified, more es6-like version of resizer - DP-20892
    
    * Added simplified, more es6-like version of resizer - DP-20892
    
    * Modified script to theoretically work with multiple iframes - DP-20892
    
    * Added a little extra padding to prevent scrollbar - DP-20892
    
    * Update iframe_resizer_parent.js
    
    * Update iframe_resizer_iframe.js
    
    * Update iframe_resizer_iframe.js
    
    * lint fixes
    
    * more eslint fix
    
    * fix code
    
    * Update conf/drupal/config/field.storage.paragraph.field_iframe_height_config.yml
    
    Co-authored-by: Minghua Sun <clairesunstudio@gmail.com>
    
    * Fixed script in help text - DP-20892
    
    * word wrap radio form wrappers in the admin theme - DP-20892
    
    * Update docroot/themes/custom/mass_theme/overrides/js/iframe_resizer_iframe.js
    
    * Update conf/drupal/config/field.storage.paragraph.field_iframe_height_config.yml
    
    * Update docroot/themes/custom/mass_theme/overrides/js/iframe_resizer_iframe.js
    
    * Changelog change - DP-20892
    
    * Removed changelog - DP-20892
    
    * Re-added changelog - DP-20892
    
    * renamed changelog - DP-20892
    
    * Removed / from changelog - DP-20892
    
    * Switched changelog to use related issue - DP-20892
    
    * Modified changelog description - DP-20892
    
    * Removed changelog - DP-20892
    
    * Added new changelog - DP-20892
    
    * Added back other changelog - DP-20892
    
    * DP-20892 - Update iframe to handle more events
    
    * DP-20892_iframe-height
    
    * Eslint fi
    
    Co-authored-by: Minghua Sun <clairesunstudio@gmail.com>
    Co-authored-by: Diego Diestra <ddiestras@gmail.com>

---
+++ docroot/themes/custom/mass_theme/overrides/js/iframe_resizer_parent.js
@@ -0,0 +1,70 @@
+/*
+MIT License
+
+Copyright (c) 2019 Jacob Filipp
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+
+// Add this script to the <b>parent page on which your iFrame is embedded</b>
+// This code resizes the iFrame's height in response to a postMessage from the child iFrame
+
+
+
+// event.data - the object that the iframe sent us
+// event.origin - the URL from which the message came
+// event.source - a reference to the 'window' object that sent the message
+function gotResizeMessage(event) {
+  'use strict';
+  var matches = document.querySelectorAll('.js-ma-responsive-iframe iframe'); // iterate through all iFrames on page
+  var i = 0;
+  for (; i < matches.length; i++) {
+    // found the iFrame that sent us a message
+    if (matches[i].contentWindow === event.source) {
+      // matches[i].width = Number( event.data.width )	 <--- we do not do anything with the page width for now
+      matches[i].height = Number(event.data.height);
+      return 1;
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+  // eslint-disable-next-line no-console
+  console.log('loaded parent JS');
+
+  window.addEventListener('message', gotResizeMessage, false);
+
+  if (window.MutationObserver) {
+
+    // Crea una instancia de observer
+    var observer = new MutationObserver(function (mutations) {
+      var matches = document.querySelectorAll('.js-ma-responsive-iframe iframe');
+      var i = 0;
+      for (; i < matches.length; i++) {
+        matches[i].contentWindow.postMessage('update', '*');
+      }
+    });
+
+    var config = {attributes: true};
+    observer.observe(document.body, config);
+  }
+}); // on DOM ready
