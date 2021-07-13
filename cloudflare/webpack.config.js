5b4c883a4fec7d5be378b989be008308551afd83
abaghdasaryan <arthur.baghdasar@gmail.com>
2021-07-13 14:08:29 +0400

    Adding noe env to webpack

--- cloudflare/webpack.config.js
+++ cloudflare/webpack.config.js
@@ -1,20 +1,21 @@
 const webpack = require('webpack');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
 module.exports = {
   entry: "./src/index.js",
-  mode: "development",
+  mode: "production",
   optimization: {
-    minimize: false
+    minimize: true
   },
   performance: {
     hints: "warning"
   },
   plugins: [
+    new NodePolyfillPlugin(),
     // Pull in the CDN verification token from the environment.
     // This will be added as a header to all origin requests to prevent
     // CDN bypass attacks. See scripts/cloudflare-deploy.
     new webpack.DefinePlugin({
-      process: 'process/browser',
       'process.env.MASS_CDN_TOKEN': JSON.stringify(process.env.MASS_CDN_TOKEN),
     })
   ],
