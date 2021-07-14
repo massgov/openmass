29bc32735febdf3c92a29fdef2721f25e503222d
Diego Diestra <ddiestras@gmail.com>
2021-07-14 22:27:25 +0200

    DP-22395 - Refactoring svg to helper

--- docroot/modules/custom/mass_alerts/src/Controller/AlertsController.php
+++ docroot/modules/custom/mass_alerts/src/Controller/AlertsController.php
@@ -12,6 +12,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Render\Renderer;
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Url;
+use Drupal\mayflower\Helper;
 
 /**
  * Defines a route controller for entity query.
@@ -126,22 +127,8 @@ class AlertsController extends ControllerBase implements ContainerInjectionInter
     ];
 
     $output = $this->renderer->renderRoot($build);
+    $this->attachSvg($output);
 
-    preg_match_all("<svg-placeholder path=\"(.*\.svg)\">", $output, $matches);
-    if (!empty($matches[1])) {
-      foreach (array_unique(array_filter($matches[1])) as $path) {
-        $replacement = '';
-        if ($svgNode = $this->getSvg($path)) {
-          $hash = md5($path);
-          $svgNode->setAttribute('id', $hash);
-          $replacement = $this->getEmbed($hash);
-          $inlined[] = $this->getSource($hash, $svgNode);
-        }
-        $output = str_replace(sprintf('<svg-placeholder path="%s">', $path), $replacement, $output);
-      }
-    }
-
-    $output .= $this->wrapInlinedSvgs($inlined);
     $response = new CacheableResponse($output);
 
     if ($node) {
@@ -151,95 +138,6 @@ class AlertsController extends ControllerBase implements ContainerInjectionInter
     $response->addCacheableDependency(CacheableMetadata::createFromRenderArray($build));
     $response->setMaxAge(60);
     return $response;
-  }
-
-  /**
-   * Return the HTML to SVG source.
-   *
-   * An icon unit is wrapped with <symbol> to add structure and semantics
-   * to it, which promotes accessibility.
-   *
-   * <title> and <desc> tags can be added within the <symbol> for
-   * accessibility, but in our case, the svg icons are decorative,
-   * and they are not necessary.
-   * Ones used for linked images are handled their accessibility
-   * treatment with their parent <a>.
-   *
-   * The viewBox can be defined on the <symbol>, so you don't need to use it
-   * in the markup (easier and less error prone).
-   * Symbols don't display as you define them, so no need for a <defs> block.
-   */
-  private function getSource($hash, \DOMElement $sourceNode) {
-    $symbol = $sourceNode->ownerDocument->createElementNS($sourceNode->namespaceURI, 'symbol');
-
-    // Copy attributes from <svg> to <symbol>.
-    /** @var \DOMAttr $attribute */
-    foreach ($sourceNode->attributes as $attribute) {
-      $symbol->setAttribute($attribute->name, $attribute->value);
-    }
-
-    // Set an explicit ID.
-    $symbol->setAttribute('id', $hash);
-
-    // Copy all child nodes from the SVG to the symbol.
-    // This has to be a double loop due to an issue with DOMNodeList.
-    // @see http://php.net/manual/en/domnode.appendchild.php#121829
-    foreach ($sourceNode->childNodes as $node) {
-      $children[] = $node;
-    }
-
-    foreach ($children as $child) {
-      $symbol->appendChild($child);
-    }
-
-    return $sourceNode->ownerDocument->saveXML($symbol);
-  }
-
-  /**
-   * Load a single SVG as a DOMElement.
-   *
-   * @return \DOMElement|null
-   *   The SVG's DOMElement, or null if the SVG file was not found.
-   */
-  private function getSvg($path) {
-    // Make sure the file exists before trying to fetch it and parse it as an
-    // XML document.
-    if (!file_exists($path)) {
-      trigger_error(sprintf('Not a valid file: "%s"', $path), E_USER_DEPRECATED);
-      return;
-    }
-    // For security reasons, we don't want to allow anything but an .svg file
-    // to be included this way.
-    if (!pathinfo($path, PATHINFO_EXTENSION) === 'svg') {
-      trigger_error(sprintf('Invalid SVG file: "%s"', $path), E_USER_WARNING);
-      return;
-    }
-    if ($svg = file_get_contents($path)) {
-      $doc = new \DOMDocument('1.0', 'UTF-8');
-      if ($doc->loadXML($svg)) {
-        return $doc->firstChild;
-      }
-      // No need to error_log here. \DomDocument will log for us.
-    }
-
-  }
-
-  /**
-   * Wrap an array of SVG strings with a div that hides them from display.
-   */
-  private function wrapInlinedSvgs(array $inlineSvgs) {
-    if ($inlineSvgs) {
-      // All icons can be wrapped in one <svg>.
-      return sprintf('<svg xmlns="http://www.w3.org/2000/svg" style="display: none">%s</svg>', implode('', $inlineSvgs));
-    }
-    return '';
-  }
-
-  /**
-   * Return the HTML to reference an SVG.
-   */
-  private function getEmbed($hash) {
-    return sprintf('<svg aria-hidden="true" focusable="false"><use xlink:href="#%s"></use></svg>', $hash);
   }
 
   /**
@@ -325,8 +223,8 @@ class AlertsController extends ControllerBase implements ContainerInjectionInter
 
         $alert = [
           'id' => $id,
-          'accordion' => true,
-          'isExpanded' => false,
+          'accordion' => TRUE,
+          'isExpanded' => FALSE,
           'accordionLabel' => $this->t('Expand @label', ['@label' => $label]),
           'icon' => $icon,
           'title' => $label,
@@ -369,22 +267,7 @@ class AlertsController extends ControllerBase implements ContainerInjectionInter
     ];
 
     $output = $this->renderer->renderRoot($build);
-
-    preg_match_all("<svg-placeholder path=\"(.*\.svg)\">", $output, $matches);
-    if (!empty($matches[1])) {
-      foreach (array_unique(array_filter($matches[1])) as $path) {
-        $replacement = '';
-        if ($svgNode = $this->getSvg($path)) {
-          $hash = md5($path);
-          $svgNode->setAttribute('id', $hash);
-          $replacement = $this->getEmbed($hash);
-          $inlined[] = $this->getSource($hash, $svgNode);
-        }
-        $output = str_replace(sprintf('<svg-placeholder path="%s">', $path), $replacement, $output);
-      }
-    }
-
-    $output .= $this->wrapInlinedSvgs($inlined);
+    $this->attachSvg($output);
 
     $response = new CacheableResponse($output);
     foreach ($nodes as $node) {
@@ -394,4 +277,25 @@ class AlertsController extends ControllerBase implements ContainerInjectionInter
     return $response;
   }
 
+  private function attachSvg(&$content) {
+
+    $svgs = Helper::findSvg($content);
+
+    if ($svgs) {
+      foreach ($svgs as $path) {
+        $replacement = '';
+        if ($svgNode = Helper::getSvg($path)) {
+          $hash = md5($path);
+          $svgNode->setAttribute('id', $hash);
+          $replacement = Helper::getSvgEmbed($hash);
+          $inlined[] = Helper::getSvgSource($hash, $svgNode);
+        }
+        $content = str_replace(sprintf('<svg-placeholder path="%s">', $path), $replacement, $content);
+      }
+    }
+
+    $content .= Helper::wrapInlinedSvgs($inlined);
+  }
+
 }
+
