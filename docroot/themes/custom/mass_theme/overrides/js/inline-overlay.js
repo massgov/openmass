cef027399b77a5bf1cea2409e10f1bb5c9df489c
Yukiko Gannett <yukikogannett@gmail.com>
2021-07-20 13:37:53 -0400

    clean up the code to simplify
    resolve merge conflict

--- docroot/themes/custom/mass_theme/overrides/js/inline-overlay.js
+++ docroot/themes/custom/mass_theme/overrides/js/inline-overlay.js
@@ -6,29 +6,28 @@
   var lastFocusableElement = tocFocusableElements[tocFocusableElements.length - 1];
   var tocButtons = $('.ma__toc--hierarchy__container .js-accordion-link');
   var tocLastButton = tocButtons[tocButtons.length - 1];
+  var lastContainer = $(tocLastButton).closest('.ma__toc--hierarchy__accordion');
 
   $(tocLastButton).keydown(function (e) {
-    var key = (e.keyCode ? e.keyCode : e.witch);
-    var lastContainer = $(tocLastButton).closest('.ma__toc--hierarchy__accordion');
-
+    var key = e.keyCode || e.which;
     if (!lastContainer.hasClass('is-open')) {
-      // When tab key is hit on the last ficusable element,
-      if (key === '9') {
+      // When tab key is hit on the last focusable element,
+      if (key === 9) {
         // Set focus on the first focusable element in the overlay.
         firstFocusableElement.focus();
       }
     }
-
-    // When the sub menu is open.
-    if (lastContainer.hasClass('is-open')) {
-      $(lastFocusableElement).keydown(function (e) {
-        var key = (e.keyCode ? e.keyCode : e.witch);
-        // When tab key is hit on the last ficusable element,
-        if (key === '9') {
-          // Set focus on the first focusable element in the overlay.
-          firstFocusableElement.focus();
-        }
-      });
-    }
   });
+
+  // When the sub menu is open.
+  if (lastContainer.hasClass('is-open')) {
+    $(lastFocusableElement).keydown(function (e) {
+      var key = e.keyCode || e.which;
+      // When tab key is hit on the last focusable element,
+      if (key === 9) {
+        // Set focus on the first focusable element in the overlay.
+        firstFocusableElement.focus();
+      }
+    });
+  }
 })(jQuery);
