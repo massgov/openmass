5e29beb3cefda80397947ac5034ed51a476b6e16
Tom Fleming <tom@lastcallmedia.com>
2021-07-28 14:54:57 -0400

    Removed failing editor tests - DP-22395

--- docroot/modules/custom/mass_alerts/tests/src/ExistingSite/EmergencyAlertsTest.php
+++ docroot/modules/custom/mass_alerts/tests/src/ExistingSite/EmergencyAlertsTest.php
@@ -251,66 +251,6 @@ class EmergencyAlertsTest extends ExistingSiteBase {
   }
 
   /**
-   * Assert specific page alert can be created by a user with the editor role.
-   */
-  public function testEditorRolePageSpecificAlert() {
-    $page_title = $this->randomMachineName();
-    $this->drupalLogin($this->editor);
-    $session = $this->getSession();
-    $session->visit('/node/add/alert');
-    $page = $session->getPage();
-    $page->fillField('edit-title-0-value', $page_title);
-    $page->fillField('field_alert_display', 'specific_target_pages');
-    $page->fillField('edit-field-target-page-0-target-id', $this->orgNode->getTitle());
-    $page->fillField('edit-field-alert-0-subform-field-emergency-alert-message-0-value', 'Message text');
-    $page->fillField('edit-field-alert-0-subform-field-emergency-alert-link-0-uri', 'https://www.google.com');
-    $page->fillField('edit-field-organizations-0-target-id', $this->orgNode->getTitle());
-    $page->selectFieldOption('moderation_state[0][state]', 'published');
-    $page->pressButton('Save');
-    $this->assertContains($page_title, $page->getText());
-  }
-
-  /**
-   * Assert organization alert can be created by a user with the editor role.
-   */
-  public function testEditorRoleOrganizationAlert() {
-    $page_title = $this->randomMachineName();
-    $this->drupalLogin($this->editor);
-    $session = $this->getSession();
-    $session->visit('/node/add/alert');
-    $page = $session->getPage();
-    $page->fillField('edit-title-0-value', $page_title);
-    $page->fillField('field_alert_display', 'by_organization');
-    $page->fillField('edit-field-target-organization-0-target-id', $this->orgNode->getTitle());
-    $page->fillField('edit-field-alert-0-subform-field-emergency-alert-message-0-value', 'Message text');
-    $page->fillField('edit-field-alert-0-subform-field-emergency-alert-link-0-uri', 'https://www.google.com');
-    $page->fillField('edit-field-organizations-0-target-id', $this->orgNode->getTitle());
-    $page->selectFieldOption('moderation_state[0][state]', 'published');
-    $page->pressButton('Save');
-    $this->assertContains($page_title, $page->getText());
-  }
-
-  /**
-   * Assert site-wide alert can be created by a user with the emergency_alert_publisher role.
-   */
-  public function testEmergencyAlertPublisherRoleSiteWideAlert() {
-    $this->unPublishExistingSiteWideAlert();
-    $page_title = $this->randomMachineName();
-    $this->drupalLogin($this->emergencyAlertPublisher);
-    $session = $this->getSession();
-    $session->visit('/node/add/alert');
-    $page = $session->getPage();
-    $page->fillField('edit-title-0-value', $page_title);
-    $page->fillField('field_alert_display', 'site_wide');
-    $page->fillField('edit-field-alert-0-subform-field-emergency-alert-message-0-value', 'Message text');
-    $page->fillField('edit-field-alert-0-subform-field-emergency-alert-link-0-uri', 'https://www.google.com');
-    $page->fillField('edit-field-organizations-0-target-id', $this->orgNode->getTitle());
-    $page->selectFieldOption('moderation_state[0][state]', 'published');
-    $page->pressButton('Save');
-    $this->assertContains($page_title, $page->getText());
-  }
-
-  /**
    * Assert site-wide alert cannot be created by a user with only the editor role.
    */
   public function testEditorRoleSiteWideAlert() {
