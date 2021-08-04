5d0b4e023964c9dd1aa753ab4a85c15e29f264dc
Tom Fleming <tom@lastcallmedia.com>
2021-08-04 10:15:41 -0400

    Removed JSONAPI test - DP-22653

--- docroot/modules/custom/mass_alerts/tests/src/ExistingSite/EmergencyAlertsTest.php
+++ docroot/modules/custom/mass_alerts/tests/src/ExistingSite/EmergencyAlertsTest.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\Tests\mass_alerts\ExistingSite;
 
-use Drupal\Core\Datetime\DrupalDateTime;
-use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
 use Drupal\dynamic_page_cache\EventSubscriber\DynamicPageCacheSubscriber;
 use Drupal\mass_content_moderation\MassModeration;
 use Drupal\node\Entity\Node;
@@ -47,39 +45,6 @@ class EmergencyAlertsTest extends ExistingSiteBase {
       'status' => 1,
       'moderation_state' => 'published',
     ]);
-  }
-
-  /**
-   * Check that the JSONAPI output contains fields we care about.
-   */
-  public function testEmergencyAlertAppearsInJson() {
-    $related = $this->createNode([
-      'type' => 'service_page',
-      'title' => 'Alert Service Page',
-      'moderation_state' => MassModeration::PUBLISHED,
-    ]);
-    $timestamp = new DrupalDateTime('now');
-    $node = $this->createNode([
-      'type' => 'alert',
-      'title' => 'Test Alert',
-      'status' => 1,
-      'field_alert_related_links_5' => [
-        'uri' => 'entity:node/' . $related->id(),
-        'title' => 'Test Alert',
-      ],
-      'field_alert_node_timestamp' => [
-        'value' => $timestamp->format(DateTimeItemInterface::DATETIME_STORAGE_FORMAT),
-      ],
-      'moderation_state' => MassModeration::PUBLISHED,
-    ]);
-
-    $session = $this->getSession();
-    $session->visit('/jsonapi/node/alert?filter[title][value]=Test%20Alert&sort=-changed');
-    $response = json_decode($session->getPage()->getContent(), TRUE);
-    $alert = $this->findNodeInResponse($node, $response);
-    $this->assertEquals($node->toUrl()->toString(), $alert['attributes']['entity_url'], 'Alert has entity_url attribute with aliased path.');
-    $this->assertCount(1, $alert['attributes']['field_alert_related_links_5']);
-    $this->assertEquals($related->toUrl()->toString(), $alert['attributes']['field_alert_related_links_5'][0]['uri'], 'Related link field contains links that point to the aliased entity.');
   }
 
   /**
