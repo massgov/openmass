2cb8ef06ae3ca80a83d888a4dbaebb0195f07948
April Sides <weekbeforenext@gmail.com>
2021-09-30 13:03:27 -0400

    DP-22007: Adding a deploy function to the mass_entity_usage module

---
+++ docroot/modules/custom/mass_entity_usage/mass_entity_usage.deploy.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Implementations of hook_deploy_NAME() for Mass Entity Usage.
+ */
+
+/**
+ * Run the entity_usage recreate drush command.
+ *
+ * Create all entity usage statistics.
+ */
+function mass_entity_usage_deploy_run_entity_usage_command() {
+  ini_set('memory_limit', '1024M');
+  $command = \Drupal::service('entity_usage.commands');
+  $command->recreate();
+}
