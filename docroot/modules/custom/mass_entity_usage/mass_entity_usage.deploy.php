58e686d024dddf59c8258d7865984f3e12f50526
April Sides <weekbeforenext@gmail.com>
2021-10-01 10:52:24 -0400

    DP-22007: Changing the memory limit to 2048 for the entity_usage deploy function

--- docroot/modules/custom/mass_entity_usage/mass_entity_usage.deploy.php
+++ docroot/modules/custom/mass_entity_usage/mass_entity_usage.deploy.php
@@ -11,7 +11,7 @@
  * Create all entity usage statistics.
  */
 function mass_entity_usage_deploy_run_entity_usage_command() {
-  ini_set('memory_limit', '-1');
+  ini_set('memory_limit', '2048');
   $command = \Drupal::service('entity_usage.commands');
   $command->recreate(['use-queue' => TRUE, 'multi-pass' => FALSE, 'clear-multi-pass' => FALSE]);
 }
