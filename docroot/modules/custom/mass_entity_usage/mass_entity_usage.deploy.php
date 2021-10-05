43815e98bfd5fedbe38d439b0f1dd227a753c43a
April Sides <weekbeforenext@gmail.com>
2021-10-04 21:04:55 -0400

    DP-22007: Removing the entity_usage recreate() flags to see if it will run without them

--- docroot/modules/custom/mass_entity_usage/mass_entity_usage.deploy.php
+++ docroot/modules/custom/mass_entity_usage/mass_entity_usage.deploy.php
@@ -11,7 +11,6 @@
  * Create all entity usage statistics.
  */
 function mass_entity_usage_deploy_run_entity_usage_command() {
-  ini_set('memory_limit', '2048');
   $command = \Drupal::service('entity_usage.commands');
-  $command->recreate(['use-queue' => TRUE, 'multi-pass' => FALSE, 'clear-multi-pass' => FALSE]);
+  $command->recreate();
 }
