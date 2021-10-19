013337e1efce33509ae3565c6cf7ad75cd42d5a7
April Sides <weekbeforenext@gmail.com>
2021-10-19 10:47:42 -0400

    DP-22007: Updating entity_usage patch and custom overrides

--- docroot/modules/custom/mass_entity_usage/src/Controller/MassLocalTaskUsageController.php
+++ docroot/modules/custom/mass_entity_usage/src/Controller/MassLocalTaskUsageController.php
@@ -2,41 +2,22 @@
 
 namespace Drupal\mass_entity_usage\Controller;
 
-use Drupal\block_content\BlockContentInterface;
-use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Entity\EntityFieldManagerInterface;
-use Drupal\Core\Entity\EntityInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\RevisionableInterface;
 use Drupal\Core\Language\LanguageInterface;
-use Drupal\Core\Link;
 use Drupal\Core\Routing\RouteMatchInterface;
-use Drupal\Core\StringTranslation\TranslatableMarkup;
-use Drupal\entity_usage\Controller\LocalTaskUsageController;
-use Drupal\entity_usage\EntityUsageInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Pager\PagerManagerInterface;
+use Drupal\entity_usage\Controller\LocalTaskUsageSubQueryController;
 
 /**
  * Controller for our pages.
  */
-class MassLocalTaskUsageController extends LocalTaskUsageController {
+class MassLocalTaskUsageController extends LocalTaskUsageSubQueryController {
 
   /**
    * {@inheritdoc}
    */
-  public function listUsageLocalTask(RouteMatchInterface $route_match) {
-    $entity = $this->getEntityFromRouteMatch($route_match);
-    return $this->listUsagePage($entity->getEntityTypeId(), $entity->id());
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function listUsagePage($entity_type, $entity_id) {
+  public function listUsagePageSubQuery($entity_type, $entity_id) {
     $this->loadEntity($entity_type, $entity_id);
-    $total = $this->getRowsCount();
+    $total = $this->getSubQueryRowsCount();
     if (!$total) {
       return [
         '#markup' => $this->t('No pages link here.'),
@@ -53,7 +34,7 @@ class MassLocalTaskUsageController extends LocalTaskUsageController {
 
     $pager = $this->pagerManager->createPager($total, $this->itemsPerPage);
     $page = $pager->getCurrentPage();
-    $page_rows = $this->getPageRows($page, $this->itemsPerPage);
+    $page_rows = $this->getSubQueryRows($page, $this->itemsPerPage);
 
     $build[] = [
       '#theme' => 'table',
@@ -73,12 +54,8 @@ class MassLocalTaskUsageController extends LocalTaskUsageController {
   /**
    * {@inheritdoc}
    */
-  protected function getPageRows($page, $num_per_page) {
-    $offset = $page * $num_per_page;
-    $entity_types = $this->entityTypeManager->getDefinitions();
-    $languages = $this->languageManager()->getLanguages(LanguageInterface::STATE_ALL);
-    $all_usages = $this->entityUsage->listSources($this->entity, TRUE, $offset);
-    foreach ($all_usages as $source_type => $ids) {
+  public function prepareRows($usages) {
+    foreach ($usages as $source_type => $ids) {
       $type_storage = $this->entityTypeManager->getStorage($source_type);
       foreach ($ids as $source_id => $records) {
         // We will show a single row per source entity. If the target is not
@@ -118,24 +95,9 @@ class MassLocalTaskUsageController extends LocalTaskUsageController {
           $field_label,
           $published,
         ];
-        // Capture the title text in an array for sorting.
-        switch (gettype($link)) {
-          case 'string':
-            $row_link_text[] = trim($link);
-            break;
-
-          case 'object':
-            if ($link instanceof Link) {
-              $row_link_text[] = trim($link->getText());
-            }
-            elseif ($link instanceof TranslatableMarkup) {
-              $row_link_text[] = trim($link->__toString());
-            }
-            break;
-        }
       }
     }
-    // Sort the array by title text.
+
     return $rows;
   }
 
