8209f8a9c556ff414631f84b7f2528821916f819
beto-aveiga <luis.aveiga@gmail.com>
2021-11-03 11:49:23 -0500

    DP-23222 fix alerts positionning for page alerts

--- docroot/modules/custom/mass_alerts/js/alerts-block.js
+++ docroot/modules/custom/mass_alerts/js/alerts-block.js
@@ -2,6 +2,8 @@
 function alerts(path, nodeType, $alertsBlock) {
   'use strict';
   var removeContainer = false;
+  var positioned = false;
+  var alertPositionInterval = null;
 
   function insertBefore(nodeA, nodeBselector) {
     var nodeB = document.querySelector(nodeBselector);
@@ -10,51 +12,72 @@ function alerts(path, nodeType, $alertsBlock) {
 
   function insertAfter(nodeA, nodeBselector) {
     var nodeB = document.querySelector(nodeBselector);
-    document.getElementById(nodeA).insertAdjacentElement('afterend', nodeB);
+    nodeB.insertAdjacentElement('afterend', nodeA);
   }
 
-  if (path !== '/alerts/sitewide') {
-    if (nodeType) {
-      var positioned = false;
 
-      if (nodeType === 'how_to_page') {
-        if (document.querySelector('.mdocument.querySelector__page-header__optional-content') != null) {
-          insertBefore($alertsBlock, '.ma__page-header__optional-content');
-          removeContainer = true;
-          positioned = true;
-        }
-      }
-      else if (nodeType === 'person') {
-        if (document.querySelector('.ma__page-intro') != null) {
-          insertAfter($alertsBlock, '.ma__page-intro');
-          removeContainer = true;
-          positioned = true;
-        }
-      }
-
-      if (!positioned) {
-
-        if (document.querySelector('.ma__illustrated-header') != null) {
-          insertAfter($alertsBlock, '.ma__illustrated-header');
-        }
-        else if (document.querySelector('.ma__page-header') != null) {
-          insertAfter($alertsBlock, '.ma__page-header');
-        }
-        else if (document.querySelector('.ma__organization-navigation') != null) {
-          insertAfter($alertsBlock, '.ma__organization-navigation');
-        }
-        else if (document.querySelector('.ma__page-banner') != null) {
-          insertAfter($alertsBlock, '.ma__page-banner');
-        }
-        else if (document.querySelector('.pre-content') != null) {
-          insertAfter($alertsBlock, '.pre-content');
-        }
-      }
-    }
-    else {
-      // Not a node page.
+  function positionAlert() {
+    if (!nodeType) {
       path = false;
+      return;
     }
+
+    if (nodeType === 'how_to_page') {
+      if (document.querySelector('.mdocument.querySelector__page-header__optional-content') != null) {
+        insertBefore($alertsBlock, '.ma__page-header__optional-content');
+        removeContainer = true;
+        positioned = true;
+      }
+    }
+    else if (nodeType === 'person') {
+      if (document.querySelector('.ma__page-intro') != null) {
+        insertAfter($alertsBlock, '.ma__page-intro');
+        removeContainer = true;
+        positioned = true;
+      }
+    }
+
+    if (positioned) {
+      return;
+    }
+
+    if (document.querySelector('.ma__illustrated-header') != null) {
+      insertAfter($alertsBlock, '.ma__illustrated-header');
+      positioned = true;
+    }
+    else if (document.querySelector('.ma__page-header') != null) {
+      insertAfter($alertsBlock, '.ma__page-header');
+      positioned = true;
+    }
+    else if (document.querySelector('.ma__organization-navigation') != null) {
+      insertAfter($alertsBlock, '.ma__organization-navigation');
+      positioned = true;
+    }
+    else if (document.querySelector('.ma__page-banner') != null) {
+      insertAfter($alertsBlock, '.ma__page-banner');
+      positioned = true;
+    }
+    else if (document.querySelector('.pre-content') != null) {
+      insertAfter($alertsBlock, '.pre-content');
+      positioned = true;
+    }
+  }
+
+  var alertPositionIntervalMs = 100;
+
+  if (path !== '/alerts/sitewide') {
+    // This is faster than listen DOMContentLoaded.
+    alertPositionInterval = setInterval(function () {
+      positionAlert();
+      if (positioned) {
+        clearInterval(alertPositionInterval);
+      }
+    }, alertPositionIntervalMs);
+
+    // Avoid interval to run forever, in case the logic fails.
+    document.addEventListener('DOMContentLoaded', function () {
+      setTimeout(function () { clearInterval(alertPositionInterval); }, alertPositionIntervalMs);
+    });
   }
 
   function processData(content) {
