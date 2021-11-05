f40ecac5128994e3d8fed5dbfa4205a5431311bb
Moshe Weitzman <weitzman@tejasa.com>
2021-11-05 13:58:32 -0400

    Omit dbmass, et al

---
+++ scripts/pulldb.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Pull database from CircleCI artifacts
+ */
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+//Get recent builds
+
+//Identify right populate job and fetch its artifacts.
