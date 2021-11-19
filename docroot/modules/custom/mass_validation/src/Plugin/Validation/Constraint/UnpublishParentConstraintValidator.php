88ab47a3a3ff923e528bbb9257a3c341551a049d
April Sides <weekbeforenext@gmail.com>
2021-11-18 20:49:21 -0500

    DP-23302: Adding validation to check for children before unpublishing or trashing a node

---
+++ docroot/modules/custom/mass_validation/src/Plugin/Validation/Constraint/UnpublishParentConstraintValidator.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\mass_validation\Plugin\Validation\Constraint;
+
+use Drupal\Core\StringTranslation\PluralTranslatableMarkup;
+use Drupal\entity_hierarchy\Information\ChildEntityWarningBuilder;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * Checks if an entity has a parent before unpublishing.
+ */
+class UnpublishParentConstraintValidator extends ConstraintValidator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate($entity, Constraint $constraint) {
+    if (!isset($entity)) {
+      return;
+    }
+
+    $moderation_state = $entity->get('moderation_state')->value;
+    $unpublished_states = ['unpublished', 'trash'];
+    if (in_array($moderation_state, $unpublished_states)) {
+      if ($children = \Drupal::service('class_resolver')
+        ->getInstanceFromDefinition(ChildEntityWarningBuilder::class)
+        ->buildChildEntityWarnings($entity)) {
+        foreach ($children as $child) {
+          $items = $child->getList()['#items'];
+          if (!empty($items)) {
+            $items_string = implode(', ', $items);
+            $message = new PluralTranslatableMarkup(
+              count($items),
+              $constraint->message . ' 1 child: ' . $items_string,
+              $constraint->message . ' @count children: ' . $items_string);
+            $this->context->addViolation($message);
+          }
+        }
+      }
+    }
+  }
+
+}
