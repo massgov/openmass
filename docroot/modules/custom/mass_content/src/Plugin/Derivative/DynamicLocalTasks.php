c1da02000c2e98d5e227a82afce591434bd628f5
Tom Fleming <tom@lastcallmedia.com>
2021-11-24 16:55:08 -0500

    Added derivative for views tab - DP-23356

---
+++ docroot/modules/custom/mass_content/src/Plugin/Derivative/DynamicLocalTasks.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\mass_content\Plugin\Derivative;
+
+use Drupal\Component\Plugin\Derivative\DeriverBase;
+
+/**
+  * Defines dynamic local tasks.
+  */
+class DynamicLocalTasks extends DeriverBase {
+
+  /**
+  * {@inheritdoc}
+  */
+  public function getDerivativeDefinitions($base_plugin_definition) {
+    // Implement dynamic logic to provide values for the same keys as in example.links.task.yml.
+    $this->derivatives['mass_content.change_parents'] = $base_plugin_definition;
+    $this->derivatives['mass_content.change_parents']['title'] = "Move Children";
+    $this->derivatives['mass_content.change_parents']['route_name'] = 'view.change_parents.page_1';
+    $this->derivatives['mass_content.change_parents']['base_route'] = 'entity.node.canonical';
+
+    return parent::getDerivativeDefinitions($base_plugin_definition);
+  }
+
+}
