f7adef1ab0ab631eb7f566113d4fc0b67abb3b5e
beto-aveiga <luis.aveiga@gmail.com>
2021-11-26 11:50:37 -0500

    DP-23356 mass_admin_theme on change_parents view

---
+++ docroot/modules/custom/mass_admin_pages/src/Theme/ThemeNegotiator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\mass_admin_pages\Theme;
+
+use Drupal\Core\Routing\RouteMatchInterface;
+use Symfony\Component\Routing\Route;
+use Drupal\Core\Theme\ThemeNegotiatorInterface;
+
+/**
+ * A basic Theme Negotiator.
+ */
+class ThemeNegotiator implements ThemeNegotiatorInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    $route = $route_match->getRouteObject();
+    if (!$route instanceof Route) {
+      return FALSE;
+    }
+
+    $routes_to_show_admin_theme = [
+      '/node/{node}/move-children',
+    ];
+
+    return in_array($route->getPath(), $routes_to_show_admin_theme);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function determineActiveTheme(RouteMatchInterface $route_match) {
+    return 'mass_admin_theme';
+  }
+}
