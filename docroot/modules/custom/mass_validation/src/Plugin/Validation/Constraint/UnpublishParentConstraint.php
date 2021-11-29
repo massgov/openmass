40902c8fe59b03ae921e6efe6644cf696bfa1fef
April Sides <april.sides@lullabot.com>
2021-11-29 14:03:00 -0500

    DP-23302: Prevent unpublish of a page with published children (#1176)
    * DP-23302: Adding validation to check for children before unpublishing or trashing a node
    
    * DP-23302: Adding links to the unpublish parent validation message and tests

---
+++ docroot/modules/custom/mass_validation/src/Plugin/Validation/Constraint/UnpublishParentConstraint.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\mass_validation\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Entity Reference valid reference constraint.
+ *
+ * Verifies that referenced entities are valid.
+ *
+ * @Constraint(
+ *   id = "UnpublishParent",
+ *   label = @Translation("Unpublish parent without children", context = "Validation")
+ * )
+ */
+class UnpublishParentConstraint extends Constraint {
+
+  /**
+   * The default violation message.
+   *
+   * @var string
+   */
+  public $message = 'This content cannot be unpublished or trashed because it is a parent of ';
+
+}
