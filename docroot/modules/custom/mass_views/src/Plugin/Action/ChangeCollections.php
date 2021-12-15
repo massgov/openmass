552e2b025b5bd10bdd6fae00f8d6ac6717d7d420
abaghdasaryan <arthur.baghdasar@gmail.com>
2021-12-15 11:30:51 +0400

    Initial commit with view and custom action

--- docroot/modules/custom/mass_views/src/Plugin/Action/ChangeCollections.php
+++ docroot/modules/custom/mass_views/src/Plugin/Action/ChangeCollections.php
@@ -121,6 +121,7 @@ class ChangeCollections extends ViewsBulkOperationsActionBase {
       '#start_minimized' => TRUE,
       '#title' => $this->t('New Collection'),
       '#required' => TRUE,
+      '#attributes' => ['class' => ['field--widget-term-reference-tree']]
     ];
     return $form;
   }
