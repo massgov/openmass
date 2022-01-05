0d6f41fe90205c85f5ccef010480b0a0f66188ab
beto-aveiga <luis.aveiga@gmail.com>
2022-01-05 17:32:56 -0500

    DP-23763 remove unpublished from the validation

--- docroot/modules/custom/mass_validation/src/Information/MassChildEntityWarningBuilder.php
+++ docroot/modules/custom/mass_validation/src/Information/MassChildEntityWarningBuilder.php
@@ -5,11 +5,29 @@ namespace Drupal\mass_validation\Information;
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\entity_hierarchy\Information\ChildEntityWarningBuilder;
+use Drupal\mass_content_moderation\MassModeration;
+use Drupal\node\Entity\Node;
 
 /**
  * Defines a class for building a list of child entity warnings.
  */
 class MassChildEntityWarningBuilder extends ChildEntityWarningBuilder {
+
+  /**
+   * Remove children that are not published.
+   */
+  private function removeUnpublished($children) {
+    foreach ($children as $key => $child) {
+      $nid = $child->getId();
+      $node = Node::load($nid);
+      $state = $node->moderation_state[0]->value;
+
+      if ($state != MassModeration::PUBLISHED) {
+        unset($children[$key]);
+      }
+    }
+    return $children;
+  }
 
   /**
    * Gets warning about child entities before deleting a parent.
@@ -20,7 +38,7 @@ class MassChildEntityWarningBuilder extends ChildEntityWarningBuilder {
    * @return \Drupal\mass_validation\Information\MassChildEntityWarning[]
    *   Array of warning value objects.
    */
-  public function buildChildEntityWarnings(ContentEntityInterface $parent) {
+  public function buildChildEntityWarnings(ContentEntityInterface $parent, $removeUnpublished = FALSE) {
     $return = [];
     if ($fields = $this->parentCandidate->getCandidateFields($parent)) {
       $cache = new CacheableMetadata();
@@ -29,6 +47,7 @@ class MassChildEntityWarningBuilder extends ChildEntityWarningBuilder {
         $storage = $this->nestedSetStorageFactory->get($field_name, $parent->getEntityTypeId());
         $nodeKey = $this->nodeKeyFactory->fromEntity($parent);
         $children = $storage->findChildren($nodeKey);
+        $children = !$removeUnpublished ? $children : $this->removeUnpublished($children);
         if ($parent_node = $storage->findParent($nodeKey)) {
           $children[] = $parent_node;
         }
