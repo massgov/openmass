58a12a1d66e760c6e820cfb09b8dfaccf1d93b9b
Moshe Weitzman <weitzman@tejasa.com>
2022-01-12 14:17:07 -0500

    Add bundle classes for Media entity types

---
+++ docroot/modules/custom/mass_media/src/Entity/Bundle/MediaBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\mass_media\Entity\Bundle;
+
+use Drupal\media\Entity\Media;
+
+/**
+ * A base bundle class for media entities.
+ */
+abstract class MediaBundle extends Media {
+
+}
---
+++ docroot/modules/custom/mass_media/src/Entity/Bundle/VideoBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\mass_media\Entity\Bundle;
+
+/**
+ * A bundle class for media entities.
+ */
+class VideoBundle extends MediaBundle {
+
+}
