fb3808cc62e5426949cb93f17654331b2d44adc9
Moshe Weitzman <weitzman@tejasa.com>
2022-02-02 13:16:17 -0500

    Add a bundle class for each node,media,user type

---
+++ docroot/modules/custom/mass_content/src/Entity/Bundle/media/MediaBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\mass_content\Entity\Bundle\media;
+
+use Drupal\mass_content_moderation\MassModerationAwareInterface;
+use Drupal\mass_content_moderation\MassModerationTrait;
+use Drupal\media\Entity\Media;
+
+/**
+ * A base bundle class for media entities.
+ */
+abstract class MediaBundle extends Media implements MassModerationAwareInterface {
+  use MassModerationTrait;
+}
---
+++ docroot/modules/custom/mass_content/src/Entity/Bundle/user/UserBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\mass_content\Entity\Bundle\user;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\user\Entity\User;
+
+/**
+ * A bundle class for user entities.
+ */
+class UserBundle extends User {
+  /**
+   * Get user's organization.
+   */
+  public function getOrg(): FieldItemListInterface {
+    return $this->get('field_user_org');
+  }
+}
