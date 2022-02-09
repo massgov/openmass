c703b307910b2f3ba1c4e0b08bddf9f15b467041
beto-aveiga <luis.aveiga@gmail.com>
2022-02-09 13:31:17 -0500

    DP-23222 less spaces

--- docroot/modules/custom/mass_alerts/js/alerts-block.js
+++ docroot/modules/custom/mass_alerts/js/alerts-block.js
@@ -216,20 +216,13 @@ var jQueryLike = function (elemOrSelector, context) {
 
           // https://daily-dev-tips.com/posts/vanilla-javascript-cookies/
           var getCookie = function (name) {
-            // Add the = sign
             name = name + '=';
-
-            // Get the decoded cookie
             var decodedCookie = decodeURIComponent(document.cookie);
-
             // Get all cookies, split on ; sign
             var cookies = decodedCookie.split(';');
-
             // Loop over the cookies
             for (var i = 0; i < cookies.length; i++) {
-              // Define the single cookie, and remove whitespace
               var cookie = cookies[i].trim();
-
               // If this cookie has the name of what we are searching
               if (cookie.indexOf(name) === 0) {
                 // Return everything after the cookies name
