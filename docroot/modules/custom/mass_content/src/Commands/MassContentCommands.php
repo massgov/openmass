eec33a3cf910ceaf3a2aa16bcf20cb1bda6390b1
abaghdasaryan <arthur.baghdasar@gmail.com>
2022-03-15 21:10:33 +0400

    Drush command with argument

--- docroot/modules/custom/mass_content/src/Commands/MassContentCommands.php
+++ docroot/modules/custom/mass_content/src/Commands/MassContentCommands.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\mass_content\Commands;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\mass_content\MassContentBatchManager;
 use Drush\Commands\DrushCommands;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -12,37 +14,110 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class MassContentCommands extends DrushCommands {
 
   /**
-   * The Mass Content batch manager.
+   * Entity type service.
    *
-   * @var \Drupal\mass_auto_parents\MassAutoParentsBatchManager
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
-  protected $batchManager;
+  private $entityTypeManager;
+  /**
+   * Logger service.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  private $loggerChannelFactory;
 
   /**
    * {@inheritdoc}
    */
-  public function __construct(MassContentBatchManager $batch_manager) {
-    parent::__construct();
-    $this->batchManager = $batch_manager;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('mass_content.batch_manager'),
-    );
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, LoggerChannelFactoryInterface $loggerChannelFactory) {
+    $this->entityTypeManager = $entityTypeManager;
+    $this->loggerChannelFactory = $loggerChannelFactory;
   }
 
   /**
    * Migrate date field values.
    *
+   * @param string $type
+   *   Type of node to update
+   *   Argument provided to the drush command.
+   *
    * @command mass-content:migrate-dates
+   *
+   * @usage mass-content:migrate-dates foo
+   *   foo is the type of node to update.
    */
-  public function migrateDateFields() {
-    $this->batchManager->migrateDateFields();
+  public function migrateDateFields($type = '') {
+    // Don't spam all the users with content update emails.
+    $_ENV['MASS_FLAGGING_BYPASS'] = TRUE;
+
+    // Turn off entity_hierarchy writes while processing the item.
+    \Drupal::state()->set('entity_hierarchy_disable_writes', TRUE);
+
+    $memory_cache = \Drupal::service('entity.memory_cache');
+
+    // 1. Log the start of the script.
+    $this->loggerChannelFactory->get('mass_content')->info('Update nodes batch operations start');
+
+    // 2. Retrieve all nodes of this type.
+    $storage = $this->entityTypeManager->getStorage('node');
+    try {
+      $query = $storage->getQuery();
+      //
+      // Check the type of node given as argument, if not, set article as default.
+      if (strlen($type) == 0) {
+        $query->condition('type', ['advisory', 'binder', 'decision', 'executive_order', 'info_details', 'regulation', 'rules', 'news'], 'IN');
+      }
+      else {
+        $query->condition('type', $type);
+      }
+
+      $nids = $query->execute();
+    }
+    catch (\Exception $e) {
+      $this->output()->writeln($e);
+      $this->loggerChannelFactory->get('mass_content')->error('Error found @e', ['@e' => $e]);
+    }
+    // 3. Create the operations array for the batch.
+    $operations = [];
+    $numOperations = 0;
+    $batchId = 1;
+    if (!empty($nids)) {
+      foreach ($nids as $nid) {
+        // Prepare the operation. Here we could do other operations on nodes.
+        $this->output()->writeln("Preparing batch: " . $batchId);
+        $operations[] = [
+          '\Drupal\mass_content\MassContentBatchManager::processNode',
+          [
+            $batchId,
+            $storage->load($nid),
+            t('Updating node @nid', ['@nid' => $nid]),
+          ],
+        ];
+        $batchId++;
+        $numOperations++;
+      }
+    }
+    else {
+      $this->logger()->warning('No nodes of this type @type', ['@type' => $type]);
+    }
+    // 4. Create the batch.
+    $batch = [
+      'title' => t('Updating @num node(s)', ['@num' => $numOperations]),
+      'operations' => $operations,
+      'finished' => '\Drupal\mass_content\MassContentBatchManager::processNodeFinished',
+    ];
+    // 5. Add batch operations as new batch sets.
+    batch_set($batch);
+    // 6. Process the batch sets.
     drush_backend_batch_process();
+    // 6. Show some information.
+    $this->logger()->notice("Batch operations end.");
+    $memory_cache->deleteAll();
+    // Turn on entity_hierarchy writes after processing the item.
+    \Drupal::state()->set('entity_hierarchy_disable_writes', FALSE);
+    // 7. Log some information.
+    $this->loggerChannelFactory->get('mass_content')->info('Update batch operations end.');
+
   }
 
 }
