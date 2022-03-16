962a7486f48ffd2a3df453b1e8a738caf874e4c3
abaghdasaryan <arthur.baghdasar@gmail.com>
2022-03-16 11:30:51 +0400

    Fix Date time conversion handling

--- docroot/modules/custom/mass_content/src/MassContentBatchManager.php
+++ docroot/modules/custom/mass_content/src/MassContentBatchManager.php
@@ -42,7 +42,7 @@ class MassContentBatchManager {
           if ($field_name == 'field_news_date') {
             $news_date = $node->get($field_name)->getValue()[0]['value'];
             $user_timezone = $node->getOwner()->getTimeZone();
-            $date_original= new DrupalDateTime($news_date, 'UTC');
+            $date_original = new DrupalDateTime($news_date, 'UTC');
             $date_original->setTimezone(timezone_open($user_timezone));
             $converted_date = $date_original->format('Y-m-d');
             $published_date = explode('T', $converted_date)[0];
