1c8db3bf94a2dbabad1789814e488af1c9740a22
abaghdasaryan <arthur.baghdasar@gmail.com>
2022-04-14 12:46:15 +0400

    Code refactoring

--- docroot/modules/custom/mass_content/mass_content.deploy.php
+++ docroot/modules/custom/mass_content/mass_content.deploy.php
@@ -756,6 +756,11 @@ function mass_content_deploy_regenerate_image_styles_focal_point(&$sandbox) {
         $uri = $file->getFileUri();
         $image_factory = \Drupal::service('image.factory');
         $image = $image_factory->get($uri);
+        Drush::logger()->notice(dt("FID @count , image: @max , node: @node.", [
+          "@count" => $fid,
+          "@node" => $node->id(),
+          "@max" => json_encode($image)
+        ]));
         if (!empty($image->getFileSize()) && is_file($uri) && $stream_wrapper_manager->isValidUri($uri)) {
           // Apply a tiny change to generate image.
           $focal_point = "51,50";
