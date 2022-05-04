e3ac83df825aaddbcddf202cb7afc77b328bb7ad
Andrew Berry <andrew.berry@lullabot.com>
2022-05-04 18:49:52 -0400

    DP-24512 Initial database-backed similar titles queue and worker

---
+++ docroot/modules/custom/mass_admin_pages/src/Commands/SimilarTitles.php
@@ -0,0 +1,99 @@
+<?php
+declare(strict_types=1);
+
+namespace Drupal\mass_admin_pages\Commands;
+
+use Drupal\Core\Cache\MemoryCache\MemoryCacheInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Queue\QueueFactory;
+use Drupal\Core\Queue\QueueInterface;
+use Drupal\Core\State\StateInterface;
+use Drush\Commands\DrushCommands;
+
+/**
+ * Add nodes to be checked for similar titles to identify duplicate content.
+ */
+class SimilarTitles extends DrushCommands {
+
+  public const STATE_KEY = 'mass_admin_pages.similar_node_titles';
+
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  private EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * @var \Drupal\Core\Cache\MemoryCache\MemoryCacheInterface
+   */
+  private MemoryCacheInterface $entityMemoryCache;
+
+  /**
+   * @var \Drupal\Core\State\StateInterface
+   */
+  private StateInterface $state;
+
+  /**
+   * @var \Drupal\Core\Queue\QueueInterface
+   */
+  private QueueInterface $queue;
+
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, MemoryCacheInterface $entityMemoryCache, StateInterface $state, QueueFactory $queueFactory) {
+    parent::__construct();
+    $this->entityTypeManager = $entityTypeManager;
+    $this->entityMemoryCache = $entityMemoryCache;
+    $this->state = $state;
+    $this->queue = $queueFactory->get('mass_admin_pages.similar_titles');
+  }
+
+  /**
+   * @command ma:similar-titles
+   */
+  public function queue(): void {
+    /** @var \Drupal\Core\Entity\EntityTypeManagerInterface $m */
+    $query = $this->entityTypeManager->getStorage('node')->getQuery();
+
+    // Limit to published nodes of the given content types.
+    $query->condition($this->entityTypeManager->getDefinition('node')->getKey('bundle'), [
+      'campaign_landing',
+      'guide_page',
+      'how_to_page',
+      'info_details',
+      'location',
+      'org_page',
+      'service_details',
+      'service_page',
+      'topic_page',
+    ], 'IN');
+    $query->condition('status', 1);
+    $results = $query->execute();
+
+    // @todo We don't delete node data for nodes that have been unpublished.
+    // Load nodes in groups of 1000 so we can clear the memory cache.
+    $chunked = array_chunk($results, 1000);
+    $node_titles = [];
+    foreach ($chunked as $chunk) {
+      $nodes = $this->entityTypeManager->getStorage('node')
+        ->loadMultiple($chunk);
+      foreach($nodes as $node) {
+        $node_titles[$node->id()] = $node->label();
+      }
+
+      /** @noinspection DisconnectedForeachInstructionInspection */
+      $this->entityMemoryCache->deleteAll();
+    }
+
+    // Since we can't control for edits made after we make the comparison, we
+    // take this opportunity to load all node titles now of matching nodes, and
+    // them save them into state for use by the queue. We don't use cache as
+    // memcache has a limit of 1MB, and we expect this data to be 5-20MB.
+    $this->state->set(self::STATE_KEY, $node_titles);
+
+    // Now, queue the nodes to check. We also group this by 1000 as we don't
+    // really need to process nodes individually.
+    $queue_chunk = array_chunk($node_titles, 1000, TRUE);
+    foreach ($queue_chunk as $chunk) {
+      $this->queue->createItem($chunk);
+    }
+  }
+
+}
---
+++ docroot/modules/custom/mass_admin_pages/src/Plugin/QueueWorker/SimilarTitlesWorker.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\mass_admin_pages\Plugin\QueueWorker;
+
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\Core\State\StateInterface;
+use Drupal\mass_admin_pages\Commands\SimilarTitles;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * @see \Drupal\mass_admin_pages\Commands\SimilarTitles
+ *
+ * @QueueWorker(
+ *   id="mass_admin_pages.similar_titles",
+ *   title=@Translation("Similar Titles"),
+ * )
+ */
+class SimilarTitlesWorker extends QueueWorkerBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * @var mixed
+   */
+  private $node_titles;
+
+  /**
+   * @var \Drupal\Core\Database\Connection
+   */
+  private Connection $database;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, StateInterface $state, Connection $database) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->node_titles = $state->get(SimilarTitles::STATE_KEY);
+    $this->database = $database;
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('state'),
+      $container->get('database')
+    );
+  }
+
+  /**
+   * Process the queue item of ~1000 nodes.
+   *
+   * This is somewhat of an expensive operation because we have to compare every
+   * single node against every single other node. We could consider filtering
+   * this list, such as only comparing in one direction (new to old), accepting
+   * that similar_text() gives different non-comparable results when you flip
+   * its arguments around.
+   *
+   * @param $data
+   *
+   * @return void
+   */
+  public function processItem($data) {
+    foreach ($data as $nid => $title) {
+      foreach ($this->node_titles as $compare_nid => $compare_title) {
+        if ($nid === $compare_nid) {
+          continue;
+        }
+        $matching_characters = similar_text($title, $compare_title, $percent);
+        if ($percent > 80) {
+          $this->database->upsert('mass_admin_pages_similar_titles')->fields([
+            'nid' => $nid,
+            'title' => $title,
+            'other_nid' => $compare_nid,
+            'other_title' => $compare_title,
+            'percent' => $percent,
+          ])->execute();
+        }
+        else {
+          $this->database->delete('mass_admin_pages_similar_titles')->condition('nid', $nid);
+        }
+      }
+    }
+
+  }
+
+}
