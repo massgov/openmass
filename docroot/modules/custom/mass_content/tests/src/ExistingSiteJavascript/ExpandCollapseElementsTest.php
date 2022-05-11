b173b70cd6d6759ff00358d1095e1d592c559a4b
Andrew Berry <andrew.berry@lullabot.com>
2022-05-11 14:08:14 -0400

    DP-24877 Refactor Accordion tests to fix concurrency issues

--- docroot/modules/custom/mass_content/tests/src/ExistingSiteJavascript/ExpandCollapseElementsTest.php
+++ docroot/modules/custom/mass_content/tests/src/ExistingSiteJavascript/ExpandCollapseElementsTest.php
@@ -2,60 +2,144 @@
 
 namespace Drupal\Tests\mass_content\ExistingSiteJavascript;
 
-use Behat\Mink\Element\NodeElement;
+use Behat\Mink\Session;
 use weitzman\DrupalTestTraits\ExistingSiteWebDriverTestBase;
 
 /**
- * Test Expandible/Collapsible elements.
+ * Test Expandable/Collapsible elements.
  */
 class ExpandCollapseElementsTest extends ExistingSiteWebDriverTestBase {
 
   /**
-   * Loads a node based on its title.
+   * Tests a single accordion for a given page, located at a CSS selector.
+   *
+   * @dataProvider accordionDataProvider
+   *
+   * @param string $path
+   *   The path of the page to load.
+   * @param string $css_selector
+   *   The CSS selector to find the accordion at.
+   * @param \Closure|null $before_function
+   *   An optional function to call to set up any preconditions on the page,
+   *   such as opening a parent accordion or menu. This closure takes a single
+   *   Session parameter.
+   *
+   * @return void
    */
-  private function getNidByTitle(string $title) {
-    $eq_node = \Drupal::entityQuery('node');
-    $res = $eq_node->condition('title', $title)->execute();
-    return reset($res);
-  }
-
-  /**
-   * Tests one accordion element.
-   */
-  private function testAccordion(NodeElement $accordion) {
+  public function testAccordion(string $path, string $css_selector, \Closure $before_function = null): void {
     $session = $this->getSession();
-    $accordion_link = $accordion->find('css', '.js-accordion-link');
+    $this->drupalGet($path);
+    if ($before_function) {
+      // We can't use $this->getSession() in the closure because PHPUnit creates
+      // a new object in between gathering data provider test cases and running
+      // the test.
+      $before_function($session);
+    }
+
+    $page = $session->getPage();
+    $accordion = $page->find('css', $css_selector);
 
     // Get the accordion state, then click on it.
-    $initial_state = $accordion->hasClass('is-open');
+    $initial_state_open = $accordion->hasClass('is-open');
+    $accordion_link = $accordion->find('css', '.js-accordion-link');
     $accordion_link->click();
-    $session->wait(1000);
 
-    // Check the accordion collased/expanded state was toggled.
-    $this->assertTrue($initial_state != $accordion->hasClass('is-open'));
-  }
+    // Wait up to 30 seconds for the accordion to open. Unfortunately, there's
+    // no unique identifier to pass into JS beyond the XPath, but luckily
+    // all browsers except IE11 support it, even if it's in maintenance mode
+    // only.
+    // This is significantly faster than the below API call.
+    // $this->assertSession()->waitForElementVisible('xpath', $accordion->getXpath());
+    $accordion_xpath = str_replace('"', '\"', $accordion->getXpath());
+    if ($initial_state_open) {
+      $wait_for = sprintf('document.evaluate("%s", document, null, XPathResult.ANY_TYPE, null ).iterateNext().classList.contains("is-open") == false', $accordion_xpath);
+    }
+    else {
+      $wait_for = sprintf('document.evaluate("%s", document, null, XPathResult.ANY_TYPE, null ).iterateNext().classList.contains("is-open")', $accordion_xpath);
+    }
+    if (!$session->wait(30000, $wait_for)) {
+      $this->fail('The accordion never toggled for testing.');
+    }
 
-  /**
-   * Tests accordion elements on a node given a $nid.
-   */
-  private function testAccordionsAtNode(int $nid) {
-    $session = $this->getSession();
-    $this->drupalGet('node/' . $nid);
-    // Wait for async notifications to be processed.
-    $session->wait(2000);
-    $page = $session->getPage();
-    $accordion_links = $page->findAll('css', '.js-accordion');
-    foreach ($accordion_links as $accordion_link) {
-      $this->testAccordion($accordion_link);
+    // Check the accordion collapsed/expanded state was toggled.
+    $this->assertNotSame($initial_state_open, $accordion->hasClass('is-open'));
+
+    // Revert it back to the original state.
+    $accordion_link->click();
+    if ($initial_state_open) {
+      $wait_for = sprintf('document.evaluate("%s", document, null, XPathResult.ANY_TYPE, null ).iterateNext().classList.contains("is-open")', $accordion_xpath);
+    }
+    else {
+      $wait_for = sprintf('document.evaluate("%s", document, null, XPathResult.ANY_TYPE, null ).iterateNext().classList.contains("is-open") == false', $accordion_xpath);
+    }
+    $this->assertSame($initial_state_open, $accordion->hasClass('is-open'));
+    if (!$session->wait(30000, $wait_for)) {
+      $this->fail('The accordion was never restored to the original state.');
     }
   }
 
   /**
-   * Tests accordions on 2 nodes.
+   * Data provider of accordion test cases.
+   *
+   * @return array
+   *   An array of test cases, indexed by a human-friendly name, containing:
+   *     - The path of the page to test accordions on.
+   *     - A CSS selector to locate the accordion with.
+   *     - An optional function to call before running the test case, accepting
+   *       a Session parameter.
    */
-  public function testMultipleAccordions() {
-    $this->testAccordionsAtNode($this->getNidByTitle('_QAG Binder_Report'));
-    $this->testAccordionsAtNode($this->getNidByTitle('_QAG Request Help with a Computer Problem '));
+  public function accordionDataProvider(): array {
+    return [
+      '_QAG Binder_Report Table of Contents' => [
+        'report/qag-binderreport',
+        '.ma__toc--hierarchy__accordion.js-accordion',
+      ],
+      '_QAG Binder_Report Contact Us' => [
+        'report/qag-binderreport',
+        '.ma__contact-us.js-accordion',
+      ],
+      '_QAG Request Help with a Computer Problem Accordion in Table of Contents' => [
+        'how-to/qag-request-help-with-a-computer-problem',
+        '.ma__toc--hierarchy__accordion.js-accordion',
+        function(Session $session): void {
+          // Open up the Table of Contents containing the accordion.
+          $page = $session->getPage();
+          /** @noinspection NullPointerExceptionInspection */
+          $page->find('css', '.ma__toc__toc__toggle')->click();
+          if (!$session->wait(30000, "jQuery('.ma__toc--overlay__container.is-open').is(':visible')")) {
+            $this->fail('The Table of Contents overlay did not become visible');
+          }
+        }
+      ],
+      '_QAG Request Help with a Computer Problem Notices and Alerts' => [
+        'how-to/qag-request-help-with-a-computer-problem',
+        '.ma__header-alerts.js-accordion',
+      ],
+      '_QAG Request Help with a Computer Problem Inside Alert' => [
+        'how-to/qag-request-help-with-a-computer-problem',
+        '.ma__action-step.js-accordion.ma__action-step--accordion:nth-child(1)',
+      ],
+      '_QAG Request Help with a Computer Problem Help by Phone' => [
+        'how-to/qag-request-help-with-a-computer-problem',
+        '.ma__action-step.js-accordion.ma__action-step--accordion:nth-child(2)',
+      ],
+      '_QAG Request Help with a Computer Problem Help Online' => [
+        'how-to/qag-request-help-with-a-computer-problem',
+        '.ma__action-step.js-accordion.ma__action-step--accordion:nth-child(3)',
+      ],
+      '_QAG Request Help with a Contact in Body' => [
+        'how-to/qag-request-help-with-a-computer-problem',
+        '.ma__contact-us.ma__contact-us--accordion.js-accordion',
+      ],
+      // Note this block is actually duplicated in the page-content div for
+      // narrow viewports. Since the code inside is the same, and we haven't
+      // seen regressions with it, we don't bother changing the viewport and
+      // testing it as well.
+      '_QAG Request Help with a Sidebar Contact' => [
+        'how-to/qag-request-help-with-a-computer-problem',
+        '.ma__contact-us.js-accordion',
+      ],
+    ];
   }
 
 }
