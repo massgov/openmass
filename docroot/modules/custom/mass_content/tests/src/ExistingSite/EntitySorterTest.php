ecc450e90902d646a628ce2bbe7ab3c3d77be362
beto-aveiga <luis.aveiga@gmail.com>
2022-05-27 11:56:17 -0500

    DP-24800 updating expected values

--- docroot/modules/custom/mass_content/tests/src/ExistingSite/EntitySorterTest.php
+++ docroot/modules/custom/mass_content/tests/src/ExistingSite/EntitySorterTest.php
@@ -34,7 +34,7 @@ class EntitySorterTest extends ExistingSiteBase {
 
     $this->entitySorter = new EntitySorter();
     $this->entitiesForSorting = $this->createEntitiesForSorting();
-    $this->expectedIndexesAsc = [1, 7, 8, 11, 5, 3, 4, 9, 12, 6, 10, 2];
+    $this->expectedIndexesAsc = [1, 7, 8, 11, 5, 3, 4, 9, 12, 6, 10, 2, 13];
   }
 
   /**
