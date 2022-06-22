f70a1f72c7b2f35391107a48505a7fe3ba132656
beto-aveiga <luis.aveiga@gmail.com>
2022-06-22 12:48:30 -0500

    DP-25197 eslint

--- docroot/themes/custom/mass_admin_theme/js/avoid-body-scroll-with-modals.js
+++ docroot/themes/custom/mass_admin_theme/js/avoid-body-scroll-with-modals.js
@@ -1,6 +1,6 @@
 'use strict';
 
-(() => {
+(function () {
   function controlBodyOverflow() {
     const modals = document.querySelectorAll('[aria-describedby=drupal-modal]').length > 0;
     document.getElementsByTagName('body')[0].setAttribute('data-showing-modal', modals);
