a269cd577a116449bae16de4de320ce8317a66a8
Moshe Weitzman <weitzman@tejasa.com>
2022-06-27 16:37:05 -0400

    Add file comment

--- docroot/modules/custom/mass_admin_pages/src/tmp.php
+++ docroot/modules/custom/mass_admin_pages/src/tmp.php
@@ -1,1 +1,8 @@
 <?php
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
+ */
