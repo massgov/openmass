b3dfba2b808bff28e8d7f3f6dcd32e6dc2cd8e54
Moshe Weitzman <weitzman@tejasa.com>
2022-07-12 09:01:17 -0400

    Log state changes in nodes and media.

--- docroot/modules/custom/mass_content/src/Entity/Bundle/node/NodeBundle.php
+++ docroot/modules/custom/mass_content/src/Entity/Bundle/node/NodeBundle.php
@@ -2,11 +2,12 @@
 
 namespace Drupal\mass_content\Entity\Bundle\node;
 
+use Drupal\mass_content_moderation\MassModerationTrait;
 use Drupal\node\Entity\Node;
 
 /**
  * A base bundle class for node entities.
  */
 abstract class NodeBundle extends Node {
-
+  use MassModerationTrait;
 }
