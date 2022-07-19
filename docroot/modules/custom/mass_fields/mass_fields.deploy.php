3e53663617690fb8b750ad18df038c946e152323
Moshe Weitzman <weitzman@tejasa.com>
2022-07-19 16:07:33 -0400

    Use post update

--- docroot/modules/custom/mass_fields/mass_fields.deploy.php
+++ docroot/modules/custom/mass_fields/mass_fields.deploy.php
@@ -6,7 +6,5 @@ use Drupal\devel_entity_updates\DevelEntityDefinitionUpdateManager;
  * Entity schema update for new 'search' boolean field.
  */
 function mass_fields_deploy_search_field() {
-  Drupal::classResolver()
-    ->getInstanceFromDefinition(DevelEntityDefinitionUpdateManager::class)
-    ->applyUpdates();
+
 }
