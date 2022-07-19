3940f1153c90f254715a0a652aa4fd38f4ad3af0
Moshe Weitzman <weitzman@tejasa.com>
2022-07-19 07:44:58 -0400

    Use update hook

--- docroot/modules/custom/mass_fields/mass_fields.post_update.php
+++ docroot/modules/custom/mass_fields/mass_fields.post_update.php
@@ -15,15 +15,4 @@ function mass_fields_post_update_text_format() {
     ->update('paragraph__field_rules_section_body')
     ->fields(['field_rules_section_body_format' => 'rules_of_court_section'])
     ->execute();
-
-}
-
-/**
- * Updates media and node entity types for new search boolean field.
- */
-function mass_fields_post_update_search_field() {
-  foreach (['node', 'media'] as $type) {
-    $entity_type = \Drupal::entityTypeManager()->getDefinition($type);
-    \Drupal::entityDefinitionUpdateManager()->updateEntityType($entity_type);
-  }
 }
