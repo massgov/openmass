a7674f28ce85f153def0e278703f5a1fc9d04167
Moshe Weitzman <weitzman@tejasa.com>
2022-07-20 11:38:14 -0400

    Add migration.

---
+++ docroot/modules/custom/mass_fields/mass_fields.deploy.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Migrate from simple sitemap exclusion to the new boolean 'search' field.
+ */
+function mass_fields_deploy_search_field() {
+  $result = \Drupal::database()->select('simple_sitemap_entity_overrides', 'sseo')->fields('sseo', ['id', 'entity_type', 'entity_id'])->execute();
+  $items = $result->fetchAllAssoc('id');
+  $i=0;
+  foreach ($items as $item) {
+    $i++;
+    if ($entity = Drupal::entityTypeManager()->getStorage($item->entity_type)->load($item->entity_id)) {
+      $entity->set('search', TRUE)->save();
+      \Drupal::logger('mass_fields')->notice('Resaved !url (!i of !total).', ['!url' => $entity->toUrl()->toString(), '!i' => $i, '!total' => count($items)]);
+    }
+  }
+  \Drupal::database()->truncate('simple_sitemap_entity_overrides');
+}
