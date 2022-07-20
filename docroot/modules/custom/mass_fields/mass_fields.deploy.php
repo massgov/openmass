ee5a8243b6bfd4dbe14ff1f3791016a353e64eca
Moshe Weitzman <weitzman@tejasa.com>
2022-07-20 12:36:12 -0400

    Set right default value.
    Hide simple sitemap section from node/media form

--- docroot/modules/custom/mass_fields/mass_fields.deploy.php
+++ docroot/modules/custom/mass_fields/mass_fields.deploy.php
@@ -4,6 +4,7 @@
  * Migrate from simple sitemap exclusion to the new boolean 'search' field.
  */
 function mass_fields_deploy_search_field() {
+  // Get all overrides.
   $result = \Drupal::database()->select('simple_sitemap_entity_overrides', 'sseo')->fields('sseo', ['id', 'entity_type', 'entity_id'])->execute();
   $items = $result->fetchAllAssoc('id');
   $i = 0;
@@ -11,7 +12,7 @@ function mass_fields_deploy_search_field() {
     $i++;
     if ($entity = Drupal::entityTypeManager()->getStorage($item->entity_type)->load($item->entity_id)) {
       $entity->set('search', TRUE)->save();
-      \Drupal::logger('mass_fields')->notice('Resaved !url (!i of !total).', ['!url' => $entity->toUrl()->toString(), '!i' => $i, '!total' => count($items)]);
+      \Drupal::logger('mass_fields')->notice('Migrated !url (!i of !total).', ['!url' => $entity->toUrl()->toString(), '!i' => $i, '!total' => count($items)]);
     }
   }
   \Drupal::database()->truncate('simple_sitemap_entity_overrides');
