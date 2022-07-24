2ab114ef2e33df4045a598dc8282fd4c2ab149b7
abaghdasaryan <arthur.baghdasar@gmail.com>
2022-07-24 14:44:27 +0400

    DP-25473: Update mg_organizations metatag value on organization pages

--- docroot/modules/custom/mass_metatag/src/Service/MassMetatagUtilities.php
+++ docroot/modules/custom/mass_metatag/src/Service/MassMetatagUtilities.php
@@ -63,7 +63,7 @@ class MassMetatagUtilities {
       }
       // For all other nodes, get all the organizations referenced
       // and add it to the orgs array so they can be checked for parents.
-      elseif ($node->hasField('field_organizations')) {
+      if ($node->hasField('field_organizations')) {
         /** @var \Drupal\node\Entity\Node[] $org_pages */
         $org_pages = $node->field_organizations->referencedEntities();
         foreach ($org_pages as $org_page) {
