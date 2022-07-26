b86e7b9dbc36e4fc84e0f15b3e2c3f3473c0515f
abaghdasaryan <arthur.baghdasar@gmail.com>
2022-07-26 11:51:48 +0400

    Refactor code

--- docroot/modules/custom/mass_metatag/src/Service/MassMetatagUtilities.php
+++ docroot/modules/custom/mass_metatag/src/Service/MassMetatagUtilities.php
@@ -60,23 +60,10 @@ class MassMetatagUtilities {
         if (!$node->field_parent->isEmpty() && !is_null($node->field_parent->entity) && !in_array($node->field_parent->entity->id(), $checked_orgs)) {
           $orgs[] = $node->field_parent->entity;
         }
-        if ($node->hasField('field_organizations') && !$node->field_organizations->isEmpty()) {
-          /** @var \Drupal\node\Entity\Node[] $org_pages */
-          $org_pages = $node->field_organizations->referencedEntities();
-          foreach ($org_pages as $org_page) {
-            if (!in_array($org_page->id(), $checked_orgs)) {
-              // Only add the referenced orgs if they have not already been
-              // checked.
-              if (!in_array($org_page->id(), $checked_orgs)) {
-                $orgs[] = $org_page;
-              }
-            }
-          }
-        }
       }
       // For all other nodes, get all the organizations referenced
       // and add it to the orgs array so they can be checked for parents.
-      elseif ($node->hasField('field_organizations')) {
+      if ($node->hasField('field_organizations')) {
         /** @var \Drupal\node\Entity\Node[] $org_pages */
         $org_pages = $node->field_organizations->referencedEntities();
         foreach ($org_pages as $org_page) {
