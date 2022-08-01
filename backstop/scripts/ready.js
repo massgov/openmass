6b64992c73bb5be223d2dd636dfdd7cd5aec55ff
abaghdasaryan <arthur.baghdasar@gmail.com>
2022-08-01 11:50:04 +0400

    Ready js change

--- backstop/scripts/ready.js
+++ backstop/scripts/ready.js
@@ -134,9 +134,10 @@ module.exports = async function (page, scenario, vp) {
       visible: true,
       timeout: 10000,
     })
+    await page.waitForTimeout(1000);
   }
   else if (leafletMapInitialized == true) {
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(2000);
   }
 
   if (!scenario.hideAlerts || scenario.hideAlerts === undefined) {
