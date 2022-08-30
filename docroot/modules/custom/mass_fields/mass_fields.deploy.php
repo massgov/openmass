d98915ef8d8488c0b779c065d30d1b4ae1243639
Moshe Weitzman <weitzman@tejasa.com>
2022-08-30 10:41:42 -0400

    Improve migration

--- docroot/modules/custom/mass_fields/mass_fields.deploy.php
+++ docroot/modules/custom/mass_fields/mass_fields.deploy.php
@@ -5,7 +5,7 @@
  */
 function mass_fields_deploy_search_field3() {
   $i = 0;
-  $migrated = [];
+  $migrated = ['node' => [], 'media' => []];
 
   // Migrate from metatag.
   $types = \Drupal::entityTypeManager()->getStorage('node_type')->loadMultiple();
@@ -19,16 +19,17 @@ function mass_fields_deploy_search_field3() {
       $ids = $result->fetchCol();
       foreach ($ids as $id) {
         $i++;
-        if (isset($migrated['node'][$id])) {
+        if (in_array($id, $migrated['node'])) {
           // We already migrated this entity.
           continue;
         }
         elseif ($entity = Drupal::entityTypeManager()->getStorage('node')->load($id)) {
           $entity->set('search', TRUE)->save();
-          \Drupal::logger('mass_fields')->notice('Migrated !url from metatag (!i of !total).', ['!url' => $entity->toUrl()->toString(), '!i' => $i, '!total' => count($ids)]);
-          $migrated['node'][$id];
+          \Drupal::logger('mass_fields')->notice('Migrated !url from metatag (!i of !total !type).', ['!url' => $entity->toUrl()->toString(), '!i' => $i, '!total' => count($ids), '!type' => $type->id()]);
+          $migrated['node'][] = $id;
         }
       }
+      $i=0;
     }
   }
 
@@ -39,9 +40,10 @@ function mass_fields_deploy_search_field3() {
   foreach ($items as $item) {
     $i++;
     $entity = Drupal::entityTypeManager()->getStorage($item->entity_type)->load($item->entity_id);
-    if ($entity && !isset($migrated['node'][$entity->id()])) {
+    if ($entity && !in_array($entity->id(), $migrated['node'])) {
       $entity->set('search', TRUE)->save();
       \Drupal::logger('mass_fields')->notice('Migrated !url from simple sitemap (!i of !total).', ['!url' => $entity->toUrl()->toString(), '!i' => $i, '!total' => count($items)]);
+      $migrated[$entity->getEntityTypeId()][] = $id;
     }
   }
   \Drupal::database()->truncate('simple_sitemap_entity_overrides');
