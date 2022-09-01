f7987a4cae3b5def2f9a14a7ec4dca6b9dfdb852
Moshe Weitzman <weitzman@tejasa.com>
2022-09-01 14:06:37 -0400

    Use bundle methods.

--- docroot/modules/custom/mass_content/src/Entity/Bundle/node/EventBundle.php
+++ docroot/modules/custom/mass_content/src/Entity/Bundle/node/EventBundle.php
@@ -7,4 +7,21 @@ namespace Drupal\mass_content\Entity\Bundle\node;
  */
 class EventBundle extends NodeBundle {
 
+  public function getMeetingType() {
+    return $this->get('field_event_type_list')->getString();
+  }
+
+  public function isPublicMeeting() {
+    return $this->getMeetingType() == 'public_meeting';
+  }
+
+  public function isPublicHearing() {
+    return $this->getMeetingType() == 'public_hearing';
+
+  }
+
+  public function getAddressType() {
+    return $this->get('field_event_address_type');
+  }
+
 }
