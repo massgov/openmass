d76ad86a13946f51dc4d8b15e27045a1caa4ae7e
Yukiko Gannett <yukikogannett@gmail.com>
2022-09-20 12:43:35 -0400

    add custom JS for auto complete field combobox

---
+++ docroot/themes/custom/mass_admin_theme/js/auto-complete-combobox.js
@@ -0,0 +1,7 @@
+/**
+ * @file
+ * Add missing combobox association between auto complete fields and
+ * their option boxes with aria.
+ *
+ * This addresses the accessibility issue with JAWS:  DP-25848
+ */
