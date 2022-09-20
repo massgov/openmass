5756713ca91cdec1d0983696d32047bc0d64b166
Yukiko Gannett <yukikogannett@gmail.com>
2022-09-20 15:31:27 -0400

    add wrapper

--- docroot/themes/custom/mass_admin_theme/js/auto-complete-combobox.js
+++ docroot/themes/custom/mass_admin_theme/js/auto-complete-combobox.js
@@ -5,44 +5,47 @@
  *
  * This addresses the accessibility issue with JAWS:  DP-25848
  */
-let autoCompleteFields = document.querySelectorAll(".ui-autocomplete-input");
-let optionLists = document.querySelectorAll(".ui-autocomplete");
+(function () {
+  'use strict';
 
-// Add missing accessibility components to pairng a field and its combobox(option list).
-autoCompleteFields.setAttribute("role", "combobox");
-autoCompleteFields.setAttribute("aria-autocomplete", "none");
-autoCompleteFields.setAttribute("aria-expanded", "false");
+  let autoCompleteFields = document.querySelectorAll(".ui-autocomplete-input");
+  let optionLists = document.querySelectorAll(".ui-autocomplete");
 
-optionLists.attr("role", "listbox");
+  // Add missing accessibility components to pairng a field and its combobox(option list).
+  autoCompleteFields.setAttribute("role", "combobox");
+  autoCompleteFields.setAttribute("aria-autocomplete", "none");
+  autoCompleteFields.setAttribute("aria-expanded", "false");
+
+  optionLists.attr("role", "listbox");
 
 
-// autoCompleteFields.addEventListener("keyPress", () => {
+  // autoCompleteFields.addEventListener("keyPress", () => {
 
-// });
+  // });
 
-autoCompleteFields.forEach(autoCompleteField => {
-  autoCompleteField.addEventListener("keyPress", (e) => {
-    console.log(e.target);
+  autoCompleteFields.forEach(autoCompleteField => {
+    autoCompleteField.addEventListener("keyPress", (e) => {
+      console.log(e.target);
+    });
   });
-});
-// autoCompleteFields.findIndex();
+  // autoCompleteFields.findIndex();
 
 
-// Add missing accessibility components to pairng a field and its combobox(option list) as their lists get generated.
-// Get index for the field with keypress event fired.
+  // Add missing accessibility components to pairng a field and its combobox(option list) as their lists get generated.
+  // Get index for the field with keypress event fired.
 
-// Find the UL that got its content.
-// Get ID and assign it to its corresponding field's aria-controls.
-// autoCompleteFields[XX].setAttribute("aria-controls", "ID");
-// 1. Check the UL doesn't have display: none;
-// 2. Change the aria-expanded value to true.
-// autoCompleteFields[XX].setAttribute("aria-expanded", "true");
-// Add roles to LIs and As.
-// optionLists[XX].setAttribute("role", "combobox");
-// optionLists[XX].querySelectorAll("li").setAttribute("role", "none");
-// optionLists[XX].querySelectorAll("li a").setAttribute("role", "option");
+  // Find the UL that got its content.
+  // Get ID and assign it to its corresponding field's aria-controls.
+  // autoCompleteFields[XX].setAttribute("aria-controls", "ID");
+  // 1. Check the UL doesn't have display: none;
+  // 2. Change the aria-expanded value to true.
+  // autoCompleteFields[XX].setAttribute("aria-expanded", "true");
+  // Add roles to LIs and As.
+  // optionLists[XX].setAttribute("role", "combobox");
+  // optionLists[XX].querySelectorAll("li").setAttribute("role", "none");
+  // optionLists[XX].querySelectorAll("li a").setAttribute("role", "option");
 
 
-// // When UL has display: none;, set its corresnponding field's aria-expanded to false.
-// autoCompleteFields[XX].setAttribute("aria-expanded", "false");
-
+  // // When UL has display: none;, set its corresnponding field's aria-expanded to false.
+  // autoCompleteFields[XX].setAttribute("aria-expanded", "false");
+})();
