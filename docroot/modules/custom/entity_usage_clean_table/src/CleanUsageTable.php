35c77ffc62aefb0682e349d74ff42f6376d692f5
Thomas Fleming <tom@lastcallmedia.com>
2022-09-30 17:10:12 -0400

    Added CleanUsageTable service and command - DP-25357

---
+++ docroot/modules/custom/entity_usage_clean_table/src/CleanUsageTable.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Drupal\entity_usage;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\RevisionableInterface;
+
+/**
+ * Cleans entity usage table from repeated records.
+ */
+class CleanUsageTable {
+
+  /**
+   * Database service.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  private $database;
+
+  /**
+   * Entity Type Manager Service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  private $entityTypeManager;
+
+  /**
+   * Returns a CleanUsageTable object with populated properties.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   * @param \Drupal\Core\Database\Connection $database
+   *
+   * @return void
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, Connection $database) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->database = $database;
+  }
+
+  /**
+   * Execute private methods for cleanining.
+   */
+  public function clean() {
+    $this->deleteRecordsWithNonCurrentRevisions();
+    $this->deleteRecordsPointingThemselves();
+  }
+
+  /**
+   * Delete references from other revisions.
+   */
+  private function deleteOtherRevisions($id, $vid, $suffix = '') {
+    $this->database->delete('entity_usage')
+      ->condition('source_id' . $suffix, $id)
+      ->condition('source_vid' . $suffix, $vid, '<>')
+      ->execute();
+  }
+
+  /**
+   * Deletes records where the target is the same as the source.
+   */
+  private function deleteRecordsPointingThemselves() {
+    $this->database->query("
+      DELETE FROM entity_usage
+      WHERE target_id = source_id
+    ")->execute();
+
+    $this->database->query("
+      DELETE FROM entity_usage
+      WHERE target_id_string = source_id_string
+    ")->execute();
+
+  }
+
+  /**
+   * Returns records with repeated entity IDs.
+   */
+  private function getRepeatedRecords($field) {
+    return $this->database->query("
+        SELECT
+          {$field}, source_type, COUNT({$field})
+        FROM entity_usage
+        GROUP BY {$field} HAVING COUNT({$field}) > 1
+    ")->fetchAll();
+  }
+
+  /**
+   * Deletes records with revisions that are not the current one.
+   */
+  private function deleteRecordsWithNonCurrentRevisions() {
+    $res = [];
+    $res[''] = $this->getRepeatedRecords('source_id');
+    $res['_string'] = $this->getRepeatedRecords('source_id_string');
+
+    foreach ($res as $key => $records) {
+
+      foreach ($records as $record) {
+        $source_type = $record->source_type;
+        $source_id = $record->source_id;
+        $storage = $this->entityTypeManager->getStorage($source_type);
+        /** @var \Drupal\Core\Entity\RevisionableInterface */
+        $entity = $storage->load($source_id);
+        if (!($entity instanceof RevisionableInterface)) {
+          continue;
+        }
+        $vid = $entity->getRevisionId();
+        $this->deleteOtherRevisions($source_id, $vid, $key);
+      }
+    }
+  }
+
+}
---
+++ docroot/modules/custom/entity_usage_clean_table/src/Commands/CleanUsageTableCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\entity_usage\Commands;
+
+use Drush\Commands\DrushCommands;
+
+/**
+ * Exposes the CleanUsageTable service to clean the usage table.
+ */
+class CleanUsageTableCommand extends DrushCommands {
+
+  private $cleanUsageTableService;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct($clean_usage_table) {
+    $this->cleanUsageTableService = $clean_usage_table;
+  }
+
+  /**
+   * Drush command that clean the usage table.
+   *
+   * @command clean_usage_table
+   */
+  public function clean() {
+    $this->cleanUsageTableService->clean();
+  }
+
+}
