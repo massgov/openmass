24cadfd8dfc63d6175882e4e5205ab209e056c60
Moshe Weitzman <weitzman@tejasa.com>
2022-10-04 12:27:08 -0400

    Build form

--- docroot/modules/custom/mass_utility/src/Form/MoveRedirectsForm.php
+++ docroot/modules/custom/mass_utility/src/Form/MoveRedirectsForm.php
@@ -3,16 +3,37 @@
 namespace Drupal\mass_utility\Form;
 
 use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Form\FormBase;
+use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Url;
 use Drupal\mass_content\Entity\Bundle\node\NodeBundle;
 use Drupal\mass_content_moderation\MassModeration;
+use Drupal\redirect\RedirectRepository;
+use Psr\Container\ContainerInterface;
 
 /**
- * Provides a Mass.gov Utility Module form.
+ * Provides a Move Redirects entity form.
  */
-class MoveRedirectsForm extends FormBase {
+class MoveRedirectsForm extends ContentEntityForm {
+
+  protected RedirectRepository $redirectRepository;
+
+  public function __construct($redirectRepository) {
+    $this->redirectRepository = $redirectRepository;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    return new static(
+    // Load the service required to construct this class.
+      $container->get('redirect.repository'),
+    );
+  }
 
   /**
    * User must have edit access and the node must be in Trash.
@@ -35,13 +56,24 @@ class MoveRedirectsForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-
     $items = [];
 
+    /** @var NodeBundle $node */
+    $node = $this->getEntity();
+    $items = $this->getRedirectItems($node, $items);
+    $items = $this->getAliasItems($node, $items);
+
+    $form['target'] = [
+      '#type' => 'entity_autocomplete',
+      '#target_type' => 'node',
+      '#title' => $this->t('Pick a target for the URLs'),
+      '#required' => TRUE,
+    ];
+
     $form['list'] = [
-      '#type' => 'item_list',
+      '#theme' => 'item_list',
       '#items' => $items,
-      '#title' => 'The following items will be moved',
+      '#title' => 'The following URLs will be redirected',
     ];
 
     $form['actions'] = [
@@ -49,7 +81,7 @@ class MoveRedirectsForm extends FormBase {
     ];
     $form['actions']['submit'] = [
       '#type' => 'submit',
-      '#value' => $this->t('Move'),
+      '#value' => $this->t('Save'),
     ];
 
     return $form;
@@ -59,17 +91,54 @@ class MoveRedirectsForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    if (mb_strlen($form_state->getValue('message')) < 10) {
-      $form_state->setErrorByName('message', $this->t('Message should be at least 10 characters.'));
-    }
+    // Nothing yet.
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->messenger()->addStatus($this->t('The message has been sent.'));
-    $form_state->setRedirect('<front>');
+    $node = $this->getEntity();
+    $redirects = $this->redirectRepository->findByDestinationUri(['internal:/node/' . $node->id()]);
+    foreach ($redirects as $redirect) {
+      $redirect->setRedirect('node/' . $form['target']);
+    }
+    $this->messenger()->addStatus($this->t('The URLs have been redirected.'));
+  }
+
+  /**
+   * Get any redirects to move.
+   *
+   * @param \Drupal\mass_content\Entity\Bundle\node\NodeBundle $node
+   * @param array $items
+   *
+   * @return array
+   */
+  public function getRedirectItems(NodeBundle $node, array $items): array {
+    $redirects = $this->redirectRepository->findByDestinationUri(['internal:/node/' . $node->id()]);
+    foreach ($redirects as $redirect) {
+      $items[] = $redirect->getSourceUrl();
+    }
+    return $items;
+  }
+
+  /**
+   * Get any potential alias.
+   *
+   * @param \Drupal\mass_content\Entity\Bundle\node\NodeBundle $node
+   * @param array $items
+   *
+   * @return array
+   * @throws \Drupal\Core\Entity\EntityMalformedException
+   */
+  public function getAliasItems(NodeBundle $node, array $items): array {
+    $url = $node->toUrl()->toString();
+    // Strip off unwanted suffix
+    $url = str_replace('---unpublished', '', $url);
+    if (strpos($url, 'node/') === FALSE) {
+      $items[] = Link::fromTextAndUrl($url, Url::fromUserInput($url));
+    }
+    return $items;
   }
 
 }
