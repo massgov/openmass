78776bf98333dc40e53dd79076fc1d714e4731c5
Yukiko Gannett <yukikogannett@gmail.com>
2022-10-26 12:19:12 -0400

    replace the first timeout function with Drupal.behavior

--- docroot/themes/custom/mass_admin_theme/js/auto-complete-combobox.js
+++ docroot/themes/custom/mass_admin_theme/js/auto-complete-combobox.js
@@ -8,64 +8,70 @@
 (function () {
   'use strict';
 
-  window.addEventListener('load', function (e) {
-  // The timeout function is necessary to recognize the fields and the lists.
-  // setTimeout(function () {
-    var instructionForAll = document.querySelector('#block-mass-admin-theme-mainpagecontent .view-header') ? document.querySelector('#block-mass-admin-theme-mainpagecontent .view-header') : null;
-    var instructionForSrContent = document.createTextNode('Use tab key to navigate.');
+  Drupal.behaviors.autoCompleteCombobox = {
+    attach: function (context, settings) {
 
-    var instructionForCombobox = document.createElement('p');
-    var instructionForComboboxContent = document.createTextNode('Use enter key to select an option from the list.');
+    // window.addEventListener('load', function (e) {
+    // The timeout function is necessary to recognize the fields and the lists.
+    // setTimeout(function () {
+      var instructionForAll = document.querySelector('#block-mass-admin-theme-mainpagecontent .view-header') ? document.querySelector('#block-mass-admin-theme-mainpagecontent .view-header') : null;
+      var instructionForSrContent = document.createTextNode('Use tab key to navigate.');
 
-    var autoCompleteFields = document.querySelectorAll('.ui-autocomplete-input');
-    var optionLists = document.querySelectorAll('.ui-autocomplete');
+      var instructionForCombobox = document.createElement('p');
+      var instructionForComboboxContent = document.createTextNode('Use enter key to select an option from the list.');
 
-    // Add instructions for screen reader users.
-    if (instructionForAll && instructionForAll.innerHTML.includes('Search for content using any of the filters below.')) {
-      var instructionForSr = document.createElement('p');
-      instructionForSr.classList.add('visually-hidden');
-      instructionForSr.appendChild(instructionForSrContent);
-      instructionForAll.appendChild(instructionForSr);
-    }
+      var autoCompleteFields = document.querySelectorAll('.ui-autocomplete-input');
+      var optionLists = document.querySelectorAll('.ui-autocomplete');
 
-    // Set up aria-describedby content for auto complete field option box.
-    instructionForCombobox.setAttribute('id', 'comboboxInfo');
-    instructionForCombobox.setAttribute('style', 'display: none;');
-    instructionForCombobox.setAttribute('aria-hidden', 'true');
-    instructionForCombobox.appendChild(instructionForComboboxContent);
-    document.querySelector('main.page-content').appendChild(instructionForCombobox);
+      // Add instructions for screen reader users.
+      if (instructionForAll && instructionForAll.innerHTML.includes('Search for content using any of the filters below.')) {
+        var instructionForSr = document.createElement('p');
+        instructionForSr.classList.add('visually-hidden');
+        instructionForSr.appendChild(instructionForSrContent);
+        instructionForAll.appendChild(instructionForSr);
+      }
+
+      // Set up aria-describedby content for auto complete field option box.
+      instructionForCombobox.setAttribute('id', 'comboboxInfo');
+      instructionForCombobox.setAttribute('style', 'display: none;');
+      instructionForCombobox.setAttribute('aria-hidden', 'true');
+      instructionForCombobox.appendChild(instructionForComboboxContent);
+      document.querySelector('main.page-content').appendChild(instructionForCombobox);
 
 
 
-    optionLists.forEach(function (optionList) {
-      optionList.setAttribute('role', 'listbox');
-    });
-
-    autoCompleteFields.forEach(function (autoCompleteField, index) {
-      // Add missing accessibility components to pair a field and its combobox(option list).
-      autoCompleteField.setAttribute('role', 'combobox');
-      autoCompleteField.setAttribute('aria-autocomplete', 'list');
-      // Currently the listbox status is not used.
-      // autoCompleteField.setAttribute('aria-expanded', 'false');
-      autoCompleteField.setAttribute('aria-describedby', 'comboboxInfo');
-
-      // Get ID of the UL.
-      var listId = optionLists[index].getAttribute('id');
-      // Add aria-controls with the UL ID value.
-      // aria-controls doesn't work with VoiceOver.
-      autoCompleteField.setAttribute('aria-activedescendant', listId);
-
-      autoCompleteField.addEventListener('change', function (e) {
-        // Wait till the options are added to the list container .ui-autocomplete.
-        setTimeout(function () {
-          // Set role to LIs and their child As.
-          optionLists[index].querySelectorAll('.ui-menu-item').forEach(function (item) {
-            item.setAttribute('role', 'none');
-            item.querySelector('.ui-menu-item-wrapper').setAttribute('role', 'option');
-          });
-        }, 200);
+      optionLists.forEach(function (optionList) {
+        optionList.setAttribute('role', 'listbox');
       });
-    });
-  // }, 200);
-  });
+
+      autoCompleteFields.forEach(function (autoCompleteField, index) {
+        // Add missing accessibility components to pair a field and its combobox(option list).
+        autoCompleteField.setAttribute('role', 'combobox');
+        autoCompleteField.setAttribute('aria-autocomplete', 'list');
+        // Currently the listbox status is not used.
+        // autoCompleteField.setAttribute('aria-expanded', 'false');
+        autoCompleteField.setAttribute('aria-describedby', 'comboboxInfo');
+
+        // Get ID of the UL.
+        var listId = optionLists[index].getAttribute('id');
+        // Add aria-controls with the UL ID value.
+        // aria-controls doesn't work with VoiceOver.
+        autoCompleteField.setAttribute('aria-activedescendant', listId);
+
+        autoCompleteField.addEventListener('change', function (e) {
+          // Wait till the options are added to the list container .ui-autocomplete.
+          setTimeout(function () {
+            // Set role to LIs and their child As.
+            optionLists[index].querySelectorAll('.ui-menu-item').forEach(function (item) {
+              item.setAttribute('role', 'none');
+              item.querySelector('.ui-menu-item-wrapper').setAttribute('role', 'option');
+            });
+          }, 200);
+        });
+      });
+    // }, 200);
+    // });
+
+    }
+  };
 })();
