49b57d47fc42c308ac584586562f0b1fbe2c2add
Moshe Weitzman <weitzman@tejasa.com>
2022-10-27 07:37:22 -0400

    Change admin controller

---
+++ docroot/modules/custom/mass_admin_pages/src/EventSubscriber/RouteSubscriber.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\mass_admin_pages\EventSubscriber;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    if ($route = $collection->get('system.admin')) {
+      // $route_new = $collection->get('mass_admin_pages.author_home');
+      $route->setDefault('_controller', '\Drupal\mass_admin_pages\Controller\AuthorHomePageController::authorHome');
+    }
+  }
+
+}
