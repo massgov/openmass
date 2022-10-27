3a7cfb0b01a07addea141334a55c8a1f1efe7853
Moshe Weitzman <weitzman@tejasa.com>
2022-10-27 16:08:59 -0400

    Move help links

--- docroot/modules/custom/mass_gin/mass_gin.deploy.php
+++ docroot/modules/custom/mass_gin/mass_gin.deploy.php
@@ -15,11 +15,16 @@ use Drupal\workflows\Entity\Workflow;
  * Initial migration to from mass_admin_theme to Gin.
  */
 function mass_gin_deploy_initial(&$sandbox) {
-  // 'Taxonomy' and 'Menu' links
+  // 'Taxonomy' and 'Menu' links are not needed at top level of toolbar.
   foreach ([416, 426] as $id) {
     MenuLinkContent::load($id)->delete();
   }
   // D2d redirects link - move under Mass.
   $link =  MenuLinkContent::load(411);
   $link->set('parent', 'mass_admin_pages.mass')->save();
+
+  // Move links under Help.
+  foreach ([481, 471, 466, 476] as $id) {
+    MenuLinkContent::load($id)->set('parent', 'help.main')->save();
+  }
 }
