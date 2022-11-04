56e5bc981266b81cf5159ed4dfaf3ceae8681b96
Moshe Weitzman <weitzman@tejasa.com>
2022-11-04 15:37:34 -0400

    Improve block

--- docroot/modules/custom/mass_admin_pages/src/Plugin/Block/LocalTasksMovedNoticeBlock.php
+++ docroot/modules/custom/mass_admin_pages/src/Plugin/Block/LocalTasksMovedNoticeBlock.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\mass_admin_pages\Plugin\Block;
 
+use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Session\AccountInterface;
 
 /**
  * Provides a local tasks moved notice block.
@@ -19,7 +21,7 @@ class LocalTasksMovedNoticeBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $body = 'The Edit link has moved to the left toolbar. The <em>Revisions</em> and similar links have moved under the <em>Published</em> link in upper right.';
+    $body = 'The Edit link has moved to the left side of toolbar. The <em>Revisions</em> and similar links have moved under the <em>Published</em> link in upper right.';
     $title = 'Edit links moved';
     $markup = <<<EOM
 <div class="admonition tip">
@@ -32,6 +34,17 @@ EOM;
       '#cache' => ['max-age' => -1],
     ];
     return $build;
+  }
+
+  /**
+  * {@inheritdoc}
+  */
+  protected function blockAccess(AccountInterface $account) {
+    // @todo cacheability.
+    $node = \Drupal::routeMatch()->getParameter('node');
+    return AccessResult::allowedIfHasPermission($account, 'use mass dashboard')
+      ->andIf(AccessResult::allowedIf($node))
+      ->addCacheableDependency($node);
   }
 
   public function getCacheMaxAge() {
