0ee2b6ab83f53dd9629fbbf6f5ee264643400151
Arthur Baghdasaryan <62405127+arthurbaghdas@users.noreply.github.com>
2022-11-08 09:47:22 +0400

    DP-24421: Add test coverage for search autocomplete on www.mass.gov (#1776)
    * DP-24421: Add test coverage for search autocomplete on www.mass.gov
    
    * DP-24421: Add test coverage for search autocomplete on www.mass.gov
    
    * DP-24421: Add test coverage for search autocomplete on www.mass.gov
    
    * Code refactor
    
    * Code refactor
    
    * Try waitForElement() again
    
    Co-authored-by: Moshe Weitzman <weitzman@tejasa.com>

---
+++ docroot/modules/custom/mass_search/tests/src/ExistingSiteJavascript/MassSearchAutosuggestionTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\Tests\mass_seacrh\ExistingSiteJavascript;
+
+use Behat\Mink\Exception\ExpectationException;
+use weitzman\DrupalTestTraits\ExistingSiteSelenium2DriverTestBase;
+
+/**
+ * Tests "Search" autosuggestions.
+ */
+class MassSearchAutosuggestionTest extends ExistingSiteSelenium2DriverTestBase {
+
+  const SEARCH = 'health';
+
+  /**
+   * Asserts the banner search provides autosuggestions.
+   */
+  public function testHomepageBannerSearch() {
+    // Navigate to front page.
+    $this->drupalGet("");
+    $this->testSearch('banner');
+  }
+
+  /**
+   * Asserts the header search provides autosuggestions.
+   */
+  public function testHeaderSearch() {
+
+    // Navigate to internal page.
+    $this->drupalGet("/info-details/qag-information-details2");
+    $this->testSearch('header');
+  }
+
+  private function testSearch(string $identifier) {
+    $search_id = sprintf("#%s-search", $identifier);
+    $suggestion_id = sprintf("#%s-search-0", $identifier);
+    $assert_session = $this->assertSession();
+    $assert_session->elementExists('css', $search_id);
+
+    // Populate search input.
+    $fields = $this->getCurrentPage()->findAll('css', $search_id);
+    foreach ($fields as $field) {
+      if ($field->isVisible()) {
+        $field->setValue(self::SEARCH);
+      }
+    }
+    $suggestion_element = $this->assertSession()->waitForElement('css', $suggestion_id);
+
+    // Make sure the element has value.
+    $this->assertNotNull($suggestion_element->getText());
+
+    // Make sure the search value exists in the string.
+    $this->assertStringContainsString(self::SEARCH, $suggestion_element->getText());
+
+  }
+
+}
