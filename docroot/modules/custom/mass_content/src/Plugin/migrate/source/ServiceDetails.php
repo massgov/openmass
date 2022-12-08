532b5c4988ea4f67cd0129b778cc0aa14ba29602
Moshe Weitzman <weitzman@tejasa.com>
2022-12-08 13:14:03 -0500

    Remove conditions

--- docroot/modules/custom/mass_content/src/Plugin/migrate/source/ServiceDetails.php
+++ docroot/modules/custom/mass_content/src/Plugin/migrate/source/ServiceDetails.php
@@ -24,7 +24,6 @@ class ServiceDetails extends SqlBase {
     $query = $this->select('node', 'n')
       ->fields('n', ['nid'])
       ->condition('n.type', 'service_details');
-      // ->condition('n.nid', 384431);
     $query->innerJoin('node_field_data', 'nfd', 'nfd.nid=n.nid AND nfd.vid=n.vid');
     return $query;
   }
