27f9116f765fbc9f8cd575ef5329a579532f9be7
Moshe Weitzman <weitzman@tejasa.com>
2022-12-12 14:20:58 -0500

    Add script

---
+++ scripts/migrate_service_details.sh
@@ -0,0 +1,15 @@
+#Show starting status. Re-run this as desired to see status.
+drush migrate:status
+
+#This is the long migration that creates info_details nodes with all paragraphs.
+drush migrate:import service_details
+
+# Watches
+drush migrate:import flaggings
+
+# Migrate old redirects and add redirects from old service details alias to new node.
+drush migrate:import update_redirects
+drush migrate:import insert_redirects
+
+#Delete all service details nodes.
+drush entity:delete node --bundle=service_details
