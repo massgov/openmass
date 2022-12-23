fb05f35374c0ebb4aaf1c391dd7df8e9424c6ae6
Moshe Weitzman <weitzman@tejasa.com>
2022-12-23 10:23:45 -0500

    Update refs improvements

--- docroot/modules/custom/mass_content/src/Plugin/migrate/source/UpdateReferences.php
+++ docroot/modules/custom/mass_content/src/Plugin/migrate/source/UpdateReferences.php
@@ -3,6 +3,8 @@
 namespace Drupal\mass_content\Plugin\migrate\source;
 
 use Drupal\Core\Database\Query\SelectInterface;
+use Drupal\mass_content\Field\FieldType\DynamicLinkItem;
+use Drupal\migrate\MigrateSkipRowException;
 use Drupal\migrate\Plugin\migrate\source\SqlBase;
 use Drupal\migrate\Row;
 use Drupal\node\Entity\Node;
@@ -25,6 +27,8 @@ class UpdateReferences extends SqlBase {
       ->condition('eu.target_type', 'node')
       ->groupBy('eu.source_id');
     $query->innerJoin('migrate_map_service_details', 'mmsd', 'eu.target_id=mmsd.sourceid1');
+    // Limit to just the most revision in the entity_usage table
+    $query->innerJoin('node', 'n', 'eu.target_id=n.nid');
     $query->addExpression('COUNT(eu.source_id)', 'count');
     return $query;
   }
@@ -51,20 +55,40 @@ class UpdateReferences extends SqlBase {
   }
 
   public function prepareRow(Row $row) {
+    $changed = FALSE;
     /** @var \Drupal\mass_content\Entity\Bundle\node\NodeBundle $node */
     $node = Node::load($row->getSourceProperty('source_id'));
-    // Get all the fields we need to change in this sourceid.
+    // Get all the fields that we need to change in this sourceid.
     $query = $this->select('entity_usage', 'eu')
       ->fields('eu', ['method', 'field_name'])
+      ->fields('mmsd', ['sourceid1'])
       ->condition('eu.target_type', 'node')
-      ->condition('eu.source_id_id', $row->getSourceProperty('source_id'));
-    // ->addField('eu', 'source_id', 'source_id')
+      ->condition('eu.source_id', $row->getSourceProperty('source_id'))
+      ->condition('eu.source_vid', $node->getLoadedRevisionId());
     $query->addField('eu', 'target_id', 'reference_value_old');
     $query->addField('mmsd', 'destid1', 'reference_value_new');
+    $query->addField('n', 'type', 'content_type');
     $query->innerJoin('migrate_map_service_details', 'mmsd', 'eu.target_id=mmsd.sourceid1');
-    $refs = $query->execute()->fetchAllKeyed();
+    $query->innerJoin('node', 'n', 'mmsd.sourceid1=n.nid');
+    $refs = $query->execute()->fetchAll();
     foreach ($refs as $ref) {
-      $a=1;
+      $field_name = $ref['field_name'];
+      $list = $node->get($field_name);
+      foreach ($list as $delta => $item) {
+        switch (get_class($item)) {
+          case DynamicLinkItem::class:
+            // Only update the delta that was migrated (when there are multiple values).
+            if ($item->get('uri')->getString() == 'entity:node/' . $ref['reference_value_old']) {
+              $prop_name = "$field_name/$delta/uri";
+              $row->setDestinationProperty($prop_name, 'entity:node/' . $ref['reference_value_new']);
+              $changed = TRUE;
+            }
+            break;
+        }
+      }
+    }
+    if (!$changed) {
+      // throw new MigrateSkipRowException('No changes', TRUE);
     }
   }
 
