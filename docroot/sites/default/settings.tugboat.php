a0797ceab8133fad0aea36c83e2bbf062630af52
Moshe Weitzman <weitzman@tejasa.com>
2023-02-06 07:46:43 -0500

    Remove and block non-Azure logins for Drupal (#1867)
    * Remove manual login and TFA
    
    * Remove TFA modules
    
    * Remove Tugbat basic auth with link provinding justification
    
    * Add changelog
    
    * Increase autologout duration to be closer to Azure
    
    * Remove unnneeded behat test
    
    * Fixes
    
    * Remmove 3 password policy modules
    
    * Block passsword urls
    
    * Fix Behat test
    
    ---------
    
    Co-authored-by: joeg8612 <joseph.galluccio@state.ma.us>

--- docroot/sites/default/settings.tugboat.php
+++ docroot/sites/default/settings.tugboat.php
@@ -1,21 +1,23 @@
 <?php
 
 /**
+ * No longer needed as per https://docs.tugboatqa.com/faq/common-questions/#can-i-password-protect-my-preview-urls.
+ *
  * Password protect Tugboat environments.
  *
  * @see https://docs.acquia.com/articles/password-protect-your-non-production-environments-acquia-hosting#phpfpm
  */
-$agent = $_SERVER['HTTP_USER_AGENT'];
-if (php_sapi_name() !== 'cli' && !str_contains($agent, 'Chrome-Lighthouse') && !str_contains($agent, 'HeadlessChrome')) {
-  $username = getenv('LOWER_ENVIR_AUTH_USER');
-  $password = getenv('LOWER_ENVIR_AUTH_PASS');
-  $is_oauth = strpos($_SERVER['REQUEST_URI'], '/oauth/token') !== FALSE;
-  $is_endpoint = strpos($_SERVER['REQUEST_URI'], '/api/v1/') !== FALSE;
-  if (!$is_oauth && !$is_endpoint && !(isset($_SERVER['PHP_AUTH_USER']) && ($_SERVER['PHP_AUTH_USER']==$username && $_SERVER['PHP_AUTH_PW']==$password))) {
-    header('WWW-Authenticate: Basic realm="This site is protected"');
-    header('HTTP/1.0 401 Unauthorized');
-    // Fallback message when the user presses cancel / escape
-    echo 'Access denied';
-    exit;
-  }
-}
+//$agent = $_SERVER['HTTP_USER_AGENT'];
+//if (php_sapi_name() !== 'cli' && !str_contains($agent, 'Chrome-Lighthouse') && !str_contains($agent, 'HeadlessChrome')) {
+//  $username = getenv('LOWER_ENVIR_AUTH_USER');
+//  $password = getenv('LOWER_ENVIR_AUTH_PASS');
+//  $is_oauth = strpos($_SERVER['REQUEST_URI'], '/oauth/token') !== FALSE;
+//  $is_endpoint = strpos($_SERVER['REQUEST_URI'], '/api/v1/') !== FALSE;
+//  if (!$is_oauth && !$is_endpoint && !(isset($_SERVER['PHP_AUTH_USER']) && ($_SERVER['PHP_AUTH_USER']==$username && $_SERVER['PHP_AUTH_PW']==$password))) {
+//    header('WWW-Authenticate: Basic realm="This site is protected"');
+//    header('HTTP/1.0 401 Unauthorized');
+//    // Fallback message when the user presses cancel / escape
+//    echo 'Access denied';
+//    exit;
+//  }
+//}
