d5ce83458811d975a6ed2d4a4053b4313163307b
abaghdasaryan <arthur.baghdasar@gmail.com>
2023-03-14 12:48:18 +0400

    Entity hierarchy queue processing

--- docroot/modules/custom/mass_migrate/src/Commands/MassMigrateCommands.php
+++ docroot/modules/custom/mass_migrate/src/Commands/MassMigrateCommands.php
@@ -17,7 +17,21 @@ class MassMigrateCommands extends DrushCommands {
   public function overrideMigrateImportPreCommand() {
     // Usage updates now gfo into a queue which we will process at end of migration.
     $GLOBALS['config']['entity_usage.settings']['track_enabled_source_entity_types'] = ['placeholder', 'another'];
+    // Turn off entity_hierarchy writes before processing the items.
+    \Drupal::state()->set('entity_hierarchy_disable_writes', TRUE);
     \Drupal::service('config.factory')->clearStaticCache();
+    \Drupal::service('cache.discovery')->deleteAll();
+  }
+
+  /**
+   * Override config after running drush migrate:import.
+   *
+   * @hook post-command migrate:import
+   */
+  public function overrideMigrateImportPostCommand() {
+    // Turn on entity_hierarchy writes after processing the items.
+    \Drupal::state()->set('entity_hierarchy_disable_writes', FALSE);
+    \Drupal::service('cache.discovery')->deleteAll();
   }
 
 }
---
+++ docroot/modules/custom/mass_migrate/src/Plugin/Field/FieldType/EntityHierarchyOverriddenItem.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\mass_migrate\Plugin\Field\FieldType;
+
+use Drupal\entity_hierarchy\Plugin\Field\FieldType\EntityReferenceHierarchy;
+
+class EntityHierarchyOverriddenItem extends EntityReferenceHierarchy {
+  public function postSave($update) {
+    // Add the item to the queue to process later.
+    \Drupal::queue('entity_hierarchy_tracker')->createItem([
+      'field_item' => $this
+    ]);
+  }
+}
