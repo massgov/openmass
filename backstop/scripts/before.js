e0aaf82f6a37493dda489d3f3566b42ebbe43497
Tom Fleming <tom@lastcallmedia.com>
2023-05-03 17:15:04 -0400

    Update backstop/scripts/before.js

--- backstop/scripts/before.js
+++ backstop/scripts/before.js
@@ -20,7 +20,7 @@ module.exports = async (page, scenario, viewport, isReference, browserContext) =
   });
 
   await browserContext.setExtraHTTPHeaders(
-    {'mass-bypass-rate-limit': process.env.MASS_BYPASS_RATE_LIMIT}
+    {'Mass-Bypass-Rate-Limit': process.env.MASS_BYPASS_RATE_LIMIT}
   );
 
   let cookies = [
