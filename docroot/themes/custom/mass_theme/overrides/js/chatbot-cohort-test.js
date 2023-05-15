38abc5a75c50581e66d1afa05d47ba8e0536cb4e
Thomas Fleming <tom@lastcallmedia.com>
2023-05-15 16:58:35 -0400

    Updated alert message

--- docroot/themes/custom/mass_theme/overrides/js/chatbot-cohort-test.js
+++ docroot/themes/custom/mass_theme/overrides/js/chatbot-cohort-test.js
@@ -17,10 +17,11 @@ function loadChatbot(labelsIncluded = [], cohortsIncluded = [], totalCohorts = 1
       labelMatches.push(false);
     }
   });
+  console.log(labelMatches);
   const hasLabels = labelsIncluded.length > 0 && labelMatches.indexOf(false) === -1;
   const inCohort = cohortsIncluded.indexOf(assignedCohort) != -1;
   const stopChatbotAlert = parseInt(localStorage.getItem('stopMassgovChatbotAlert'));
-  const successMessage = `CHATBOT LOADED\r\n\r\nAvailable cohorts are ${cohortsIncluded}. Your cohort is ${assignedCohort}.\r\n\r\nLabels included are ${labelsIncluded}. This page's labels are ${labelsFound}.\r\n\r\nChange your mds-chatbot-cohort local storage value to one not available and load a page that does match all included labels to see failure message.`;
+  const successMessage = `CHATBOT LOADED\r\n\r\nAvailable cohorts are ${cohortsIncluded}. Your cohort is ${assignedCohort}.\r\n\r\nLabels included are ${labelsIncluded}. This page's labels are ${labelsFound}.\r\n\r\nChange your mds-chatbot-cohort local storage value to one not available and load a page that does not match all included labels to see failure message.`;
   const failureMessage = `NO CHATBOT\r\n\r\nAvailable cohorts are ${cohortsIncluded}. Your cohort is ${assignedCohort}.\r\n\r\nLabels included are ${labelsIncluded}. This page's labels are ${labelsFound}.\r\n\r\nChange your mds-chatbot-cohort local storage value to one available and load a page that matches all included labels to see success message.`;
   if (hasLabels && inCohort) {
     stopChatbotAlert !== 1 ? alert(successMessage) : console.log(successMessage);
