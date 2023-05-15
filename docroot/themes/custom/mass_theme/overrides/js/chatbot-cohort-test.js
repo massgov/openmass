df9b9cdd9e5e9f7e2a280f58a03b86a3ecb90d29
Thomas Fleming <tom@lastcallmedia.com>
2023-05-15 16:06:32 -0400

    Fixed compiled JS issue

--- docroot/themes/custom/mass_theme/overrides/js/chatbot-cohort-test.js
+++ docroot/themes/custom/mass_theme/overrides/js/chatbot-cohort-test.js
@@ -7,7 +7,7 @@ function loadChatbot(labelsIncluded = [], cohortsIncluded = [], totalCohorts = 1
     localStorage.setItem('massgovChatbotCohort', Math.floor(Math.random() * totalCohorts) + 1);
   }
   const assignedCohort = parseInt(localStorage.getItem('mds-chatbot-cohort'));
-  const labelsFound = document.querySelector(`meta[name="mg_labels"]`).content;
+  const labelsFound = document.querySelector(`meta[name="mg_labels"]`).getAttribute('content');
   let labelMatches = [];
   labelsIncluded.forEach((label) => {
     if (labelsFound.includes(label)) {
