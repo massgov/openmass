9dbe46a0f25094fb4e50fdbb43648e65de18da17
abaghdasaryan <arthur.baghdasar@gmail.com>
2023-05-31 12:45:09 +0400

    DP-28303: Entity Usage regenerate new command

---
+++ docroot/modules/custom/mass_entity_usage/src/Commands/MassEntityUsageCommands.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\mass_entity_usage\Commands;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drush\Commands\DrushCommands;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\mass_entity_usage\EntityUsageQueueBatchManager;
+
+/**
+ * Mass Entity usage drush commands.
+ */
+class MassEntityUsageCommands extends DrushCommands {
+
+  /**
+   * The Entity Usage queue batch manager.
+   *
+   * @var \Drupal\mass_entity_usage\EntityUsageQueueBatchManager
+   */
+  protected $queueBatchManager;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The entity usage configuration.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $entityUsageConfig;
+
+  /**
+   * The database service.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(EntityUsageQueueBatchManager $queue_batch_manager, EntityTypeManagerInterface $entity_type_manager, ConfigFactoryInterface $config_factory, $database) {
+    parent::__construct();
+    $this->queueBatchManager = $queue_batch_manager;
+    $this->entityTypeManager = $entity_type_manager;
+    $this->entityUsageConfig = $config_factory->get('entity_usage.settings');
+    $this->database = $database;
+  }
+
+  /**
+   * Recreate all entity usage statistics.
+   *
+   * @command mass-content:usage-regenerate
+   * @aliases maur,mass-usage-regenerate
+   * @option batch-size
+   *   When --use-queue is used, the queue will be populated in a batch process
+   *   to avoid memory issues. The --batch-size flag can be optionally used to
+   *   specify the batch size, for example --batch-size=500.
+   */
+  public function recreate($options = ['batch-size' => 1000]) {
+    $this->database->delete('queue')->condition('name', 'entity_usage_tracker')->execute();
+    $this->queueBatchManager->populateQueue($options['batch-size']);
+    drush_backend_batch_process();
+  }
+
+}
