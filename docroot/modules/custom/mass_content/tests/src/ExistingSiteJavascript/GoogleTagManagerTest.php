354b45ec16192eadb507ba0c17c672f34a273de1
abaghdasaryan <arthur.baghdasar@gmail.com>
2023-06-13 17:11:07 +0400

    Refactor

--- docroot/modules/custom/mass_content/tests/src/ExistingSiteJavascript/GoogleTagManagerTest.php
+++ docroot/modules/custom/mass_content/tests/src/ExistingSiteJavascript/GoogleTagManagerTest.php
@@ -9,6 +9,30 @@ use weitzman\DrupalTestTraits\ExistingSiteSelenium2DriverTestBase;
  */
 class GoogleTagManagerTest extends ExistingSiteSelenium2DriverTestBase {
 
+  const QAG_PATHS = [
+    "/audit/qag-binderaudit",
+    "/report/qag-binderreport",
+    "/qagcampaign-landing-with-solid-color-key-message-header",
+    "/lists/qag-curatedlist",
+    "/mandate/qag-decisionmandate",
+    "/decision-tree/qag-decision-tree",
+    "/executive-orders/no-1-qag-executiveorder",
+    "/forms/qag-formwithfileuploads",
+    "/guides/qag-guide",
+    "/how-to/qag-request-help-with-a-computer-problem",
+    "/info-details/qag-info-detail-with-landing-page-features",
+    "/location-details/qag-locationdetails",
+    "/locations/qag-locationgeneral1",
+    "/news/qag-newsnews",
+    "/orgs/qag-executive-office-of-technology-services-and-security",
+    "/regulations/900-CMR-2-qag-regulation-title",
+    "/person/qag-person-boardmember-role",
+    "/trial-court-rules/qag-rulesofcourt",
+    "/service-details/qag-servicedetails",
+    "/qag-service1",
+    "/topics/qag-topicpage1",
+  ];
+
   /**
    * Tests GTM on Homepage.
    */
@@ -25,17 +49,12 @@ class GoogleTagManagerTest extends ExistingSiteSelenium2DriverTestBase {
    * Tests GTM on random published page.
    */
   public function testGoogleTagManagerOnRandomPage() {
-    $result = \Drupal::entityQuery('node')
-      ->condition('status', 1)
-      ->addTag('sort_by_random')
-      ->range(0, 1)
-      ->execute();
-    $nid = reset($result);
-    $this->drupalGet('node/' . $nid);
+    $paths = self::QAG_PATHS;
+    $path = $paths[array_rand($paths)];
+    $this->drupalGet($path);
     $this->assertSession()->elementExists('css', 'script[src="https://www.googletagmanager.com/gtm.js?id=GTM-MPHNMQ"]');
     $data_layer = $this->getSession()->evaluateScript('window.dataLayer.length > 0');
     $this->assertTrue($data_layer, 'DataLayer is missing on the Page.');
-
   }
 
 }
