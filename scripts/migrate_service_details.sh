79e2024ee2685e719e8a8f44cf1f12732f21402a
Moshe Weitzman <weitzman@tejasa.com>
2023-08-10 11:51:31 -0400

    Add a drush cr

--- scripts/migrate_service_details.sh
+++ scripts/migrate_service_details.sh
@@ -2,7 +2,7 @@
 # - Ensure you have a backup before running on Prod.
 
 # Disable entity hierarchy. Not needed as the migration does ther disable itself.
-drush sset entity_hierarchy_disable_writes 1
+drush sset entity_hierarchy_disable_writes 1 && drush cr
 
 #Show migration status. Re-run anytime to see the current status.
 drush migrate:status
@@ -11,8 +11,7 @@ drush migrate:status
 drush migrate:import service_details
 
 # Disable again because it got enabled by migrate:import post hook
-drush sset entity_hierarchy_disable_writes 1
-drush cr
+drush sset entity_hierarchy_disable_writes 1 && drush cr
 
 # After service details migration make sure that the new usage data is populated before running the other ones.
 drush queue:run entity_usage_tracker
@@ -21,42 +20,37 @@ drush queue:run entity_usage_tracker
 drush migrate:import update_references_node
 
 # Disable again because it got enabled by migrate:import post hook
-drush sset entity_hierarchy_disable_writes 1
-drush cr
+drush sset entity_hierarchy_disable_writes 1 && drush cr
 
 #This updates entity refs and link fields and other usages of the old service_detail paragraphs
 drush migrate:import update_references_paragraph
 
 # Disable again because it got enabled by migrate:import post hook
-drush sset entity_hierarchy_disable_writes 1
-drush cr
+drush sset entity_hierarchy_disable_writes 1 && drush cr
 
 # Migrate old redirects and add redirects from old service details alias to new node.
 drush migrate:import update_redirects
 
 # Disable again because it got enabled by migrate:import post hook
-drush sset entity_hierarchy_disable_writes 1
-drush cr
+drush sset entity_hierarchy_disable_writes 1 && drush cr
 
 # Insert redirects from old node path aliases to new info details node. Uses node table.
 drush migrate:import insert_redirects
 
 # Disable again because it got enabled by migrate:import post hook
-drush sset entity_hierarchy_disable_writes 1
-drush cr
+drush sset entity_hierarchy_disable_writes 1 && drush cr
 
 # Watches
 drush migrate:import flaggings
 
 # Disable again because it got enabled by migrate:import post hook
-drush sset entity_hierarchy_disable_writes 1
-drush cr
+drush sset entity_hierarchy_disable_writes 1 && drush cr
 
 #Delete all service details nodes.
 drush entity:delete node --bundle=service_details
 
 # Re-enable entity hierarchy
-drush sset entity_hierarchy_disable_writes 0
+drush sset entity_hierarchy_disable_writes 0 && drush cr
 
 # Entity usage updates are queued up. Just let cron process them.
 # or manually run the command below
