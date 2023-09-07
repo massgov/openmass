db034ffde35903dab21d416092aabebdd280beb5
abaghdasaryan <arthur.baghdasar@gmail.com>
2023-09-07 16:54:15 +0400

    Event image fix

--- docroot/modules/custom/mass_schema_metatag/mass_schema_event/src/Plugin/metatag/Tag/SchemaEventImage.php
+++ docroot/modules/custom/mass_schema_metatag/mass_schema_event/src/Plugin/metatag/Tag/SchemaEventImage.php
@@ -28,4 +28,52 @@ use Drupal\schema_metatag\SchemaMetatagManager;
  */
 class SchemaEventImage extends SchemaImageObjectBase {
 
+  /**
+   * Generate a form element for this meta tag.
+   */
+  public function form(array $element = []) {
+    $form = parent::form($element);
+    $form['#attributes']['placeholder'] = '[node:summary]';
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function output(): array {
+    $value = SchemaMetatagManager::recomputeSerializedLength($this->value());
+    $value = unserialize($value);
+    $value = array_merge($value, [
+      '@type' => 'ImageObject',
+    ]);
+    $this->setValue(SchemaMetatagManager::serialize($value));
+
+    $element = parent::output();
+
+    if (!empty($element)) {
+      $element['#attributes']['content'] = [];
+      $images = SchemaMetatagManager::unserialize($this->value());
+
+      if (empty($images['url'])) {
+        return [];
+      }
+
+      $images = explode(', ', $images['url']);
+
+      foreach ($images as $url) {
+        // If it is null, continue;.
+        if (empty($url)) {
+          continue;
+        }
+
+        $element['#attributes']['content'][] = [
+          '@type' => 'ImageObject',
+          'url' => json_decode($url, TRUE),
+        ];
+      }
+    }
+
+    return $element;
+  }
+
 }
