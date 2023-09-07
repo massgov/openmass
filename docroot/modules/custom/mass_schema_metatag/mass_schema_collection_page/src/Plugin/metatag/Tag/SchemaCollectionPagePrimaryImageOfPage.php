afbb109b031348ff55575a4af2710e6c0ea96b15
abaghdasaryan <arthur.baghdasar@gmail.com>
2023-09-07 16:56:01 +0400

    Topic page image fix:

--- docroot/modules/custom/mass_schema_metatag/mass_schema_collection_page/src/Plugin/metatag/Tag/SchemaCollectionPagePrimaryImageOfPage.php
+++ docroot/modules/custom/mass_schema_metatag/mass_schema_collection_page/src/Plugin/metatag/Tag/SchemaCollectionPagePrimaryImageOfPage.php
@@ -3,6 +3,7 @@
 namespace Drupal\mass_schema_collection_page\Plugin\metatag\Tag;
 
 use Drupal\schema_metatag\Plugin\metatag\Tag\SchemaImageObjectBase;
+use Drupal\schema_metatag\SchemaMetatagManager;
 
 /**
  * Provides a plugin for 'schema_collection_page_primary_image_of_page' metatag.
@@ -25,5 +26,53 @@ use Drupal\schema_metatag\Plugin\metatag\Tag\SchemaImageObjectBase;
  * )
  */
 class SchemaCollectionPagePrimaryImageOfPage extends SchemaImageObjectBase {
+
+  /**
+   * Generate a form element for this meta tag.
+   */
+  public function form(array $element = []) {
+    $form = parent::form($element);
+    $form['#attributes']['placeholder'] = '[node:summary]';
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function output(): array {
+    $value = SchemaMetatagManager::recomputeSerializedLength($this->value());
+    $value = unserialize($value);
+    $value = array_merge($value, [
+      '@type' => 'ImageObject',
+    ]);
+    $this->setValue(SchemaMetatagManager::serialize($value));
+
+    $element = parent::output();
+
+    if (!empty($element)) {
+      $element['#attributes']['content'] = [];
+      $images = SchemaMetatagManager::unserialize($this->value());
+
+      if (empty($images['url'])) {
+        return [];
+      }
+
+      $images = explode(', ', $images['url']);
+
+      foreach ($images as $url) {
+        // If it is null, continue;.
+        if (empty($url)) {
+          continue;
+        }
+
+        $element['#attributes']['content'][] = [
+          '@type' => 'ImageObject',
+          'url' => $url,
+        ];
+      }
+    }
+
+    return $element;
+  }
 
 }
