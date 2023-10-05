a56d04d956eb8340d085049a9947a7f75915f571
abaghdasaryan <arthur.baghdasar@gmail.com>
2023-10-05 17:48:36 +0400

    Fixes

--- docroot/modules/custom/mass_content/src/Commands/MassContentCommands.php
+++ docroot/modules/custom/mass_content/src/Commands/MassContentCommands.php
@@ -249,7 +249,7 @@ class MassContentCommands extends DrushCommands {
     $rows = [];
     // Don't spam all the users with content update emails.
     $_ENV['MASS_FLAGGING_BYPASS'] = TRUE;
-    
+
     $entity_names = ['node_type', 'paragraphs_type'];
     foreach ($entity_names as $entity_name) {
       if ($entity_name == 'node_type') {
