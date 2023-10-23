b57b157d563007add3cad79f8ffe44f26343770e
Moshe Weitzman <weitzman@tejasa.com>
2023-10-23 16:11:02 -0400

    Fix DateTime::__construct(): Passing null to parameter #1 () of type string is deprecated DeployCommands.php:606

--- drush/Commands/DeployCommands.php
+++ drush/Commands/DeployCommands.php
@@ -603,7 +603,7 @@ EOT;
    * @return string
    */
   private function getTimestamp() {
-    return (new \DateTime(NULL, new \DateTimeZone('America/New_York')))->format('Y-m-d g:i:s A');
+    return (new \DateTime('now', new \DateTimeZone('America/New_York')))->format('Y-m-d g:i:s A');
   }
 
   /**
