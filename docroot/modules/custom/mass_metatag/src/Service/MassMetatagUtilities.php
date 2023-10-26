d7a76550553b954c3d8dfe5903fffd09a0ca61b7
abaghdasaryan <arthur.baghdasar@gmail.com>
2023-10-26 10:49:46 +0400

    DP-29261: Remove parent organizations from the mg_organization metadata field

--- docroot/modules/custom/mass_metatag/src/Service/MassMetatagUtilities.php
+++ docroot/modules/custom/mass_metatag/src/Service/MassMetatagUtilities.php
@@ -63,10 +63,10 @@ class MassMetatagUtilities {
             $result[] = $this->slugify(trim($node->label()));
           }
         }
-        // If there is a parent org, add it to the array to check.
-        if (!$node->field_parent->isEmpty() && !is_null($node->field_parent->entity) && !in_array($node->field_parent->entity->id(), $checked_orgs)) {
-          $orgs[] = $node->field_parent->entity;
-          if ($parent_only) {
+        else {
+          // If there is a parent org, add it to the array to check.
+          if (!$node->field_parent->isEmpty() && !is_null($node->field_parent->entity) && !in_array($node->field_parent->entity->id(), $checked_orgs)) {
+            $orgs[] = $node->field_parent->entity;
             if ($parent_meta) {
               $result[$node->field_parent->entity->id()] = [
                 'title' => $node->field_parent->entity->getTitle(),
