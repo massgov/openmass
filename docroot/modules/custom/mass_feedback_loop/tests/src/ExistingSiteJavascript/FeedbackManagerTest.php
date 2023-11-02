ed7e2d872cff5c11ea7cceccb5de6ef97e9b22ee
Thomas Fleming <tom@lastcallmedia.com>
2023-11-01 23:55:00 -0400

    Added text search for single and multiple word search

--- docroot/modules/custom/mass_feedback_loop/tests/src/ExistingSiteJavascript/FeedbackManagerTest.php
+++ docroot/modules/custom/mass_feedback_loop/tests/src/ExistingSiteJavascript/FeedbackManagerTest.php
@@ -12,7 +12,7 @@ use weitzman\LoginTrait\LoginTrait;
 /**
  * Tests "Feedback Manager" page at admin/ma-dash/feedback.
  */
-class FeedbackMgrTest extends ExistingSiteSelenium2DriverTestBase {
+class FeedbackManagerTest extends ExistingSiteSelenium2DriverTestBase {
 
   use LoginTrait;
 
@@ -22,6 +22,13 @@ class FeedbackMgrTest extends ExistingSiteSelenium2DriverTestBase {
    * @var \Behat\Mink\Element\DocumentElement
    */
   protected $page;
+
+  /**
+   * The rendered results on the page..
+   *
+   * @var \Behat\Mink\Element\DocumentElement
+   */
+  protected $results;
 
   /**
    * {@inheritdoc}
@@ -38,9 +45,49 @@ class FeedbackMgrTest extends ExistingSiteSelenium2DriverTestBase {
     $admin->save();
     $this->drupalLogin($admin);
 
-    // Visiting the view.
+    // Visiting the Feedback Manager page.
     $this->drupalGet('admin/ma-dash/feedback');
-    $this->view = $this->page->find('css', '#mass-feedback-loop-author-interface-form');
+    $this->results = $this->page->find('css', '#mass-feedback-loop-author-interface-form');
+  }
+
+  /**
+   * Resets filters.
+   */
+  private function reset() {
+    $this->results->hasButton('Reset') ? $this->results->pressButton('Reset') : NULL;
+  }
+
+  /**
+   * Asserts that filter shows results.
+   */
+  private function checkFilterHasResults() {
+    $table = $this->results->find('css', '#feedback-table');
+
+    // No results scenario.
+    $this->assertStringNotContainsString('No feedback available', $table->getText());
+
+    $rows = $table->findAll('css', 'tbody > tr');
+    $rowCount = count($rows);
+    $this->assertGreaterThanOrEqual(2, $rowCount);
+  }
+
+  /**
+   * Asserts a textbox filtering users works.
+   */
+  private function checkTextSearch($filter, $value) {
+    $this->reset();
+    $this->results->findField($filter)->setValue($value);
+    $this->getSession()->wait(1000);
+    $this->results->pressButton('Filter');
+    $this->checkFilterHasResults();
+  }
+
+  /**
+   * Tests a few things for the "Feedback Manager" page at admin/ma-dash/feedback.
+   */
+  public function testFilters() {
+    $this->checkTextSearch('Search feedback for specific text', 'help');
+    $this->checkTextSearch('Search feedback for specific text', 'help, officer, health');
   }
 
 }
