b1fb3974b3cf9fb9da8a24fba138bd6e4ea19264
abaghdasaryan <arthur.baghdasar@gmail.com>
2023-11-07 10:32:24 +0400

    initial changes

--- docroot/modules/custom/mass_fields/src/Plugin/Filter/FilterRichtextIndent.php
+++ docroot/modules/custom/mass_fields/src/Plugin/Filter/FilterRichtextIndent.php
@@ -11,7 +11,7 @@ use Drupal\filter\Plugin\FilterBase;
  * @Filter(
  *   id = "filter_richtext_indent",
  *   title = @Translation("Add css hooks to indent each child heading and its nested contents"),
- *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_IRREVERSIBLE,
  * )
  */
 class FilterRichtextIndent extends FilterBase {
