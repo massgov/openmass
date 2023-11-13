8d3efac32075191acf64e2ff52eed0b9230f4086
Moshe Weitzman <weitzman@tejasa.com>
2023-11-13 11:47:19 -0500

    Add sett

--- docroot/index.php
+++ docroot/index.php
@@ -12,8 +12,13 @@ use Drupal\Core\DrupalKernel;
 use Drupal\prod_no_redirect\ProdNoRedirectDrupalKernel;
 use Symfony\Component\HttpFoundation\Request;
 
-// As suggested by https://www.drupal.org/project/reverse_proxy_header
-$_SERVER['HTTP_X_FORWARDED_FOR'] = $_SERVER['HTTP_TRUE_CLIENT_IP'];
+/**
+ * Force Drupal to use Akamai header even without knowing all the possible IPs before it.
+ * As suggested by https://www.drupal.org/project/reverse_proxy_header
+ */
+if (isset($_SERVER['HTTP_TRUE_CLIENT_IP'])) {
+  $_SERVER['HTTP_X_FORWARDED_FOR'] = $_SERVER['HTTP_TRUE_CLIENT_IP'];
+}
 
 $autoloader = require_once 'autoload.php';
 
