2ae6357e3c83fc552ec82cf8b6b338df78b9c48f
Moshe Weitzman <weitzman@tejasa.com>
2023-11-13 12:31:56 -0500

    Upper

--- docroot/sites/default/settings.acquia.php
+++ docroot/sites/default/settings.acquia.php
@@ -51,7 +51,7 @@ if (!$cli && ($is_prod || $is_mass_gov)) {
  * Set the HTTP header name which stores real client IP. Harmless if not provided.
  * See https://www.drupal.org/project/reverse_proxy_header.
  */
-$settings['reverse_proxy_header'] = 'True-Client-IP';
+$settings['reverse_proxy_header'] = 'TRUE-CLIENT-IP';
 //# Note customization at top of index.php
 //$settings['reverse_proxy'] = TRUE;
 //# $settings['reverse_proxy_addresses'] = ['a.b.c.d', 'e.f.g.h/24', ...];
