2a5a929630457043a5a5f795c7f4873cf565fe6f
Moshe Weitzman <weitzman@tejasa.com>
2023-11-13 12:15:12 -0500

    Just use the module

--- docroot/tmp.php
+++ docroot/tmp.php
@@ -8,9 +8,9 @@ use Symfony\Component\HttpFoundation\Request;
  * Force Drupal to use trusted Akamai header even without knowing all the possible IPs before it.
  * As suggested by https://www.drupal.org/project/reverse_proxy_header
  */
-if (isset($_SERVER['HTTP_TRUE_CLIENT_IP'])) {
-  $_SERVER['HTTP_X_FORWARDED_FOR'] = $_SERVER['HTTP_TRUE_CLIENT_IP'];
-}
+//if (isset($_SERVER['HTTP_TRUE_CLIENT_IP'])) {
+//  $_SERVER['HTTP_X_FORWARDED_FOR'] = $_SERVER['HTTP_TRUE_CLIENT_IP'];
+//}
 
 $autoloader = require_once 'autoload.php';
 
