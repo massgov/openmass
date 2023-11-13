20c4975938720b625733932ae6aa617e73566444
Moshe Weitzman <weitzman@tejasa.com>
2023-11-13 11:27:37 -0500

    debug

--- docroot/tmp.php
+++ docroot/tmp.php
@@ -1,3 +1,16 @@
 <?php
 
+use Drupal\Core\DrupalKernel;
+use Drupal\prod_no_redirect\ProdNoRedirectDrupalKernel;
+use Symfony\Component\HttpFoundation\Request;
+
+$autoloader = require_once 'autoload.php';
+
+$kernel = new ProdNoRedirectDrupalKernel('prod', $autoloader);
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+
+print \Drupal::request()->getClientIp();
+
 print_r($_SERVER);
