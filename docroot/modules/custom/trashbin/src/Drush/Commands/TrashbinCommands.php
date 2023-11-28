16838f9c62c961bd75ea3d163ebeec5499f091ee
Moshe Weitzman <weitzman@tejasa.com>
2023-11-28 12:59:14 -0500

    Fixup for Drupal 10 and Drush 12

--- docroot/modules/custom/trashbin/src/Drush/Commands/TrashbinCommands.php
+++ docroot/modules/custom/trashbin/src/Drush/Commands/TrashbinCommands.php
@@ -1,40 +1,43 @@
 <?php
 
-namespace Drupal\trashbin\Commands;
+namespace Drupal\trashbin\Drush\Commands;
 
-use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
 use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drush\Attributes as CLI;
 use Drush\Commands\DrushCommands;
 use Drush\Drush;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class TrashbinCommands extends DrushCommands {
+final class TrashbinCommands extends DrushCommands {
 
   const TRASHBIN_ONLY_TRASH = 'trashbin_only_trash';
 
-  private EntityTypeManagerInterface $etm;
-  private TimeInterface $time;
+  const TRASHBIN_PURGE = 'trashbin:purge';
 
-  public function __construct(EntityTypeManagerInterface $etm, TimeInterface $time) {
-    $this->etm = $etm;
-    $this->time = $time;
+  protected function __construct(
+    private EntityTypeManagerInterface $etm,
+    private TimeInterface $time) {
+  }
+
+  public static function create(ContainerInterface $container): self
+  {
+    $commandHandler = new static(
+      $container->get('entity_type.manager'),
+      $container->get('datetime.time'),
+    );
+
+    return $commandHandler;
   }
 
   /**
    * Delete content entities that have been in the bin for more than n days.
-   *
-   * @param string $entity_type
-   *   Entity type to purge.
-   *
-   * @option $max
-   *   Maximum number of entities to delete.
-   * @option $days-ago
-   *   Number of days that the item must be unchanged in the trashbin.
-   * @usage drush --simulate trashbin:purge node
-   *   Get a report of what would be purged.
-   *
-   * @command trashbin:purge
    */
+  #[CLI\Command(name: self::TRASHBIN_PURGE, aliases: [])]
+  #[CLI\Argument(name: 'entity_type', description: 'Entity type to purge')]
+  #[CLI\Option(name: 'max', description: 'Maximum number of entities to delete.')]
+  #[CLI\Option(name: 'days-ago', description: 'Number of days that the item must be unchanged in the trashbin.')]
+  #[CLI\Usage(name: 'drush --simulate trashbin:purge node', description: 'Get a report of what would be purged.')]
   public function purge($entity_type, $options = ['max' => 1000, 'days-ago' => 180]) {
     $maximum = strtotime($options['days-ago'] . ' days ago', $this->time->getCurrentTime());
     $storage = $this->etm->getStorage($entity_type);
