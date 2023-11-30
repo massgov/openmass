5cece7a05b9e29f4574fe2107e5c349ad0b66157
Arthur Baghdasaryan <62405127+arthurbaghdas@users.noreply.github.com>
2023-11-30 20:25:34 +0400

    DP-27538: Investigate 500 errors experienced by authors (#1928)
    * DP-27538: Investigate 500 errors experienced by authors
    
    * Added changelog - DP-27538
    
    * removed dump
    
    * Fixed use order
    
    ---------
    
    Co-authored-by: Thomas Fleming <tom@lastcallmedia.com>
    Co-authored-by: Sally Young <github@justa.fish>
    Co-authored-by: Moshe Weitzman <weitzman@tejasa.com>

--- docroot/modules/custom/mayflower/src/Render/SvgProcessor.php
+++ docroot/modules/custom/mayflower/src/Render/SvgProcessor.php
@@ -3,11 +3,19 @@
 namespace Drupal\mayflower\Render;
 
 use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Asset\AssetCollectionRendererInterface;
+use Drupal\Core\Asset\AssetResolverInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Form\EnforcedResponseException;
 use Drupal\Core\Render\AttachmentsInterface;
 use Drupal\Core\Render\AttachmentsResponseProcessorInterface;
 use Drupal\Core\Render\HtmlResponse;
+use Drupal\Core\Render\HtmlResponseAttachmentsProcessor;
+use Drupal\Core\Render\RendererInterface;
 use Drupal\mayflower\Helper;
 use Drupal\views\Ajax\ViewAjaxResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Response attachments processor to dump SVGs to a single block on the page.
@@ -25,20 +33,38 @@ use Drupal\views\Ajax\ViewAjaxResponse;
  * placeholder will be swapped with an SVG use reference, embedding the SVG once
  * and using it everywhere.
  */
-class SvgProcessor implements AttachmentsResponseProcessorInterface {
+class SvgProcessor extends HtmlResponseAttachmentsProcessor {
 
   /**
-   * The decorated attachment processor.
+   * The HTML response attachments processor service.
    *
    * @var \Drupal\Core\Render\AttachmentsResponseProcessorInterface
    */
-  private $inner;
+  protected AttachmentsResponseProcessorInterface $htmlResponseAttachmentsProcessor;
 
   /**
-   * Constructor.
+   * Constructs a SvgProcessor object.
+   *
+   * @param \Drupal\Core\Render\AttachmentsResponseProcessorInterface $html_response_attachments_processor
+   *   The HTML response attachments processor service.
+   * @param \Drupal\Core\Asset\AssetResolverInterface $asset_resolver
+   *   An asset resolver.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   A config factory for retrieving required config objects.
+   * @param \Drupal\Core\Asset\AssetCollectionRendererInterface $css_collection_renderer
+   *   The CSS asset collection renderer.
+   * @param \Drupal\Core\Asset\AssetCollectionRendererInterface $js_collection_renderer
+   *   The JS asset collection renderer.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The request stack.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler service.
    */
-  public function __construct(AttachmentsResponseProcessorInterface $inner) {
-    $this->inner = $inner;
+  public function __construct(AttachmentsResponseProcessorInterface $html_response_attachments_processor, AssetResolverInterface $asset_resolver, ConfigFactoryInterface $config_factory, AssetCollectionRendererInterface $css_collection_renderer, AssetCollectionRendererInterface $js_collection_renderer, RequestStack $request_stack, RendererInterface $renderer, ModuleHandlerInterface $module_handler) {
+    $this->htmlResponseAttachmentsProcessor = $html_response_attachments_processor;
+    parent::__construct($asset_resolver, $config_factory, $css_collection_renderer, $js_collection_renderer, $request_stack, $renderer, $module_handler);
   }
 
   /**
@@ -46,6 +72,16 @@ class SvgProcessor implements AttachmentsResponseProcessorInterface {
    */
   public function processAttachments(AttachmentsInterface $response) {
     if ($response instanceof HtmlResponse) {
+
+      // (Note this is copied verbatim from
+      // \Drupal\Core\Render\HtmlResponseAttachmentsProcessor::processAttachments)
+      try {
+        $response = $this->renderPlaceholders($response);
+      }
+      catch (EnforcedResponseException $e) {
+        return $e->getResponse();
+      }
+
       $content = $response->getContent();
       $attached = $response->getAttachments();
       $inlined = [];
@@ -70,7 +106,7 @@ class SvgProcessor implements AttachmentsResponseProcessorInterface {
       $content = str_replace('<svg-sprite-placeholder>', Helper::wrapInlinedSvgs($inlined), $content);
       $response->setContent($content);
       $response->setAttachments($attached);
-      return $this->inner->processAttachments($response);
+      return $this->htmlResponseAttachmentsProcessor->processAttachments($response);
     }
     elseif ($response instanceof ViewAjaxResponse) {
       $commands = &$response->getCommands();
@@ -95,10 +131,10 @@ class SvgProcessor implements AttachmentsResponseProcessorInterface {
           }
         }
       }
-      return $this->inner->processAttachments($response);
+      return $this->htmlResponseAttachmentsProcessor->processAttachments($response);
     }
     elseif ($response instanceof AjaxResponse) {
-      return $this->inner->processAttachments($response);
+      return $this->htmlResponseAttachmentsProcessor->processAttachments($response);
     }
     else {
       throw new \InvalidArgumentException('\Drupal\Core\Render\HtmlResponse instance expected.');
