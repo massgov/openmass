428da8f348f088d815fcd435a86d865be39ae80e
Yukiko Gannett <yukikogannett@gmail.com>
2023-12-21 12:51:54 -0500

    adjust caption content check conditions

--- docroot/themes/custom/mass_theme/overrides/js/responsive-table.js
+++ docroot/themes/custom/mass_theme/overrides/js/responsive-table.js
@@ -4,24 +4,32 @@
  */
 
 (function ($) {
-  "use strict";
+  'use strict';
 
-  document.querySelectorAll(".ma__table--responsive .ma__table").forEach(function (table) {
+  document.querySelectorAll('.ma__table--responsive .ma__table').forEach(function (table) {
     // Copy captions authors entered into Mayflower template format.
     // table-responsive.twig L.12
-    if ($(table).find("caption:not(.ma__table__caption)")) {
-      var userInput = $(table).find("caption:not(.ma__table__caption)");
-      var captionText = $(userInput).text();
-      var captionSnippet =
-        '<span class="ma__table__caption__content">' + captionText + "</span>";
+    var userInputCaption = $(table).find('caption:not(.ma__table__caption)');
+    // 2nd test prevents empty caption content container. Otherwise add extra space at the top of the table.
+    if ($(table).find('caption:not(.ma__table__caption)') && $(table).find('caption:not(.ma__table__caption)').text().length > 0) {
+      var captionText = $(userInputCaption).text();
+      var captionSnippet = '<span class="ma__table__caption__content">' + captionText + '</span>';
+
       $(table).find(".ma__table__caption").prepend(captionSnippet);
-      $(userInput).remove();
+      $(userInputCaption).remove();
+      // Override JS in Mayflower when caption has content.
+      $(table).find(".ma__table__caption").removeClass("hide");
     }
 
     // Check table cell count for .ma__table--wide.
     // table-responsive.twig L.4
-    if ($(table).find("th").length > 3) {
-      $(table).addClass("ma__table--wide");
+    // Note: Some author entered tables have td instead of th.
+    //       Also covers the case of no thead.
+    if (
+      $(table).find('thead th').length > 3 ||
+      $(table).find('thead').find('td').length > 3 ||
+      $(table).find('tbody tr:first-child *').length > 3) {
+      $(table).addClass('ma__table--wide');
     }
   });
 
