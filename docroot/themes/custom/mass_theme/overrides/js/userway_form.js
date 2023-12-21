e91e70cd0751ffd582517e7782c0beab29ede46c
abaghdasaryan <arthur.baghdasar@gmail.com>
2023-12-21 14:43:36 +0400

    Change implementation

---
+++ docroot/themes/custom/mass_theme/overrides/js/userway_form.js
@@ -0,0 +1,33 @@
+(function ($) {
+  'use strict';
+
+  Drupal.behaviors.userwayForm = {
+    attach: function (context) {
+      // Process the form.
+      $('form#fsForm5560495', context).each(function (index) {
+        var $form = $(this);
+        // This is to stop a double click submitting the form twice
+        var $submitBtn = $('button[type="submit"]', $form);
+        $form.submit(function () {
+          $submitBtn.prop('disabled', true);
+        });
+
+        $form.ajaxForm({
+          data: {jsonp: 1},
+          dataType: 'script',
+          success: function(response) {
+            $form.addClass('hidden');
+            $form.parent().append('<p>Thank you for providing your email.</p>')
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+            // Handle error scenario
+            $form.addClass('hidden');
+            $form.parent().append('<p style="color: red;">Something went wrong. Please refresh the page and try again.</p>')
+          }
+        });
+
+
+      });
+    }
+  }
+})(jQuery);
