60912692462fdc8b4340e73c216d54bbd72066e5
Yukiko Gannett <yukikogannett@gmail.com>
2023-12-22 17:51:17 -0500

    add mobile format for table

--- docroot/themes/custom/mass_theme/overrides/js/table.js
+++ docroot/themes/custom/mass_theme/overrides/js/table.js
@@ -10,10 +10,43 @@
 
   document.querySelectorAll('.ma__table').forEach(function (table) {
     // Set up assistive technology friendly tables.
-    // 1. Add row scope for accessibility.
-    $(table).find('tbody th').forEach(function (rowHeader) {
-      $(rowHeader).attr('scope', 'row');
+    // Add row scope for accessibility.
+    $(table).find('tbody th').each(function () {
+      $(this).attr('scope', 'row');
     });
+
+    // Mobile format for table
+    if ($(table).has('thead th').length > 0) {
+      // 1. Set up mobile headers
+      $(table).find('thead th').each(function (hIndex) {
+        var headerLabel = $(this).text();
+
+        // Loop each row.
+        $(table).find('tbody tr').each(function () {
+          var targetCells = $(this).find('td');
+
+          if ($(this).has("th").length > 0) {
+            hIndex--;
+          }
+
+          // Find matching column cell in each row to apply data-label.
+          $(targetCells).each(function (cellIndex) {
+            if (cellIndex == hIndex && (headerLabel.length > 0)) {
+              $(this).attr("data-label", headerLabel);
+            }
+          });
+        });
+      });
+    }
+    else {
+      // No row headers
+      if ($(window).width() < 621) {
+        $(table).find('tbody tr *').each(function () {
+          $(this).css('padding-left', '0');
+          $(this).css('width', '100%');
+        });
+      }
+    }
   });
 
 })(jQuery);
