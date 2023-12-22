aac5ccb7799d276dc2c24e910229576863bbdad4
Thomas Fleming <tom@lastcallmedia.com>
2023-12-22 17:00:03 -0500

    Some initial config migration

--- percy/percy.js
+++ percy/percy.js
@@ -1,10 +1,55 @@
 const { chromium } = require('playwright');
 const percySnapshot = require('@percy/playwright');
 
+const target = opts.length ? opts[0].replace('--target=', '') : 'prod';
+
 (async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto('http://example.com/', { waitUntil: 'networkidle' });
-  await percySnapshot(page, 'Example Site');
-  await browser.close();
+  const pages = require('./all');;
+
+  const scenarios = pages.map(function(page) {
+    let base = process.env.BASE_URL;
+    let auth = false;
+    let removeSelectors = [];
+
+    switch (target) {
+      case 'prod':
+        base = 'https://www.mass.gov';
+        break;
+      case 'local':
+        base = 'http://mass-web';
+        break;
+      case 'test':
+        auth = getAuth();
+        base = `https://${auth.username}:${auth.password}@stage.mass.gov`;
+        break;
+      case 'tugboat':
+        const opts = process.argv.filter(arg => arg.match(/^--tugboat=/))
+        if (opts.length < 1) {
+          throw '--tugboat must be specified with a preview URL if --target=tugboat is set.'
+        }
+        base = opts[0].replace('--tugboat=', '');
+        break;
+      default:
+        auth = getAuth();
+        base = `https://${auth.username}:${auth.password}@${target}.edit.mass.gov`;
+    }
+    const url = new URL(`${base}${page.url}`);
+    let separator = "?";
+    if (url.search !== "") {
+      separator = "&";
+    }
+    if (!page.showHeaderAlerts) {
+      removeSelectors.push('.pre-content .mass-alerts-block');
+    }
+    if (!page.showGlobalAlerts) {
+      removeSelectors.push('.mass-alerts-block[data-alerts-path="/alerts/sitewide"]');
+    }
+
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle' });
+    await percySnapshot(page, scenario.label);
+    await browser.close();
+  });
+
 })();
