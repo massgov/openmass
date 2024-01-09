7073067f9ac16fb14171cba4a01f0cd7dbc2d4b6
Moshe Weitzman <weitzman@tejasa.com>
2024-01-09 13:12:54 -0500

    Centralize code in new buncle class for address paragraph

---
+++ docroot/modules/custom/mass_content/src/Entity/Bundle/paragraph/ParagraphBundle.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\mass_content\Entity\Bundle\paragraph;
+
+use Drupal\paragraphs\Entity\Paragraph;
+
+/**
+ * A base bundle class for paragraph entities.
+ */
+abstract class ParagraphBundle extends Paragraph {
+
+}
