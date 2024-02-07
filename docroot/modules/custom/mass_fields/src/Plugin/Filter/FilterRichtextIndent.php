fc1d91f62806c74ea3c322396bee00f9c89f64f5
Yukiko Gannett <yukikogannett@gmail.com>
2024-02-07 15:28:52 -0500

    Comment out the duplicate lines in FilterRichtextTable.php for test

--- docroot/modules/custom/mass_fields/src/Plugin/Filter/FilterRichtextIndent.php
+++ docroot/modules/custom/mass_fields/src/Plugin/Filter/FilterRichtextIndent.php
@@ -26,10 +26,12 @@ class FilterRichtextIndent extends FilterBase {
     // Prep table for indentation + responsive.
     // Unique ID per table for accessibility to establish pairing between the buttons and the table container for responsible table.
     // $tableId = uniqid();
-    // $tableWrapperTop = '<div class="ma__rich-text__indent ma__table--responsive js-ma-responsive-table" data-ma-heading-parent="H' . $parentHeadingNum . '">
+    $tableWrapperTop = '<table>';
+    //'<div class="ma__rich-text__indent ma__table--responsive js-ma-responsive-table" data-ma-heading-parent="H' . $parentHeadingNum . '">
     // <div class="ma__table--responsive__wrapper" id="' . $tableId . '"  role="group" tabindex="-1">
     // <table class="ma__table"><caption id="tbl-' . $tableId . '" class="ma__table__caption"><span class="ma__table__caption__scroll-info"> (Table in a horizontal scrolling container)</span></caption>';
-    // $tableWrapperBottom = '</table></div></div>';
+    $tableWrapperBottom = '</table>';
+    //'</table></div></div>';
 
     // Test for all container elements available in the editor.
     // $nonNestedTableOpen = '/(?<!<td>)<table>/';
