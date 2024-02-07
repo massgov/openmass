4c5be7f6f5d3b48167d6be1aaf99fd1bab4be970
Yukiko Gannett <yukikogannett@gmail.com>
2024-02-07 15:29:28 -0500

    regex test to exclude nested tables

--- docroot/modules/custom/mass_fields/src/Plugin/Filter/FilterRichtextTable.php
+++ docroot/modules/custom/mass_fields/src/Plugin/Filter/FilterRichtextTable.php
@@ -24,15 +24,21 @@ class FilterRichtextTable extends FilterBase {
     // Exclude nested tables.
     $tableId = uniqid();
 
-    $tableWrapperTop = '<div class="ma__table--responsive js-ma-responsive-table">
-    <div class="ma__table--responsive__wrapper" id="' . $tableId . '" role="group" tabindex="-1">
-    <table class="ma__table"><caption id="tbl-' . $tableId . '" class="ma__table__caption"><span class="ma__table__caption__scroll-info"> (Table in a horizontal scrolling container)</span></caption>';
+    $tableWrapperTop = '<div class="ma__table--responsive js-ma-responsive-table"><div class="ma__table--responsive__wrapper" id="' . $tableId . '" role="group" tabindex="-1"><table class="ma__table"><caption id="tbl-' . $tableId . '" class="ma__table__caption"><span class="ma__table__caption__scroll-info"> (Table in a horizontal scrolling container)</span></caption>';
     $tableWrapperBottom = '</table></div></div>';
     $tableHeadingScope = '<th scope="col">';
 
-    // Exclude nesting tables from the string replacement.
-    $plainTableElements = ['/(?<!\<td>\s\s+\<p>&nbsp;\<\/p>\s\s+)<table>/', '/<\/table>(?!<\/td>)/', '<th>'];
-    // $plainTableElements = ['<table>', </table>, '<th>'];
+    // In RTE, '<p>&nbsp;</p>' is added as a cell is clicked. When a table is added to the cell the empty paragraph remains.
+    // Need to clean up spaces before and after nested tables in their parent <td> to avoid
+    // Warning: preg_replace(): Compilation failed: lookbehind assertion
+    $text = preg_replace('/<td>\s\s+\<p>&nbsp;\<\/p>\s\s+<table>/', '/<td><table>/', $text);
+    $text = preg_replace('/<\/table>\s\s+\<p>&nbsp;<\/p>\s\s+<\/td>/', '/<\/table><\/td>/', $text);
+
+    // Exclude nesting tables (<table> in <td>) from the string replacement.
+    // the lookahead and lookback are not working in Drupal:
+    // '/(?<!\<td>\<p>&nbsp;\<\/p>)<table>/` and '/<\/table>(?!\<\/td>)/'
+    // As regex, they work as tested in the online regex validator for PHP.
+    $plainTableElements = ['/(?<!\<td>\<p>&nbsp;\<\/p>)<table>/', '/<\/table>(?!\<\/td>)/', '/<th>/'];
     $responsiveTableElements = [$tableWrapperTop, $tableWrapperBottom, $tableHeadingScope];
 
     $output = preg_replace($plainTableElements, $responsiveTableElements, $text);
