a606caf93f586ab75949c4421fe17c82a64d02a9
Yukiko Gannett <yukikogannett@gmail.com>
2024-02-21 10:03:04 -0500

    remove duplicate code to FilterRichtextTable.php from FilterRichtextIndent.php

--- docroot/modules/custom/mass_fields/src/Plugin/Filter/FilterRichtextIndent.php
+++ docroot/modules/custom/mass_fields/src/Plugin/Filter/FilterRichtextIndent.php
@@ -23,19 +23,6 @@ class FilterRichtextIndent extends FilterBase {
     // Set default 2 since they are children of h2 until h3 shows up.
     $parentHeadingNum = 2;
 
-    // Prep table for indentation + responsive.
-    // Unique ID per table for accessibility to establish pairing between the buttons and the table container for responsible table.
-    // $tableId = uniqid();
-    $tableWrapperTop = '<table>';
-    //'<div class="ma__rich-text__indent ma__table--responsive js-ma-responsive-table" data-ma-heading-parent="H' . $parentHeadingNum . '">
-    // <div class="ma__table--responsive__wrapper" id="' . $tableId . '"  role="group" tabindex="-1">
-    // <table class="ma__table"><caption id="tbl-' . $tableId . '" class="ma__table__caption"><span class="ma__table__caption__scroll-info"> (Table in a horizontal scrolling container)</span></caption>';
-    $tableWrapperBottom = '</table>';
-    //'</table></div></div>';
-
-    // Test for all container elements available in the editor.
-    // $nonNestedTableOpen = '/(?<!<td>)<table>/';
-    // $nonNestedTableClose = '/</table>(?!</td>)/';
     $plainElements = [
       '<h3>', '<h4>', '<h5>', '<h6>', '<p>', '<ul>', '<ol>', '<blockquote>',
       '<drupal-entity',
@@ -50,8 +37,8 @@ class FilterRichtextIndent extends FilterBase {
       '<ul class="ma__rich-text__indent" data-ma-heading-parent="H' . $parentHeadingNum . '">',
       '<ol class="ma__rich-text__indent" data-ma-heading-parent="H' . $parentHeadingNum . '">',
       '<blockquote class="ma__rich-text__indent" data-ma-heading-parent="H' . $parentHeadingNum . '">',
-      $tableWrapperTop,
-      $tableWrapperBottom,
+      '<table>',
+      '</table>',
       '<drupal-entity class="ma__rich-text__indent" data-ma-heading-parent="H' . $parentHeadingNum . '" ',
     ];
 
