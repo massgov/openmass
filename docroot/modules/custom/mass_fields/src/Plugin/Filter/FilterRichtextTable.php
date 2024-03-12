73ebf08e738210375aa7f0f2dda7bd3b8e3a5166
Yukiko Gannett <yukikogannett@gmail.com>
2024-03-12 16:34:59 -0400

    match MF change in modoule

--- docroot/modules/custom/mass_fields/src/Plugin/Filter/FilterRichtextTable.php
+++ docroot/modules/custom/mass_fields/src/Plugin/Filter/FilterRichtextTable.php
@@ -24,7 +24,7 @@ class FilterRichtextTable extends FilterBase {
     // Exclude nested tables.
     $tableId = uniqid();
 
-    $tableWrapperTop = '<div class="ma__table--responsive js-ma-responsive-table"><div class="ma__table--responsive__wrapper" id="' . $tableId . '" role="group" tabindex="-1"><table class="ma__table"><caption id="tbl-' . $tableId . '" class="ma__table__caption"><span class="ma__table__caption__scroll-info"> (Table in a horizontal scrolling container)</span></caption>';
+    $tableWrapperTop = '<div class="ma__table--responsive js-ma-responsive-table"><div class="ma__table--responsive__wrapper" id="' . $tableId . '" role="group" tabindex="-1"><table class="ma__table"><caption id="tbl-' . $tableId . '" class="ma__table__caption"><span class="ma__table__caption__scroll-info"> Note: Table has hidden columns, scroll horizontally to see more.</span></caption>';
     $tableWrapperBottom = '</table></div></div>';
     $tableHeadingScope = '<th scope="col">';
 
