7db6dc174fb3d458619f198be7a34c56441a4a1e
abaghdasaryan <arthur.baghdasar@gmail.com>
2024-03-13 15:33:40 +0400

    Adding constraint for required field validation

---
+++ docroot/modules/custom/mass_validation/src/Plugin/Validation/Constraint/BackgroundImageRequiredConstraint.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\mass_validation\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Ensures field_image is filled when field_background_type is 'image'.
+ *
+ * @Constraint(
+ *   id = "BackgroundImageRequired",
+ *   label = @Translation("Background Image Required", context = "Validation"),
+ * )
+ */
+class BackgroundImageRequiredConstraint extends Constraint {
+  public $message = 'Background Image is required.';
+}
---
+++ docroot/modules/custom/mass_validation/src/Plugin/Validation/Constraint/BackgroundImageRequiredConstraintValidator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\mass_validation\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class BackgroundImageRequiredConstraintValidator extends ConstraintValidator {
+  public function validate($items, Constraint $constraint) {
+    $field_image = $items->getValue();
+    $paragraph = $items->getEntity();
+    if (empty($field_image)) {
+      if ($paragraph->get('field_background_type')->value == 'image') {
+        $this->context->buildViolation($constraint->message)->addViolation();
+      }
+    }
+  }
+}
