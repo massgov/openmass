349cea0a532bdffa8c8353158ff2d8322b0637f4
Yukiko Gannett <yukikogannett@gmail.com>
2024-03-18 14:56:01 -0400

    test edit2

--- docroot/themes/custom/mass_theme/overrides/js/location_list_filter.js
+++ docroot/themes/custom/mass_theme/overrides/js/location_list_filter.js
@@ -1,7 +1,8 @@
 (function ($, Drupal) {
   ('use strict');
 
-  console.log('panda');
+  console.log('apple');
+
   // Add id to location listing filter combobox options to pair with
   // its associated input field .ma__input pac-target-input.
   // '.pac-container' is an element by Google code.
@@ -17,16 +18,16 @@
       $(this).attr('aria-expanded', 'true');
     }
 
-    console.log($(this).attr('aria-expanded');
+    console.log($(this).attr('aria-expanded'));
   });
 
   $('#filter-by-location').focusout(function () {
-     console.log('close');
+    console.log('close');
 
     if ($('#filter-loction-options').children().length === 0) {
       $(this).attr('aria-expanded', 'false');
 
-      console.log($(this).attr('aria-expanded');
+      console.log($(this).attr('aria-expanded'));
     }
   });
 
