ec435e12fb170aaf6d01064a8ef686a3ef16e66e
Yukiko Gannett <yukikogannett@gmail.com>
2024-03-18 10:58:57 -0400

    add timeout function

--- docroot/themes/custom/mass_theme/overrides/js/location_list_filter.js
+++ docroot/themes/custom/mass_theme/overrides/js/location_list_filter.js
@@ -1,6 +1,9 @@
 (function ($, Drupal) {
   ('use strict');
-
-  // Set up combobox
-  $('.pac-container').attr('id', 'filter-loction-options');
+  // Add id to location listing filter combobox options to pair with
+  // its associated input field .ma__input pac-target-input.
+  // '.pac-container' is an element by Google code.
+  setTimeout(function () {
+    $('.pac-container').attr('id', 'filter-loction-options');
+  }, 250);
 })(jQuery, Drupal);
