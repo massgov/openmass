2aaba082406246596ba206e93db4a2e2dd2ebd0e
Yukiko Gannett <yukikogannett@gmail.com>
2024-03-19 11:18:12 -0400

    test comment

--- docroot/themes/custom/mass_theme/overrides/js/location_list_filter.js
+++ docroot/themes/custom/mass_theme/overrides/js/location_list_filter.js
@@ -1,40 +1,44 @@
-(function ($, Drupal) {
-  ('use strict');
-  // Add id to location listing filter combobox options to pair with
-  // its associated input field .ma__input pac-target-input.
-  // '.pac-container' is an element by Google code.
-  setTimeout(function () {
-    // eslint-disable-next-line quotes
-    $('.pac-container:nth-child(2)').attr('id', 'filter-loction-options');
-  }, 250);
+// (function ($, Drupal) {
+//   ('use strict');
+//   // Add id to location listing filter combobox options to pair with
+//   // its associated input field .ma__input pac-target-input.
+//   // '.pac-container' is an element by Google code.
+//   setTimeout(function () {
+//     // eslint-disable-next-line quotes
+//     $('.pac-container:nth-child(2)').attr({
+//       id: 'filter-loction-options',
+//       role: 'listbox',
+//       aria-label: ''
+//     });
+//   }, 250);
 
-  // Update option box status.
-  $('#filter-by-location').keyup(function () {
-    if ($('#filter-loction-options').children().length > 0) {
-      $(this).attr('aria-expanded', 'true');
-    }
-    else {
-      $(this).attr('aria-expanded', 'false');
-    }
+//   // Update option box status.
+//   $('#filter-by-location').keyup(function () {
+//     if ($('#filter-loction-options').children().length > 0) {
+//       $(this).attr('aria-expanded', 'true');
+//     }
+//     else {
+//       $(this).attr('aria-expanded', 'false');
+//     }
 
-    console.log($(this).attr('aria-expanded'));
-  });
+//     console.log($(this).attr('aria-expanded'));
+//   });
 
-  // $('.pac-container').each(function (i) {
-  //   i.on('click', function() {
-  //   console.log('clicked');
+//   // $('.pac-container').each(function (i) {
+//   //   i.on('click', function() {
+//   //   console.log('clicked');
 
-  //    $('#filter-by-location').attr('aria-expanded', 'false');
-  //   $(this).css('border', '3px solid red');
-  // });
+//   //    $('#filter-by-location').attr('aria-expanded', 'false');
+//   //   $(this).css('border', '3px solid red');
+//   // });
 
-  $('.pac-item').each(function (i) {
-    i.on('click', function () {
-      $(this).css('background', 'orange');
-      console.log('selected');
-      $('#filter-by-location').attr('aria-expanded', 'false');
-    });
-  });
+//   $('.pac-item').each(function (i) {
+//     i.on('click', function () {
+//       $(this).css('background', 'orange');
+//       console.log('selected');
+//       $('#filter-by-location').attr('aria-expanded', 'false');
+//     });
+//   });
 
 
-})(jQuery, Drupal);
+// })(jQuery, Drupal);
