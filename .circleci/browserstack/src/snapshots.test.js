878a70fff076d0ea5ddfc2c51ff960d8d3c046f3
Thomas Fleming <tom@lastcallmedia.com>
2024-03-22 13:25:55 -0400

    Updated changes for successful tests

--- .circleci/browserstack/src/snapshots.test.js
+++ .circleci/browserstack/src/snapshots.test.js
@@ -3,7 +3,7 @@ const { percy } = require('browserstack-node-sdk');
 
 describe("BStack demo test", () => {
   let driver;
-  const paths = require('../../../backstop/post-release');
+  const paths = require('../../../backstop/all');
 
   beforeAll(() => {
     driver = new Builder()
@@ -18,8 +18,14 @@ describe("BStack demo test", () => {
 
     paths.forEach(async (path) => {
         test(path.label + ' test', async () => {
-            await driver.get('https://stage.mass.gov' + path.url);
-            // await percy.screenshot(driver, path.label);
+          await driver.get('https://stage.mass.gov' + path.url);
+          let options = {
+            fullPage: true,
+            ignore_region_selectors: [
+              'div.ma__banner-credit',
+            ]
+          }
+          await percy.screenshot(driver, path.label, options);
         });
     });
 
