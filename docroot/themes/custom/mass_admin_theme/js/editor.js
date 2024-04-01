9470c25b2dd4e8edc00415eab0d7aafb94ab93e2
abaghdasaryan <arthur.baghdasar@gmail.com>
2024-04-01 11:47:01 +0400

    Ckeditor5 plugin for media entity download

--- docroot/themes/custom/mass_admin_theme/js/editor.js
+++ docroot/themes/custom/mass_admin_theme/js/editor.js
@@ -8,13 +8,6 @@
   /**
    * Change tool tip text for "Download Link" provided by media_entity_download js.
    */
-  Drupal.behaviors.editorTooltips = {
-    attach: function (context, settings) {
-      setTimeout(function () {
-        $('.cke_button__mediaentitydownload').attr('title', Drupal.t('Insert Link to Document'));
-      }, 1000);
-    }
-  };
 
   if (typeof CKEDITOR != 'undefined') {
     CKEDITOR.config.contentsLangDirection = 'auto';
