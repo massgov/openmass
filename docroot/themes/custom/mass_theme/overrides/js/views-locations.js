2b34708b066a0413c14149270cbf921036673ead
Yukiko Gannett <yukikogannett@gmail.com>
2024-04-11 15:48:28 -0400

    VO focus issue test edit 3

--- docroot/themes/custom/mass_theme/overrides/js/views-locations.js
+++ docroot/themes/custom/mass_theme/overrides/js/views-locations.js
@@ -178,30 +178,33 @@
         device.indexOf('iPhone') !== -1 ||
         device.indexOf('iPad') !== -1) {
 
-          setTimeout(function () {
-            focusTarget.focus();
-          }, 1000);
-
-          // var focusInterval = 10; // ms, time between function calls
-          // var focusTotalRepetitions = 100; // number of repetitions
-
-          // focusTarget.attr("tabindex", 0);
-          // focusTarget.blur();
-
-          // var focusRepetitions = 0;
-          // var interval = window.setInterval(function () {
-          //   focusTarget.focus();
-          //   focusRepetitions++;
-          //   if (focusRepetitions >= focusTotalRepetitions) {
-          //     window.clearInterval(interval);
-          //   }
-          // }, focusInterval);
-
-          // console.log($focusTarget.attr('tabindex'));
-
-          // setTimeout(function () {
-          //   focusTarget.focus();
-          // }, 100);
+      setTimeout(function () {
+        if (document.activeElement !== focusTarget) {
+          focusTarget.focus();
         }
+        console.log('ONE SECOND');
+      }, 1000);
+
+      // var focusInterval = 10; // ms, time between function calls
+      // var focusTotalRepetitions = 100; // number of repetitions
+
+      // focusTarget.attr("tabindex", 0);
+      // focusTarget.blur();
+
+      // var focusRepetitions = 0;
+      // var interval = window.setInterval(function () {
+      //   focusTarget.focus();
+      //   focusRepetitions++;
+      //   if (focusRepetitions >= focusTotalRepetitions) {
+      //     window.clearInterval(interval);
+      //   }
+      // }, focusInterval);
+
+      // console.log($focusTarget.attr('tabindex'));
+
+      // setTimeout(function () {
+      //   focusTarget.focus();
+      // }, 100);
+    }
   }
 })(jQuery, Drupal);
