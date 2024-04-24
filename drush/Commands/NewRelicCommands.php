17fc58aa29c16223848fe7c1b8678096f71d5a4c
Moshe Weitzman <weitzman@tejasa.com>
2024-04-24 13:47:07 -0400

    Debug

--- drush/Commands/NewRelicCommands.php
+++ drush/Commands/NewRelicCommands.php
@@ -6,30 +6,29 @@ use Consolidation\AnnotatedCommand\CommandData;
 use Consolidation\AnnotatedCommand\Hooks\HookManager;
 use Drush\Attributes as CLI;
 use Drush\Boot\DrupalBootLevels;
-use Drush\Commands\DrushCommands;
 
 #[CLI\Bootstrap(level: DrupalBootLevels::NONE)]
-final class NewRelicCommands extends DrushCommands
-{
+final class NewRelicCommands extends DrushCommands {
 
   #[CLI\Option(name: 'nrname', description: 'New Relic transaction name.')]
   #[CLI\Hook(type: HookManager::OPTION_HOOK, target: '*')]
-  public function optionsetNrName($options = ['nrname' => self::REQ])
-  {
+  public function optionsetNrName($options = ['nrname' => self::REQ]) {
   }
 
   /**
    * Set NR name.
    */
   #[CLI\Hook(type: HookManager::POST_COMMAND_HOOK, target: '*')]
-  public function name($result, CommandData $commandData)
-  {
+  public function name($result, CommandData $commandData) {
     if (!extension_loaded('newrelic')) {
+      $this->logger()->info('New Relic extension is not loaded.');
       return;
     }
     $annotationData = $commandData->annotationData();
     $commandName = $annotationData['command'];
     $name = $commandData->input()->hasOption('nrname') ? $commandData->input()->getOption('nrname') : $commandName;
     newrelic_name_transaction("cli.drush.$name");
+    $this->logger()->info('New Relic transaction sent to agent.');
   }
+
 }
