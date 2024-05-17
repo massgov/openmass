369c9cf62b644e8f9e8af245147dd9a3ac5aeff7
Moshe Weitzman <weitzman@tejasa.com>
2024-05-17 14:39:12 -0400

    Fix var name. I switched approach at last minute.

--- docroot/modules/custom/mass_utility/src/Renderer.php
+++ docroot/modules/custom/mass_utility/src/Renderer.php
@@ -15,7 +15,7 @@ class Renderer extends \Drupal\Core\Render\Renderer {
   public function __construct(CallableResolver|ControllerResolverInterface $callable_resolver, ThemeManagerInterface $theme, ElementInfoManagerInterface $element_info, PlaceholderGeneratorInterface $placeholder_generator, RenderCacheInterface $render_cache, RequestStack $request_stack, array $renderer_config) {
     // See https://massgov.atlassian.net/browse/DP-33081
     if (\Drupal::request()->getHost() !== 'www.mass.gov') {
-      $rendererConfig['debug'] = TRUE;
+      $renderer_config['debug'] = TRUE;
     }
     parent::__construct($callable_resolver, $theme, $element_info, $placeholder_generator, $render_cache, $request_stack, $renderer_config);
   }
