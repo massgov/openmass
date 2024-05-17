939b0dba21aac61fdf56e4f6111f4107a0249366
Moshe Weitzman <weitzman@tejasa.com>
2024-05-17 11:19:57 -0400

    Text/comments

--- docroot/modules/custom/mass_utility/src/Renderer.php
+++ docroot/modules/custom/mass_utility/src/Renderer.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 class Renderer extends \Drupal\Core\Render\Renderer {
 
   public function __construct(CallableResolver|ControllerResolverInterface $callable_resolver, ThemeManagerInterface $theme, ElementInfoManagerInterface $element_info, PlaceholderGeneratorInterface $placeholder_generator, RenderCacheInterface $render_cache, RequestStack $request_stack, array $renderer_config) {
+    // See https://massgov.atlassian.net/browse/DP-33081
     if (\Drupal::request()->getHost() !== 'www.mass.gov') {
       $rendererConfig['debug'] = TRUE;
     }
