a6a4541c38f7af1be21fe9011b492419dc64b969
abaghdasaryan <arthur.baghdasar@gmail.com>
2024-06-06 11:29:05 +0400

    DP-33478: Fix js issue related to status messages

--- docroot/modules/custom/mass_admin_toolbar/js/mass_admin_toolbar.messages_tabs.js
+++ docroot/modules/custom/mass_admin_toolbar/js/mass_admin_toolbar.messages_tabs.js
@@ -14,60 +14,62 @@
       var $messages = $('.mass-dashboard-tabs__messages');
       var $tabs = $('.mass-dashboard-tabs__tabs');
 
-      var messageBottom = $messages.offset().top;
-      var messagesOpen = true;
+      if ($messages.length) {
+        var messageBottom = $messages.offset().top;
 
-      var $toggle = $('<a href="#" class="mass-dashboard-tabs__trigger">Hide Messages</a>');
-      if ($messages.children().length) {
-        $tabs.prepend($toggle);
-      }
+        var messagesOpen = true;
 
-      $toggle.click(function (e) {
-        e.preventDefault();
-        $(document).trigger('mass_dashboard.toggle');
-      }).mouseout(function () {
-        // Lose focus on the trigger when the mouse leaves. Using .blur() in the
-        // click handler breaks menus for users who are tabbing through.
-        $(this).blur();
-      });
-
-      $(document).on('mass_dashboard.toggle', function () {
-        if (messagesOpen) {
-          close();
+        var $toggle = $('<a href="#" class="mass-dashboard-tabs__trigger">Hide Messages</a>');
+        if ($messages.children().length) {
+          $tabs.prepend($toggle);
         }
-        else {
-          open();
-        }
-      });
-      var open = function () {
-        messagesOpen = true;
-        $messages.slideDown('slow', function () {
-          $toggle.removeClass('is-closed');
-          $toggle.text('Hide Messages');
-          $(window).on('scroll.mass_dashboard_tabs', handleScroll);
-        });
-      };
-      var close = function () {
-        messagesOpen = false;
-        $(window).off('scroll.mass_dashboard_tabs', handleScroll);
-        $messages.slideUp('slow', function () {
-          $toggle.addClass('is-closed');
-          $toggle.text('Show Messages');
-        });
-      };
 
-      var handleScroll = function () {
-        if ($(window).scrollTop() > messageBottom) {
-          // Reevaluate message height because user interactions can change it,
-          // but we don't want to calculate this on every scroll event.
-          messageBottom = $messages.offset().top + $messages.outerHeight(true);
-          if ($(window).scrollTop() > messageBottom) {
+        $toggle.click(function (e) {
+          e.preventDefault();
+          $(document).trigger('mass_dashboard.toggle');
+        }).mouseout(function () {
+          // Lose focus on the trigger when the mouse leaves. Using .blur() in the
+          // click handler breaks menus for users who are tabbing through.
+          $(this).blur();
+        });
+
+        $(document).on('mass_dashboard.toggle', function () {
+          if (messagesOpen) {
             close();
           }
-        }
-      };
-      $(window).on('scroll.mass_dashboard_tabs', handleScroll);
+          else {
+            open();
+          }
+        });
+        var open = function () {
+          messagesOpen = true;
+          $messages.slideDown('slow', function () {
+            $toggle.removeClass('is-closed');
+            $toggle.text('Hide Messages');
+            $(window).on('scroll.mass_dashboard_tabs', handleScroll);
+          });
+        };
+        var close = function () {
+          messagesOpen = false;
+          $(window).off('scroll.mass_dashboard_tabs', handleScroll);
+          $messages.slideUp('slow', function () {
+            $toggle.addClass('is-closed');
+            $toggle.text('Show Messages');
+          });
+        };
 
+        var handleScroll = function () {
+          if ($(window).scrollTop() > messageBottom) {
+            // Reevaluate message height because user interactions can change it,
+            // but we don't want to calculate this on every scroll event.
+            messageBottom = $messages.offset().top + $messages.outerHeight(true);
+            if ($(window).scrollTop() > messageBottom) {
+              close();
+            }
+          }
+        };
+        $(window).on('scroll.mass_dashboard_tabs', handleScroll);
+      }
     }
   };
 
