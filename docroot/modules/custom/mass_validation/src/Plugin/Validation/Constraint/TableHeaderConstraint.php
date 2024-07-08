3fce07582bbc1a9bdcd5b77c90ae4954753522cf
arthurbaghdas <arthur.baghdasar@gmail.com>
2024-07-08 14:42:32 +0400

    DP-33642: Require tables in rich text editor to have headers or block node save

---
+++ docroot/modules/custom/mass_validation/src/Plugin/Validation/Constraint/TableHeaderConstraint.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\mass_validation\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Checks that the table in the CKEditor content has headers.
+ *
+ * @Constraint(
+ *   id = "TableHeaderConstraint",
+ *   label = @Translation("Table Header Constraint", context = "Validation"),
+ * )
+ */
+class TableHeaderConstraint extends Constraint {
+  public $message = 'Tables must have headers.';
+}
+
