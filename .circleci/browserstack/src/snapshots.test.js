d81d4dc6a3e35307a6fb8772abe4aac0416d3634
Thomas Fleming <tom@lastcallmedia.com>
2024-07-15 13:29:29 -0400

    set up initial circle config test

--- .circleci/browserstack/src/snapshots.test.js
+++ .circleci/browserstack/src/snapshots.test.js
@@ -3,7 +3,7 @@ const { percy } = require('browserstack-node-sdk');
 
 describe("BStack demo test", () => {
   let driver;
-  const paths = require('../../../backstop/all');
+  const paths = require('../post-release');
 
   beforeAll(() => {
     driver = new Builder()
@@ -15,7 +15,7 @@ describe("BStack demo test", () => {
   afterAll(async () => {
     await driver.quit();
   })
-  
+
   paths.forEach(async (path) => {
       test(path.label + ' test', async () => {
         await driver.get('https://stage.mass.gov' + path.url);
