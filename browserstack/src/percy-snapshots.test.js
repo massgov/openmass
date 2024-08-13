1b7c2908837b4ace8c139f7180119c361cbacf6b
arthurbaghdas <arthur.baghdasar@gmail.com>
2024-08-13 11:33:51 +0400

    Modify header implementation

--- browserstack/src/percy-snapshots.test.js
+++ browserstack/src/percy-snapshots.test.js
@@ -11,8 +11,6 @@ describe("massgov-screenshots", () => {
   const target = process.env.PERCY_TARGET;
   const tugboat = process.env.PERCY_TUGBOAT;
 
-  console.log('the tugboat url is: ' + tugboat);
-
   switch (list) {
     case 'all':
       pages = require('../all');
