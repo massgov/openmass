bfaab8093d692e48d706ee234a3639775723f0b0
Dima Storozhuk <dmutro.storozhuk@gmail.com>
2024-08-15 22:50:36 -0400

    DP-34460: Update table header validate error message.

--- docroot/modules/custom/mass_validation/src/Plugin/Validation/Constraint/TableHeaderConstraint.php
+++ docroot/modules/custom/mass_validation/src/Plugin/Validation/Constraint/TableHeaderConstraint.php
@@ -13,6 +13,5 @@ use Symfony\Component\Validator\Constraint;
  * )
  */
 class TableHeaderConstraint extends Constraint {
-  public $message = 'Tables must have headers.';
-
+  public $message = 'Tables must include a header row. See our <a target="_blank" href="%link">%title</a>.';
 }
--- docroot/modules/custom/mass_validation/src/Plugin/Validation/Constraint/TableHeaderConstraintValidator.php
+++ docroot/modules/custom/mass_validation/src/Plugin/Validation/Constraint/TableHeaderConstraintValidator.php
@@ -6,25 +6,33 @@ use Drupal\Component\Utility\Html;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
+/**
+ * Constraint validator.
+ *
+ * Validate if HTML table has a header.
+ */
 class TableHeaderConstraintValidator extends ConstraintValidator {
 
   /**
    * {@inheritdoc}
    */
   public function validate($value, Constraint $constraint) {
-    if ($value !== NULL) {
-      if ($text = $value->value) {
-        $document = Html::load($text);
-        $xpath = new \DOMXPath($document);
+    if (empty($value)) {
+      return;
+    }
 
-        // XPath query to find tables without <th> or <thead> elements.
-        $xpathQuery = "//table[not(.//th) and not(.//thead)]";
-        $tablesWithoutHeaders = $xpath->query($xpathQuery);
+    $document = Html::load($value);
+    $xpath = new \DOMXPath($document);
 
-        if ($tablesWithoutHeaders->length > 0) {
-          $this->context->addViolation($constraint->message);
-        }
-      }
+    // XPath query to find tables without <th> or <thead> elements.
+    $xpathQuery = "//table[not(.//th) and not(.//thead)]";
+    $tablesWithoutHeaders = $xpath->query($xpathQuery);
+
+    if ($tablesWithoutHeaders->length > 0) {
+      $this->context->addViolation($constraint->message, [
+        '%title' => 'Knowledge Base article on header rows',
+        '%url' => '/kb/documents-images-media--tables--headers-and-captions-key-tools-for-clarity-and-accessibility',
+      ]);
     }
   }
 
