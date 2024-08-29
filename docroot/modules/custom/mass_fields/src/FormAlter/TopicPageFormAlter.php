6811e9e5f30b39313c8e5856af8b5058bd2b7355
Dima Storozhuk <dmutro.storozhuk@gmail.com>
2024-08-29 11:35:19 -0500

    DP-34727: Add more documentation.

--- docroot/modules/custom/mass_fields/src/FormAlter/TopicPageFormAlter.php
+++ docroot/modules/custom/mass_fields/src/FormAlter/TopicPageFormAlter.php
@@ -3,7 +3,6 @@
 namespace Drupal\mass_fields\FormAlter;
 
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
-use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -12,7 +11,8 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *
  * Controls access to topic fields.
  *
- * @todo Fix problem: add more descriptive documentation.
+ * This is an implementation of the Class Resolver to encapsulate the code for
+ * form alters used in mass_fields_form_node_topic_page_edit_form_alter().
  */
 class TopicPageFormAlter implements ContainerInjectionInterface {
 
@@ -24,6 +24,8 @@ class TopicPageFormAlter implements ContainerInjectionInterface {
   private AccountInterface $currentUser;
 
   /**
+   * Class TopicPageFormAlter constructor.
+   *
    * @param \Drupal\Core\Session\AccountInterface $currentUser
    */
   public function __construct(AccountInterface $currentUser) {
@@ -39,12 +41,13 @@ class TopicPageFormAlter implements ContainerInjectionInterface {
   /**
    * Controls access to topic fields.
    *
+   * Hide the listed fields for users which don't have
+   * 'create topic_page content' permissions.
+   *
    * @param array $form
    *   The form array being built.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The current state of the form.
    */
-  public function topicFieldsControlAccess(array &$form, FormStateInterface $form_state): void {
+  public function topicFieldsControlAccess(array &$form): void {
     $fields_to_restrict_access_if_no_create_topic_page_perm = [
       'field_restrict_orgs_field',
       'field_hide_feedback_component',
