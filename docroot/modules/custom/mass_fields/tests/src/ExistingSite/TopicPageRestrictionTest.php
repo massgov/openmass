00a34837bb7ef16d770591f5988e8708fea29550
joeg8612 <joseph.galluccio@mass.gov>
2024-09-09 13:38:10 -0400

    DP-34727: Topic page flags and features (#2654)
    * config changes
    
    * DP-34727: Add conditional feedback form rendering.
    
    Introduced `TopicPageFormAlter` to control access to topic fields on the form. Updated the feedback template to conditionally include the feedback form based on `field_hide_feedback_component` value.
    
    * DP-34727: Fix field visibility conditions for topic_page form display
    
    Update the configuration of the topic_page form display to ensure that the depended field's state and condition are correctly set. This change ensures that the form behaves as expected based on the organizational restrictions.
    
    * DP-34727: Refactor topic field access control logic
    
    Streamline the access control logic for topic fields by looping through a predefined set of fields. This approach reduces redundancy and ensures consistent access checks based on user permissions.
    
    * DP-34727: Add Change log.
    
    * DP-34727: Add more documentation.
    
    * DP-34727: Fix yml.
    
    * DP-34727: Conditionally display related organizations and topics section
    
    Wrapped the related organizations and topics section in a conditional statement. This ensures the section is only rendered if there are related organizations or topics present.
    
    * DP-34727: Show the field_organizations for not admins, but keep it disabled.
    
    * DP-34727: Refactor topic fields access control
    
    Separate access control logic into dedicated methods for better readability and maintainability. Introduce `adminFieldsAccess` and `organizationsFieldAccess` to handle specific field access rules based on user permissions.
    
    * DP-34727: Update feedback component description in topic_page field
    
    Clarified that the feedback form is always hidden if no organization is selected. This change aims to prevent confusion for content administrators.
    
    * DP-34727: Variable name.
    
    * DP-34727: Refactor TopicPageFormAlter for enhanced field access control
    
    Update the `topicFieldsControlAccess` method to accept `FormStateInterface` and incorporate extended access control logic for `field_organizations` based on `field_restrict_orgs_field`. This refactor ensures that the access control depends on specific node-level permissions and field values.
    
    * DP-34727: Disable conditional_fields form validation when organization field is restricted
    
    Remove conditional field validations when the organization field is restricted by admin settings. This ensures unnecessary validations are not applied, streamlining the form submission process.
    
    * DP-34727: Place the test to right directory.
    
    * DP-34727: Remove unused "use" statement.
    
    * DP-34727: Prevent link management if restricted globally
    
    Add a condition to bypass link management modifications in `TopicPageFormAlter` if globally restricted for the Topic Page. This ensures that no changes are made to link management when the restriction is active.
    
    * DP-34727: Add tests for feedback form visibility on topic pages
    
    Introduce a method to create topic pages with and without feedback forms and add corresponding tests to verify the visibility of the feedback form based on the provided condition. This enhances the coverage and robustness of the Topic Page functionality by ensuring the feedback form behaves as expected.
    
    * DP-34727: Test admin controlled fields visibility on Topic Page.
    
    * DP-34727: Add more checks.
    
    * DP-34727: Tests access to organizations field on topic page edit form.
    
    * DP-34727: Code styles.
    
    * DP-34727: Add test for Topic Page admin field behavior
    
    Introduces `TopicPageAdminFieldsTest` to verify the behavior of the organization field on the topic page. Ensures the field is disabled when the related control option is selected by an admin.
    
    * DP-34727: Check non-admin/admin can save the form.
    
    * DP-34727: Code validator fixes.
    
    ---------
    
    Co-authored-by: Joe Galluccio <joseph.galluccio@state.ma.us>
    Co-authored-by: Dima Storozhuk <dmutro.storozhuk@gmail.com>

---
+++ docroot/modules/custom/mass_fields/tests/src/ExistingSite/TopicPageRestrictionTest.php
@@ -0,0 +1,415 @@
+<?php
+
+namespace Drupal\Tests\mass_fields\ExistingSite;
+
+use Drupal\file\Entity\File;
+use Drupal\paragraphs\Entity\Paragraph;
+use MassGov\Dtt\MassExistingSiteBase;
+use weitzman\LoginTrait\LoginTrait;
+
+/**
+ * Tests restrictions on Topic Pages.
+ */
+class TopicPageRestrictionTest extends MassExistingSiteBase {
+
+  use LoginTrait;
+
+  const RESTRICTED = TRUE;
+  const UNRESTRICTED = FALSE;
+
+  private $user;
+
+  /**
+   * Returns selectors of locked fields on topic pages.
+   */
+  private function lockedFields() {
+    return [
+      '#edit-title-0-value',
+      '#edit-field-topic-lede-0-value',
+      '#edit-field-topic-ref-related-topics-0-target-id',
+    ];
+  }
+
+  /**
+   * Returns selectors of unlocked fields on topic pages.
+   */
+  private function unlockedFields() {
+    return [
+      '#edit-field-organizations-0-target-id',
+      '#edit-field-intended-audience-none',
+      '#edit-field-reusable-label-0-target-id',
+      '#edit-field-topic-content-cards-add-more-add-more-button-content-card-group',
+    ];
+  }
+
+  /**
+   * Provides data to test with testTopicPageEditForm.
+   */
+  public function provider() {
+
+    $editor = [
+      'role' => 'editor',
+      'unlocked_fields' => $this->unlockedFields(),
+      'locked_fields' => $this->lockedFields(),
+      'available_states' => ['draft', 'needs_review', 'published'],
+    ];
+
+    $author = [
+      'role' => 'author',
+      'unlocked_fields' => $this->unlockedFields(),
+      'locked_fields' => $this->lockedFields(),
+      'available_states' => ['draft', 'needs_review'],
+    ];
+
+    $content_administrator = [
+      'role' => 'content_team',
+      // This role hasn't any locked fields.
+      'unlocked_fields' => array_merge($this->unlockedFields(), $this->lockedFields()),
+      'locked_fields' => [],
+      'available_states' => [
+        'draft',
+        'needs_review',
+        'published',
+        'trash',
+        'unpublished',
+      ],
+    ];
+
+    return [
+      [$editor],
+      [$author],
+      [$content_administrator],
+    ];
+  }
+
+  /**
+   * Creates a restricted/unrestricted topic page.
+   */
+  public function createTopicPage($restricted) {
+    // Test a new Topic page can be saved.
+    $newOrgNode = $this->createNode([
+      'type' => 'org_page',
+      'title' => $this->randomMachineName(),
+    ]);
+
+    $image = File::create([
+      'uri' => 'public://test.jpg',
+    ]);
+
+    $this->markEntityForCleanup($image);
+
+    $data = [
+      'type' => 'topic_page',
+      'title' => $this->randomMachineName(),
+      'field_restrict_link_management' => $restricted,
+      'field_organizations' => $newOrgNode->id(),
+      'field_topic_lede' => $this->randomString(20),
+      'field_topic_content_cards' => [
+        Paragraph::create([
+          'type' => 'content_card_group',
+          'field_content_card_link_cards' => [
+            'uri' => 'http://test.card',
+            'title' => 'Test Card',
+          ],
+        ]),
+      ],
+      'status' => 1,
+      'moderation_state' => 'published',
+    ];
+    $topic_page = $this->createNode($data);
+    $topic_page->save();
+    return $topic_page;
+  }
+
+  /**
+   * Creates a Topic Page with and without a feedback form component.
+   */
+  private function createTopicPageShowHideFeedbackForm(bool $feedback_form_hidden) {
+    // Test a new Topic page can be saved.
+    $newOrgNode = $this->createNode([
+      'type' => 'org_page',
+      'title' => $this->randomMachineName(),
+    ]);
+
+    $image = File::create([
+      'uri' => 'public://test.jpg',
+    ]);
+
+    $this->markEntityForCleanup($image);
+
+    $data = [
+      'type' => 'topic_page',
+      'title' => $this->randomMachineName(),
+      'field_hide_feedback_component' => $feedback_form_hidden,
+      'field_organizations' => $newOrgNode->id(),
+      'field_topic_lede' => $this->randomString(20),
+      'field_topic_content_cards' => [
+        Paragraph::create([
+          'type' => 'content_card_group',
+          'field_content_card_link_cards' => [
+            'uri' => 'http://test.card',
+            'title' => 'Test Card',
+          ],
+        ]),
+      ],
+      'status' => 1,
+      'moderation_state' => 'published',
+    ];
+
+    $topic_page = $this->createNode($data);
+    $topic_page->save();
+    return $topic_page;
+  }
+
+  /**
+   * Creates a Topic Page with with admin controlled organization field.
+   */
+  private function createTopicPageWhereOrganizationAccessControledByField(bool $access_to_orgs_restricted_for_non_content_admin) {
+    // Test a new Topic page can be saved.
+    $newOrgNode = $this->createNode([
+      'type' => 'org_page',
+      'title' => $this->randomMachineName(),
+    ]);
+
+    $image = File::create([
+      'uri' => 'public://test.jpg',
+    ]);
+
+    $this->markEntityForCleanup($image);
+
+    $data = [
+      'type' => 'topic_page',
+      'title' => $this->randomMachineName(),
+      'field_restrict_orgs_field' => $access_to_orgs_restricted_for_non_content_admin,
+      'field_topic_lede' => $this->randomString(20),
+      'field_organizations' => $newOrgNode->id(),
+      'field_topic_content_cards' => [
+        Paragraph::create([
+          'type' => 'content_card_group',
+          'field_content_card_link_cards' => [
+            'uri' => 'http://test.card',
+            'title' => 'Test Card',
+          ],
+        ]),
+      ],
+      'status' => 1,
+      'moderation_state' => 'published',
+    ];
+
+    $topic_page = $this->createNode($data);
+    $topic_page->save();
+    return $topic_page;
+  }
+
+  /**
+   * Checks topic page edit form locked and unlocked fields.
+   */
+  public function checkTopicPageEditForm($locked, $unlocked) {
+    // Checks locked fields.
+    foreach ($locked as $selector) {
+      $elem = $this->getCurrentPage()->find('css', $selector);
+      $error_msg = "$selector is not disabled.";
+      $this->assertTrue($elem->hasAttribute('disabled'), $error_msg);
+    }
+    // Checks locked fields.
+    foreach ($unlocked as $selector) {
+      $elem = $this->getCurrentPage()->find('css', $selector);
+      $error_msg = "$selector is not enabled.";
+      $this->assertTrue(!$elem->hasAttribute('disabled'), $error_msg);
+    }
+  }
+
+  /**
+   * Checks moderation state options.
+   */
+  private function checkWorkflowStates($available) {
+    // Check each state.
+    foreach ($available as $state) {
+      $selector = "#edit-moderation-state-0-state [value=$state]";
+      $msg = "$selector not available";
+      $elem = $this->getCurrentPage()->find('css', $selector);
+      $this->assertNotNull($elem, $msg);
+    }
+    // Check the number of states.
+    $options_selector = '#edit-moderation-state-0-state option';
+    $options = $this->getCurrentPage()->findAll('css', $options_selector);
+    $available_count = count($available);
+    $msg = "The moderation options should be $available_count";
+    $this->assertCount(count($available), $options, $msg);
+  }
+
+  /**
+   * Tests topic page form.
+   *
+   * @dataProvider provider
+   */
+  public function testTopicPageEditForm($data) {
+
+    // Create user.
+    $user = $this->createUser();
+    $user->addRole($data['role']);
+    // Content administrators also require permissions from the editor role.
+    if ($data['role'] == 'content_team') {
+      $user->addRole('editor');
+    }
+    $user->activate();
+    $user->save();
+    $this->user = $user;
+    $this->drupalLogin($this->user);
+
+    // Create topic page not restricted to content administrators.
+    $topic_page = $this->createTopicPage(self::UNRESTRICTED);
+    $this->drupalGet('/node/' . $topic_page->id() . '/edit');
+
+    $this->checkTopicPageEditForm($data['locked_fields'], $data['unlocked_fields']);
+
+    $this->getCurrentPage()->findButton('Save')->click();
+    $this->htmlOutput();
+    $this->assertSession()->pageTextContains('Topic Page ' . $topic_page->label() . ' has been updated.');
+
+    // Create topic page restricted to content administrators.
+    $topic_page = $this->createTopicPage(self::RESTRICTED);
+    $this->drupalGet('/node/' . $topic_page->id() . '/edit');
+
+    // All fields become locked, except for content administrators.
+    if ($data['role'] != 'content_team') {
+      $data['locked_fields'] = array_merge($data['locked_fields'], $data['unlocked_fields']);
+      $data['unlocked_fields'] = [];
+    }
+
+    $this->checkTopicPageEditForm($data['locked_fields'], $data['unlocked_fields']);
+
+    $this->checkWorkflowStates($data['available_states']);
+  }
+
+  /**
+   * Tests organization feedback form show/hide functionality.
+   */
+  public function testTopicPageShowHideFeedbackForm() {
+
+    // Create user.
+    $user = $this->createUser();
+    $role = 'content_team';
+    $user->addRole('content_team');
+    // Content administrators also require permissions from the editor role.
+    if ($role) {
+      $user->addRole('editor');
+    }
+
+    $user->activate();
+    $user->save();
+    $this->user = $user;
+    $this->drupalLogin($this->user);
+
+    // Create topic page without a feedback form.
+    $topic_page = $this->createTopicPageShowHideFeedbackForm(TRUE);
+    $this->drupalGet($topic_page->toUrl()->toString());
+    $this->assertFalse($this->getCurrentPage()->hasContent('Help Us Improve Mass.gov'), 'The feedback form is visible, while it must be hidden.');
+
+    // Create topic page with a feedback form.
+    $topic_page = $this->createTopicPageShowHideFeedbackForm(FALSE);
+    $this->drupalGet($topic_page->toUrl()->toString());
+    $this->assertTrue($this->getCurrentPage()->hasContent('Help Us Improve Mass.gov'), 'The feedback form is hidden, while it must be visible.');
+  }
+
+  /**
+   * Data provider.
+   *
+   * Provides data to test with
+   * testTopicPageOrganizationFieldAccess.
+   */
+  public function topicPageOrganizationFieldAccessNonAdminRoles(): array {
+    return [
+      [['role' => 'editor']],
+      [['role' => 'author']],
+    ];
+  }
+
+  /**
+   * Tests access to organizations field on a topic page edit form.
+   *
+   * @dataProvider topicPageOrganizationFieldAccessNonAdminRoles
+   */
+  public function testTopicPageOrganizationFieldAccess(array $role_data) {
+    // Create user.
+    $user = $this->createUser();
+    $role = $role_data['role'];
+
+    $user->addRole($role);
+
+    $user->activate();
+    $user->save();
+    $this->user = $user;
+
+    $this->drupalLogin($this->user);
+
+    // Assert organizations field is disabled by admins.
+    $topic_page = $this->createTopicPageWhereOrganizationAccessControledByField(TRUE);
+    $this->drupalGet($topic_page->toUrl('edit-form')->toString());
+    $page = $this->getCurrentPage();
+    $element = $page->find('css', '#edit-field-organizations-0-target-id');
+    $this->assertTrue($element->hasAttribute('disabled'), "Organizations field must be disabled for $role if \"field_restrict_orgs_field\" is selected.");
+    // Check the page can be saved.
+    $page->findButton('Save')->click();
+    $this->htmlOutput();
+    $this->assertSession()->pageTextContains('Topic Page ' . $topic_page->label() . ' has been updated.');
+
+    // Assert admins allow organizations field.
+    $topic_page = $this->createTopicPageWhereOrganizationAccessControledByField(FALSE);
+    $this->drupalGet($topic_page->toUrl('edit-form')->toString());
+    $page = $this->getCurrentPage();
+    $element = $page->find('css', '#edit-field-organizations-0-target-id');
+    $this->assertFalse($element->hasAttribute('disabled'), "Organizations field must be visible for $role if \"field_restrict_orgs_field\" is not selected.");
+    // Check the page can be saved.
+    $page->findButton('Save')->click();
+    $this->htmlOutput();
+    $this->assertSession()->pageTextContains('Topic Page ' . $topic_page->label() . ' has been updated.');
+  }
+
+  /**
+   * Data provider.
+   *
+   * Provides data to test with
+   * testTopicPageOrganizationFieldControlsVisibility.
+   */
+  public function topicPageOrganizationFieldControlsVisibilityAdminRoles(): array {
+    return [
+      [['role' => 'editor', 'assert' => 'assertFalse']],
+      [['role' => 'author', 'assert' => 'assertFalse']],
+      [['role' => 'content_team', 'assert' => 'assertTrue']],
+    ];
+  }
+
+  /**
+   * Tests access to organizations field on topic page edit form.
+   *
+   * @dataProvider topicPageOrganizationFieldControlsVisibilityAdminRoles
+   */
+  public function testTopicPageOrganizationFieldControlsVisibility(array $role_data) {
+    // Create user.
+    $role = $role_data['role'];
+    $user = $this->createUser();
+    $user->addRole($role);
+    $user->activate();
+    $user->save();
+    $this->user = $user;
+
+    $this->drupalLogin($this->user);
+
+    $topic_page = $this->createTopicPage(FALSE);
+    $this->drupalGet($topic_page->toUrl('edit-form')->toString());
+    $page = $this->getCurrentPage();
+    // Assert field visibility.
+    $field_names = ['Disable organization(s) field and make it optional', 'Hide sitewide feedback component'];
+    foreach ($field_names as $field_name) {
+      $condition = $role_data['assert'] == 'assertTrue' ? '' : 'not';
+      $this->{$role_data['assert']}($page->hasField($field_name), "\"$field_name\" field should $condition be visible for " . $role);
+
+    }
+    // Check the page can be saved.
+    $page->findButton('Save')->click();
+    $this->htmlOutput();
+    $this->assertSession()->pageTextContains('Topic Page ' . $topic_page->label() . ' has been updated.');
+  }
+
+}
