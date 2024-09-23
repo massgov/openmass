48ded4c09cfee7039fc3aea0f77c46bd44e017e9
Luke Ollett <ollett@gmail.com>
2024-09-23 10:03:11 -0700

    Revert "DP-33362: Changed database name in BigQuery (#2687)" (#2689)
    This reverts commit 0f1618083b87418eb177ab3ec97834a175e1007b.

--- docroot/modules/custom/mass_bigquery/src/BigqueryStorage.php
+++ docroot/modules/custom/mass_bigquery/src/BigqueryStorage.php
@@ -128,7 +128,7 @@ class BigqueryStorage implements BigqueryStorageInterface {
     $time = \Drupal::time()->getRequestTime();
     // Fetch data from Bigquery.
     $query =
-        'SELECT nodeId, totalPageViews, nosPerKUniquePageViews, ejectRate, negativeSurveys, positiveSurveys, brokenLinks, gradeLevel FROM `MassgovGA4_prod.aggregated_node_analytics` WHERE nodeId IN(' . implode(', ', $ids) . ')';
+        'SELECT nodeId, totalPageViews, nosPerKUniquePageViews, ejectRate, negativeSurveys, positiveSurveys, brokenLinks, gradeLevel FROM `MassgovGA4_testing.aggregated_node_analytics` WHERE nodeId IN(' . implode(', ', $ids) . ')';
     $queryResults = $this->bigqueryClient->runQuery($query);
     foreach ($queryResults as $row) {
       $nos_per_1000 = $row['nosPerKUniquePageViews'];
@@ -168,7 +168,7 @@ class BigqueryStorage implements BigqueryStorageInterface {
     $time = \Drupal::time()->getRequestTime();
     // Fetch data from Bigquery.
     $query =
-      'SELECT nodeId, totalPageViews, nosPerKUniquePageViews, ejectRate, negativeSurveys, positiveSurveys, brokenLinks, gradeLevel FROM `MassgovGA4_prod.aggregated_node_analytics` WHERE nodeId IN(' . implode(', ', $ids) . ')';
+      'SELECT nodeId, totalPageViews, nosPerKUniquePageViews, ejectRate, negativeSurveys, positiveSurveys, brokenLinks, gradeLevel FROM `MassgovGA4_testing.aggregated_node_analytics` WHERE nodeId IN(' . implode(', ', $ids) . ')';
     $queryResults = $this->bigqueryClient->runQuery($query);
     $result = [];
     foreach ($queryResults as $row) {
