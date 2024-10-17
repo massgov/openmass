d1109d8fedf1b690e9a9b2853c3327ed28514bcf
arthurbaghdas <arthur.baghdasar@gmail.com>
2024-10-17 09:35:51 +0400

    PHPCS

--- docroot/modules/custom/mass_views/src/Controller/MassViewsController.php
+++ docroot/modules/custom/mass_views/src/Controller/MassViewsController.php
@@ -1,14 +1,31 @@
 <?php
 
-
 namespace Drupal\mass_views\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\mass_content\Entity\Bundle\media\DocumentBundle;
+use Drupal\views\Views;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Drupal\views\Views;
 
+/**
+ * Class MassViewsController.
+ *
+ * This controller is responsible for generating filtered URLs from Drupal Views.
+ * It handles two main actions:
+ *  - `pageLinks`: Generates a list of filtered URLs for pages based on the "crawler_pages" view.
+ *  - `documentLinks`: Generates a list of filtered URLs for documents based on the "crawler_documents" view.
+ *
+ * The controller also handles pagination, with each page limited to 500 items by default,
+ * and applies filters based on query string parameters.
+ *
+ * The helper method `generateView` is used to execute the appropriate view,
+ * apply filters, set pagination, and build an HTML response containing an unordered list
+ * of URLs and pagination links for navigation.
+ *
+ * - If the view does not exist or returns no results, it will return a 404 response.
+ * - If results are found, the URLs will be listed with pagination for easy navigation through pages.
+ */
 class MassViewsController extends ControllerBase {
 
   /**
@@ -35,7 +52,8 @@ class MassViewsController extends ControllerBase {
       // Set up the display and limit the number of items per page.
       $view->setDisplay('default');
       $items_per_page = 500;
-      $view->setItemsPerPage($items_per_page); // Limit to 500 results per page.
+      // Limit to 500 results per page.
+      $view->setItemsPerPage($items_per_page);
 
       // Handle query string parameters for filtering.
       $query_parameters = $request->query->all();
@@ -58,7 +76,7 @@ class MassViewsController extends ControllerBase {
           $text = $row->_entity->getTitle();
           if ($row->_entity instanceof DocumentBundle) {
             $href = $row->_entity->toUrl()->toString() . '/download';
-            $text =  $row->_entity->getTitle()->getString();
+            $text = $row->_entity->getTitle()->getString();
           }
           $output .= '<li><a href="' . $href . '">' . $text . '</a></li>';
         }
@@ -90,4 +108,5 @@ class MassViewsController extends ControllerBase {
     // Return an empty response if the view doesn't exist.
     return new Response('No results found', 404);
   }
+
 }
