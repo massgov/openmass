96eb9eea4081d396405e49c3cbe6ded0e36e6a40
Thomas Fleming <tom@lastcallmedia.com>
2025-01-03 15:55:22 -0500

    Added pre and post NR name hooks to track when command starts and finishes

--- drush/Commands/NewRelicCommands.php
+++ drush/Commands/NewRelicCommands.php
@@ -16,10 +16,25 @@ final class NewRelicCommands extends DrushCommands {
   }
 
   /**
-   * Set NR name.
+   * Set NR name before command is run.
+   */
+  #[CLI\Hook(type: HookManager::PRE_COMMAND_HOOK, target: '*')]
+  public function preName($result, CommandData $commandData) {
+    $this->name($result, $commandData, 'start');
+  }
+
+  /**
+   * Set NR name after command is run.
    */
   #[CLI\Hook(type: HookManager::POST_COMMAND_HOOK, target: '*')]
-  public function name($result, CommandData $commandData) {
+  public function postName($result, CommandData $commandData) {
+    $this->name($result, $commandData, 'finish');
+  }
+
+  /**
+   * Set NR name.
+   */
+  private function name($result, CommandData $commandData, $prefix = '') {
     if (!extension_loaded('newrelic')) {
       $this->logger()->debug('New Relic extension is not loaded.');
       return;
@@ -29,7 +44,7 @@ final class NewRelicCommands extends DrushCommands {
     if ($commandData->input()->hasOption('nrname') && $commandData->input()->getOption('nrname')) {
       $name = $commandData->input()->getOption('nrname');
     }
-    $success = newrelic_name_transaction("cli.drush.$name");
+    $success = newrelic_name_transaction("cli.drush.$prefix.$name");
     if (!$success) {
       $this->logger()->error('Failed to set New Relic transaction name.');
     }
