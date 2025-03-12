6c5257f47d9c81255e36c11f42c8b3e8c2736f48
Andy Blum <andy.blum.01@gmail.com>
2025-03-12 14:42:59 -0400

    Move nearest microsite lookup to own service

---
+++ docroot/modules/custom/mass_microsites/src/NearestMicrositeLookup.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Drupal\mass_microsites;
+
+use Drupal\entity_hierarchy\Storage\NestedSetStorageFactory;
+use Drupal\entity_hierarchy\Storage\NestedSetNodeKeyFactory;
+use Drupal\entity_hierarchy_microsite\ChildOfMicrositeLookup;
+use Drupal\entity_hierarchy_microsite\Entity\MicrositeInterface;
+use Drupal\node\NodeInterface;
+
+/**
+ * Finds the nearest microsite for a given node.
+ */
+class NearestMicrositeLookup {
+
+  protected NestedSetStorageFactory $nestedSetStorageFactory;
+
+  protected NestedSetNodeKeyFactory $nestedSetNodeKeyFactory;
+
+  protected ChildOfMicrositeLookup $micrositeLookup;
+
+  /**
+   * Constructs a new NestedSetStorageFactory object.
+   *
+   * @param \Doctrine\DBAL\Connection $connection
+   *   Dbal Connection.
+   * @param \Drupal\Core\Database\Connection $drupalConnection
+   *   Drupal connection.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   Logger.
+   */
+  public function __construct(
+    NestedSetStorageFactory $nested_set_storage_factory,
+    NestedSetNodeKeyFactory $nested_set_node_key_factory,
+    ChildOfMicrositeLookup $microsite_lookup
+  ) {
+    $this->nestedSetStorageFactory = $nested_set_storage_factory;
+    $this->nestedSetNodeKeyFactory = $nested_set_node_key_factory;
+    $this->micrositeLookup = $microsite_lookup;
+  }
+
+  /**
+   * Select the nearest microsite based on the node's hierarchy.
+   *
+   * @param array<MicrositeInterface> $microsites
+   *   An array of microsites in which the node exists.
+   * @param NodeInterface $node
+   *   The node.
+   *
+   * @return MicrositeInterface
+   *   The microsite with the fewest pages between the current node and the microsite's "home" page.
+   */
+  public function selectNearestMicrosite(array $microsites, NodeInterface $node) {
+    /**
+     * The microsite for which the "homepage" is closest to the current node.
+     * @var MicrositeInterface|null
+     */
+    $nearest_microsite = NULL;
+    $microsites_by_home_id = [];
+
+    foreach ($microsites as $microsite) {
+      $microsites_by_home_id[$microsite->getHome()->id()] = $microsite;
+    }
+
+    if (count($microsites_by_home_id)) {
+      $nestedSetStorage = $this->nestedSetStorageFactory->get('field_primary_parent', 'node');
+      $key = $this->nestedSetNodeKeyFactory->fromEntity($node);
+
+      /**
+       * Array of ancestors in hierarchy, starting with field_primary_parent and climbing upward.
+       * @var Node[]
+       */
+      $ancestors = array_reverse($nestedSetStorage->findAncestors($key));
+
+      foreach ($ancestors as $ancestor) {
+        $ancestor_id = $ancestor->getNodeKey()->getId();
+        if (
+          !$nearest_microsite &&
+          isset($microsites_by_home_id[$ancestor_id])
+        ) {
+          $nearest_microsite = $microsites_by_home_id[$ancestor_id];
+        }
+      }
+    }
+
+    return $nearest_microsite;
+  }
+
+  /**
+   * Get the nearest microsite for a node.
+   *
+   * @param NodeInterface $node
+   *   The node.
+   *
+   * @return MicrositeInterface|null
+   */
+  public function getNearestMicrosite(NodeInterface $node) {
+    if ($microsites = $this->micrositeLookup->findMicrositesForNodeAndField($node, 'field_primary_parent')) {
+      return $this->selectNearestMicrosite($microsites, $node);
+    }
+
+    return NULL;
+  }
+}
