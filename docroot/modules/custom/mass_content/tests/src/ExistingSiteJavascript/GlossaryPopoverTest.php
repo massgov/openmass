370a29596f7adf1f58b7059f335fe09fa09dcdcc
Andy Blum <andy.blum.01@gmail.com>
2025-04-14 13:08:57 -0400

    WIP GlossaryPopoverTest

--- docroot/modules/custom/mass_content/tests/src/ExistingSiteJavascript/GlossaryPopoverTest.php
+++ docroot/modules/custom/mass_content/tests/src/ExistingSiteJavascript/GlossaryPopoverTest.php
@@ -31,7 +31,6 @@ class GlossaryPopoverTest extends ExistingSiteSelenium2DriverTestBase {
     $node = $this->createNode([
       'type' => 'service_page',
       'title' => 'Test Service Page',
-      'field_short_description' => TRUE,
       'field_organizations' => $hlc_reference,
       'field_primary_parent' => $hlc_reference,
       'field_glossary' => $glossary->label() . ' (' . $glossary->id() . ') - Glossary',
@@ -41,29 +40,20 @@ class GlossaryPopoverTest extends ExistingSiteSelenium2DriverTestBase {
 
     echo("\nGlossary URL: " . $glossary->toUrl()->toString() . "\n");
     echo("\nNode URL: " . $node->toUrl()->toString() . "\n");
-    $this->drupalGet($glossary->toUrl()->toString());
+    $this->drupalGet($node->toUrl()->toString());
+    $page = $this->getSession()->getPage();
+    $page->waitFor(10, function () use ($page) {
+      $hasTemplate = $page->find('css', '#glossary-popup-template');
+      return $hasTemplate !== null;
+    });
     $this->assertSession()->elementExists('css', 'main');
-    $this->assertSession()->elementExists('css', '#glossary-popup-template');
+    // $this->assertSession()->elementExists('css', '#glossary-popup-template');
     $this->assertSession()->elementExists('css', '[data-drupal-selector="drupal-settings-json"]');
 
     $this->assertSession()->elementTextContains('css', '[data-drupal-selector="drupal-settings-json"]', 'glossaries');
     $this->assertSession()->elementTextContains('css', '[data-drupal-selector="drupal-settings-json"]', 'Lorem');
     $this->assertSession()->elementTextContains('css', '[data-drupal-selector="drupal-settings-json"]', 'Ipsum');
     $this->assertSession()->elementTextContains('css', 'main', 'Lorem');
-
-
-    // Visit the node page and check if the short description is rendered.
-    $this->drupalGet($node->toUrl()->toString());
-    $this->assertSession()->elementExists('css', 'main .popover__trigger');
-
-    // // Update the node to uncheck the checkbox.
-    // $node->set('field_display_short_description', FALSE);
-    // $node->save();
-
-    // // Visit the node page again and check
-    // // if the short description is NOT rendered.
-    // $this->drupalGet($node->toUrl()->toString());
-    // $this->assertSession()->elementNotExists('css', '.pre-content .ma__page-header__content .ma__page-header__description');
   }
 
 }
