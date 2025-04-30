a76e6eccd13ec9b6e2ebcb463d039b689eb518a2
arthurbaghdas <arthur.baghdasar@gmail.com>
2025-04-30 14:26:58 +0400

    DP-39143: Backstop false positives

--- backstop/backstop.js
+++ backstop/backstop.js
@@ -147,7 +147,7 @@ module.exports = {
     "onBeforeScript": "before.js",
     "onReadyScript": "ready.js",
     "readyTimeout": "90000",
-    "asyncCaptureLimit": 1,
+    "asyncCaptureLimit": 3,
     "report": ["browser", "CI"],
     "engine": "playwright",
     "engineFlags": [],
