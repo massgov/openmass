b05348d15576b9ba7f7570d7fd1654e67fd6e538
Minghua Sun <clairesunstudio@gmail.com>
2025-05-30 14:07:39 -0400

    incorporate iframe_resizer_init into iframe_resizer_focus to override iFrameResize with extended options

--- docroot/themes/custom/mass_theme/overrides/js/iframe_resizer_focus.js
+++ docroot/themes/custom/mass_theme/overrides/js/iframe_resizer_focus.js
@@ -1,28 +1,53 @@
-(function (Drupal, once) {
+(function ($, Drupal, iframeResizerSettings) {
   'use strict';
-  Drupal.behaviors.initializeIframeResizerFocus = {
-    attach: function () {
-      iFrameResize({
-        log: false,
-        checkOrigin: false,
-        messageCallback: function (messageData) {
-          const { message, iframe } = messageData;
 
-          if (message.type === 'scrollToFocus') {
-            // iframe position in the parent page
-            const iframeTop = iframe.getBoundingClientRect().top + window.scrollY;
+  // Set up the iFrame Resizer library's options.
+  var options = {};
+  if (iframeResizerSettings.advanced.override_defaults) {
+    if (iframeResizerSettings.advanced.options.maxHeight === -1) {
+      iframeResizerSettings.advanced.options.maxHeight = Infinity;
+    }
 
-            // message.offset is the offset inside the iframe
-            const scrollTarget = iframeTop + message.offset;
-            const adjustment = -250;
+    if (iframeResizerSettings.advanced.options.maxWidth === -1) {
+      iframeResizerSettings.advanced.options.maxWidth = Infinity;
+    }
 
-            window.scrollTo({
-              top: scrollTarget + adjustment,
-              behavior: 'smooth'
-            });
-          }
-        }
-      }, '.js-ma-responsive-iframe iframe');
+    options = iframeResizerSettings.advanced.options;
+  }
+
+  const focusOptions = {
+    log: false,
+    checkOrigin: false,
+    messageCallback: function (messageData) {
+      const { message, iframe } = messageData;
+
+      if (message.type === 'scrollToFocus') {
+        // iframe position in the parent page
+        const iframeTop = iframe.getBoundingClientRect().top + window.scrollY;
+
+        // message.offset is the offset inside the iframe
+        const scrollTarget = iframeTop + message.offset;
+        const adjustment = -250;
+
+        window.scrollTo({
+          top: scrollTarget + adjustment,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }
+
+  const newOptions = {...options, ...focusOptions};
+
+  Drupal.behaviors.initIframeResizer = {
+    attach: function (context, settings) {
+      alert('test')
+      var selector = 'iframe';
+      if (typeof settings.iframeResizer.advanced.targetSelectors !== 'undefined') {
+        selector = settings.iframeResizer.advanced.targetSelectors;
+      }
+
+      $(selector, context).iFrameResize(newOptions);
     }
   };
-})(Drupal, once);
+})(jQuery, Drupal, drupalSettings.iframeResizer);
