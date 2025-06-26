f193307409e97e757f24a7c3384095d5bcebf897
Minghua Sun <clairesunstudio@gmail.com>
2025-06-26 13:49:50 -0400

    cleanup

--- docroot/themes/custom/mass_theme/overrides/js/iframe_resizer_focus.js
+++ docroot/themes/custom/mass_theme/overrides/js/iframe_resizer_focus.js
@@ -10,14 +10,12 @@
     const iframe = messageData.iframe;
     
     if (message && message.type === 'scrollToFocus') {
-      console.log('Processing scrollToFocus message', { message, iframe });
       
       if (iframe) {
         // Check if the iframe is within a modal
         const modal = iframe.closest('.tingle-modal-box__content');
         
         if (modal) {
-          console.log('Found modal, scrolling modal content');
           // Handle focus within modal - scroll the modal content
           const modalRect = modal.getBoundingClientRect();
           const iframeRect = iframe.getBoundingClientRect();
@@ -27,32 +25,16 @@
           
           const finalScrollPosition = Math.max(0, scrollTarget + adjustment);
           
-          console.log('Modal scroll calculation:', {
-            modalScrollTop: modal.scrollTop,
-            relativeIframeTop,
-            messageOffset: message.offset,
-            finalScrollPosition
-          });
-          
           modal.scrollTo({
             top: finalScrollPosition,
             behavior: 'smooth'
           });
         } else {
-          console.log('No modal found, scrolling main page');
           // Handle focus in regular page iframe
           const iframeTop = iframe.getBoundingClientRect().top + window.scrollY;
           const scrollTarget = iframeTop + (message.offset || 0);
           const adjustment = -250;
-          
           const finalScrollPosition = Math.max(0, scrollTarget + adjustment);
-          
-          console.log('Page scroll calculation:', {
-            iframeTop,
-            windowScrollY: window.scrollY,
-            messageOffset: message.offset,
-            finalScrollPosition
-          });
           
           window.scrollTo({
             top: finalScrollPosition,
@@ -83,9 +65,7 @@
             const enhancedOptions = $.extend({}, options || {});
             const originalOnMessage = enhancedOptions.onMessage;
             
-            enhancedOptions.onMessage = function(messageData) {
-              console.log('onMessage called with:', messageData);
-              
+            enhancedOptions.onMessage = function(messageData) {              
               // Handle our custom focus messages
               handleFocusMessage(messageData);
               
@@ -94,9 +74,7 @@
                 originalOnMessage(messageData);
               }
             };
-            
-            console.log('Initializing iframe with enhanced options:', enhancedOptions);
-            
+                        
             // Call the original iFrameResize with enhanced options
             return originalIFrameResize.call(this, enhancedOptions);
           };
