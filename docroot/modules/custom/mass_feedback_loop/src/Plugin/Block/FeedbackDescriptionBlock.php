5690087b5714a7ce89fbfb5f41736f346d93f88f
arthurbaghdas <arthur.baghdasar@gmail.com>
2025-07-14 10:13:39 +0400

    Fix

--- docroot/modules/custom/mass_feedback_loop/src/Plugin/Block/FeedbackDescriptionBlock.php
+++ docroot/modules/custom/mass_feedback_loop/src/Plugin/Block/FeedbackDescriptionBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\mass_feedback_loop\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Render\Markup;
 use Drupal\Core\Url;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -27,11 +28,16 @@ class FeedbackDescriptionBlock extends BlockBase {
       return [];
     }
 
-    $neg_url = Url::fromRoute('view.pages_with_high_negative_feedback.page_2')->toString();
-    $mgr_url = Url::fromRoute('mass_feedback_loop.mass_feedback_loop_author_interface_form')->toString();
+    $negative_feedback_url = Url::fromRoute('view.pages_with_high_negative_feedback.page_2')->toString();
+    $feedback_manager_url = Url::fromRoute('mass_feedback_loop.mass_feedback_loop_author_interface_form')->toString();
+    $message = '<p class="ma__page-header__description">Also see: <a href="@negative_feedback_url">Pages with high negative feedback</a> and <a href="@feedback_manager_url">Feedback Manager</a> where you can view, filter and sort feedback submissions.</p>';
+    $markup = Markup::create($this->t($message, [
+      '@negative_feedback_url' => $negative_feedback_url,
+      '@feedback_manager_url' => $feedback_manager_url,
+    ]));
 
     return [
-      '#markup' => '<p class="ma__page-header__description">Also see: <a href="' . $neg_url . '">Pages with high negative feedback</a> and <a href="' . $mgr_url . '">Feedback Manager</a> where you can view, filter and sort feedback submissions.</p>',
+      '#markup' => $markup
     ];
   }
 
