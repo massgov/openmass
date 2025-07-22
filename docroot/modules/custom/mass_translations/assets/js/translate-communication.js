2539f55c868c695d1f71654b805b9de1f8f44464
Dmytro Storozhuk <dima@itech4web.com>
2025-07-22 18:56:01 -0500

    DP-40086: WORKING SOLUTION.

--- docroot/modules/custom/mass_translations/assets/js/translate-communication.js
+++ docroot/modules/custom/mass_translations/assets/js/translate-communication.js
@@ -7,60 +7,57 @@
 
 (function ($, Drupal) {
   'use strict';
-  // Translate on page load if previously selected.
-  // Not sure iw we need this.
+
   window.addEventListener('message', function(event) {
     // Verify origin - adjust as needed
-    // alert(event.origin);
+
     // if (event.origin !== 'https://forms.mass.gov') return;
 
-    if (event.data.type === 'getCookie') {
-      // Send cookie value back to iframe
-      event.source.postMessage({
-        action: 'cookieResponse',
-        lang: get_current_lang(),
-      }, '*');
+    if (event.data.action === 'getCookie') {
+      notifyIframesOfLanguageChange('auto', getCurrentLanguage () );
     }
   });
 
-  function getCookie(name) {
-    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-    return match ? match[2] : null;
+  /**
+   * Function to notify all iframes about language changes
+   * @param {string} from - Source language code
+   * @param {string} to - Target language code
+   */
+  function notifyIframesOfLanguageChange(from, to) {
+    // Find all iframes on the page
+    const iframes = document.querySelectorAll('iframe.js-iframe-resizer');
+
+    // Send postMessage to each iframe
+    iframes.forEach(iframe => {
+      try {
+        // Only send to iframes that are loaded and have a contentWindow
+        if (iframe.contentWindow) {
+          const message = {
+            action: 'translate',
+            language: to
+          };
+
+          // Send the message to the iframe
+          iframe.contentWindow.postMessage(message, '*');
+        }
+      }
+      catch (error) {
+        console.error('Error sending message to iframe:', error);
+      }
+    });
+  }
+
+  function getCurrentLanguage () {
+    // alert(getLangFromFragment());
+    const keyValue = document.cookie.match('(^|;) ?googtrans=([^;]*)(;|$)');
+    return keyValue ? keyValue[2].split('/')[2] : null;
+
   }
 
   Drupal.behaviors.translateCommunication = {
     attach: function (context, settings) {
       // Store the current language to detect changes
       let currentLanguage = document.documentElement.lang || 'en';
-
-      /**
-       * Function to notify all iframes about language changes
-       * @param {string} from - Source language code
-       * @param {string} to - Target language code
-       */
-      function notifyIframesOfLanguageChange(from, to) {
-        // Find all iframes on the page
-        const iframes = document.querySelectorAll('iframe.js-iframe-resizer');
-
-        // Send postMessage to each iframe
-        iframes.forEach(iframe => {
-          try {
-            // Only send to iframes that are loaded and have a contentWindow
-            if (iframe.contentWindow) {
-              const message = {
-                action: 'translate',
-                language: to
-              };
-
-              // Send the message to the iframe
-              iframe.contentWindow.postMessage(message, '*');
-            }
-          }
-          catch (error) {
-            console.error('Error sending message to iframe:', error);
-          }
-        });
-      }
 
       /**
        * Method 1: Detect language changes by observing HTML lang attribute changes
