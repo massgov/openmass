dfaf0b16180d269250a8bb82673afdfcc62a155b
Jariel Rodriguez <jarielrrz@gmail.com>
2025-09-30 12:20:36 -0400

    Revert "Update settings.tugboat.php"
    This reverts commit 25d9e5968fdb083de9d5ced22ad2cbe8ae0e2b24.

--- docroot/sites/default/settings.tugboat.php
+++ docroot/sites/default/settings.tugboat.php
@@ -1,13 +1,6 @@
 <?php
 
 /**
- * Disable caches for DP-36788 testing - timestamp parameter preservation
- */
-$settings['cache']['bins']['render'] = 'cache.backend.null';
-$settings['cache']['bins']['page'] = 'cache.backend.null';
-$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
-
-/**
  * No longer needed as per https://docs.tugboatqa.com/faq/common-questions/#can-i-password-protect-my-preview-urls.
  *
  * Password protect Tugboat environments.
