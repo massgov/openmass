c82c145e00d3bf6b4130c9bce430db3b75f14f85
arthurbaghdas <arthur.baghdasar@gmail.com>
2025-10-23 16:18:57 +0400

    Cleanup

--- docroot/modules/custom/mass_bulk_file_replace/src/Plugin/Action/MassMediaExportAction.php
+++ docroot/modules/custom/mass_bulk_file_replace/src/Plugin/Action/MassMediaExportAction.php
@@ -547,25 +547,6 @@ class MassMediaExportAction extends ViewsBulkOperationsActionBase implements Vie
   }
 
   public function access($object, ?AccountInterface $account = NULL, $return_as_object = FALSE) {
-    /** @var \Drupal\media\MediaInterface $object */
-    // Get current moderation state.
-    $state = $object->get('moderation_state')->getString();
-
-    if ($state == "restricted") {
-      // Get the current user.
-      $uid = \Drupal::currentUser()->id();
-      $current_user_roles = \Drupal::currentUser()->getRoles();
-      $author_id = $object->getOwner()->id();
-
-      // If the user is not an administrator.
-      if (!in_array("administrator", $current_user_roles)) {
-        // If the current user is not the author.
-        if ($uid !== $author_id) {
-          return;
-        }
-      }
-    }
-
     $access = $object->access('update', $account, TRUE)
       ->andIf($object->status->access('update', $account, TRUE));
 
