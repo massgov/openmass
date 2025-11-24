49aa7b5e508a2f71b24e06f0aad1c55770036645
Minghua Sun <clairesunstudio@gmail.com>
2025-11-24 11:50:45 -0500

    set images path

--- docroot/modules/custom/mayflower/js/mayflower.init.js
+++ docroot/modules/custom/mayflower/js/mayflower.init.js
@@ -1,6 +1,8 @@
 var ma = ma || {};
 // Path to assets directory (needed for handlebars templates):
 ma.themePath = drupalSettings.mayflower.assets;
+// Path to images:
+ma.iconPath = drupalSettings.mayflower.assets + '/images';
 // Path to SVG icons:
 ma.iconPath = drupalSettings.mayflower.assets + '/images/icons';
 // Twig namespaced paths:
