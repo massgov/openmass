62b71f8e28dcdc244a6f13ffa030b1948cf76b9f
Thomas Fleming <thomas.r.fleming@mass.gov>
2026-02-18 16:55:14 -0500

    Added relationship on pseudo table that includes pid count from ed11y_action

---
+++ docroot/modules/custom/mass_views/src/Plugin/views/join/Ed11yActionPidCountJoin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\mass_views\Plugin\views\join;
+
+use Drupal\views\Plugin\views\join\JoinPluginBase;
+
+/**
+ * Joins ed11y_action table with pid count.
+ *
+ * @ingroup views_join_handlers
+ *
+ * @ViewsJoin("ed11y_action_pid_count_join")
+ */
+class Ed11yActionPidCountJoin extends JoinPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildJoin($select_query, $table, $view_query) {
+    $pseudoTableAlias = $this->table . '_' . $this->leftTable;
+
+    /** @var \Drupal\mysql\Driver\Database\mysql\Select $subQuery */
+    $subQuery = \Drupal::database()->select($this->table, $pseudoTableAlias);
+    $subQuery->addField($pseudoTableAlias, 'pid', $pseudoTableAlias . '_pid');
+    $subQuery->addExpression('COUNT(' . $pseudoTableAlias . '.pid)', $pseudoTableAlias . '_pid_count');
+    $subQuery->groupBy($pseudoTableAlias. '.pid');
+
+    $right_table = $subQuery;
+    $condition = $this->leftTable . '.pid = ' . $table['alias'] . '.' . $pseudoTableAlias . '_pid';
+    $arguments = [];
+
+    $select_query->addJoin($this->type, $right_table, $table['alias'], $condition, $arguments);
+  }
+
+}
