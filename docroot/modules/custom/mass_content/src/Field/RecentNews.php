86be9ceeccbd6972f7db5cf0c93e50d0aea73814
Thomas Fleming <thomas.r.fleming@mass.gov>
2026-03-20 16:14:11 -0400

    Added layout paragraphs styling for service section news

--- docroot/modules/custom/mass_content/src/Field/RecentNews.php
+++ docroot/modules/custom/mass_content/src/Field/RecentNews.php
@@ -29,21 +29,14 @@ class RecentNews extends QueryGeneratedEntityReferenceList {
     if ($node->bundle() === 'service_page') {
       $query->condition('field_related_service.target_id', $node->id());
     }
-    else {
-      $query->condition('field_news_signees.entity.field_state_org_ref_org.entity.nid', $node->id());
-    }
     $query->condition('field_news_type', 'blog_post', '<>');
     $query->condition('langcode', 'en');
     $query->condition('status', 1);
     $query->sort('field_date_published', 'DESC');
 
     // Exclude any featured items.
-    foreach (['field_org_featured_news_items', 'field_service_featured_news_items'] as $featured_field) {
-      if (!$entity->hasField($featured_field)) {
-        continue;
-      }
-
-      $field = $entity->get($featured_field);
+    if (!$entity->hasField('field_org_featured_news_items')) {
+      $field = $entity->get('field_org_featured_news_items');
       $exclude = array_column($field->getValue(), 'target_id');
       if ($exclude) {
         $query->condition('nid', $exclude, 'NOT IN');
