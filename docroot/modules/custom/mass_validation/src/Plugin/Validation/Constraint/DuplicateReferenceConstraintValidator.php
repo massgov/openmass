eab825f341113b405f31d30730e3e418544a2791
Thomas Fleming <thomas.r.fleming@mass.gov>
2026-04-08 17:52:40 -0400

    Harden calls to entity magic method and referencedEntities in execution path

--- docroot/modules/custom/mass_validation/src/Plugin/Validation/Constraint/DuplicateReferenceConstraintValidator.php
+++ docroot/modules/custom/mass_validation/src/Plugin/Validation/Constraint/DuplicateReferenceConstraintValidator.php
@@ -15,6 +15,8 @@ class DuplicateReferenceConstraintValidator extends ConstraintValidator {
    * {@inheritdoc}
    */
   public function validate($value, Constraint $constraint) {
+    $target_type = $value->getFieldDefinition()
+      ->getSetting('target_type');
     $values = array_map(function ($item) {
       return $item['target_id'];
     }, $value->filterEmptyItems()->getValue());
@@ -26,10 +28,19 @@ class DuplicateReferenceConstraintValidator extends ConstraintValidator {
       return $occurrences[$target_id] > 1;
     }));
 
+    $entities = [];
+    if ($target_type && $values) {
+      $entities = \Drupal::entityTypeManager()
+        ->getStorage($target_type)
+        ->loadMultiple(array_unique($values));
+    }
+
     foreach ($duplicate_keys as $key) {
-      $entity = $value->get($key)->entity;
+      $target_id = $values[$key] ?? NULL;
+      $entity = $target_id ? ($entities[$target_id] ?? NULL) : NULL;
+      $label = $entity ? EntityAutocomplete::getEntityLabels([$entity]) : (string) $target_id;
       $this->context->buildViolation($constraint->message)
-        ->setParameter('%label', EntityAutocomplete::getEntityLabels([$entity]))
+        ->setParameter('%label', $label)
         ->setInvalidValue($entity)
         ->atPath((string) $key . '.target_id')
         ->addViolation();
