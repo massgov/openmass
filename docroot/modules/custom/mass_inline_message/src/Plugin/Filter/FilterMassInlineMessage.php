b00c5e971c4b68fbab0edb051a49442dbedfa647
Arthur Baghdasaryan <62405127+arthurbaghdas@users.noreply.github.com>
2026-06-16 02:00:11 +0400

    DP-46945: Add message box (#3385)
    * DP-46945: Add message box component to rich text editor
    
    * DP-46945: Add message box component to rich text editor
    
    * DP-46945: Add message box component to rich text editor
    
    * DP-46945: Add message box component to rich text editor
    
    * DP-46945: Add message box component to rich text editor
    
    * DP-46945: Add message box component to rich text editor
    
    * DP-46945: Add message box component to rich text editor
    
    * DP-46945: Add message box component to rich text editor
    
    * DP-46945: Add message box component to rich text editor
    
    * Feedback
    
    * LP rendering issue fix
    
    * Message box fixes
    
    * Issue fix
    
    * Issue fix
    
    * mayflower version update
    
    * Style fix
    
    * Cleanup
    
    * Mayflower
    
    * Remove margin bottom
    
    * updated mayflower
    
    * Mayflower
    
    * Add dedicated text format
    
    * Add dedicated text format
    
    * Add dedicated text format
    
    * Fix
    
    * Fix
    
    * Add padding right to form requirements
    
    * Add overflow auto to inline message
    
    * Fix layout pargaraphs issue
    
    * Image fox
    
    * Maxlength cursor placement fix
    
    * Tests
    
    * Tests
    
    * Tests
    
    * Tests
    
    * Table buttons fix
    
    * Table buttons fix
    
    * Table buttons fix
    
    * Table buttons fix
    
    * Updated mayflower-artifacts
    
    ---------
    
    Co-authored-by: joeg8612 <joseph.galluccio@mass.gov>
    Co-authored-by: Joe Galluccio <joseph.galluccio@state.ma.us>
    Co-authored-by: Thomas Fleming <thomas.r.fleming@mass.gov>

---
+++ docroot/modules/custom/mass_inline_message/src/Plugin/Filter/FilterMassInlineMessage.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Drupal\mass_inline_message\Plugin\Filter;
+
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\filter\FilterProcessResult;
+use Drupal\filter\Plugin\FilterBase;
+use Drupal\mass_inline_message\MassInlineMessageRenderer;
+use Drupal\mass_inline_message\MessageBoxBody;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Renders mass-inline-message elements using Mayflower inline-message.
+ *
+ * @Filter(
+ *   id = "filter_mass_inline_message",
+ *   title = @Translation("Render message boxes"),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_REVERSIBLE,
+ * )
+ */
+class FilterMassInlineMessage extends FilterBase implements ContainerFactoryPluginInterface {
+
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    protected MassInlineMessageRenderer $messageRenderer,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('mass_inline_message.renderer'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process($text, $langcode) {
+    if (stripos($text, '<mass-inline-message') === FALSE) {
+      return new FilterProcessResult($text);
+    }
+
+    $pattern = '#<mass-inline-message\s+([^>]+)>(.*?)</mass-inline-message>#si';
+    $output = preg_replace_callback($pattern, function (array $matches) {
+      $attributes = $this->parseAttributes($matches[1]);
+      $title = trim($attributes['data-title'] ?? '');
+      $type = $attributes['data-type'] ?? 'info';
+      if (!in_array($type, ['info', 'warning'], TRUE)) {
+        $type = 'info';
+      }
+
+      $body_html = MessageBoxBody::normalize($matches[2]);
+      $body_filtered = $body_html !== ''
+        ? check_markup($body_html, MessageBoxBody::FORMAT_ID)
+        : '';
+      $body_for_render = MessageBoxBody::hasRenderableContent($body_filtered)
+        ? $body_filtered
+        : NULL;
+
+      // Layout Paragraphs preview renders via Ajax and may skip the global
+      // SVG placeholder processor; inline the icon SVGs here for consistency.
+      return $this->messageRenderer->renderHtml($type, $title, $body_for_render, TRUE);
+    }, $text);
+
+    return new FilterProcessResult($output ?? $text);
+  }
+
+  /**
+   * Parses HTML attributes from a tag attribute string.
+   *
+   * @param string $attribute_string
+   *   Raw attributes, e.g. 'data-title="Foo" data-type="warning"'.
+   *
+   * @return array
+   *   Associative array of attribute name => value.
+   */
+  protected function parseAttributes(string $attribute_string): array {
+    $attributes = [];
+    if (preg_match_all('/([\w-]+)\s*=\s*(?:"([^"]*)"|\'([^\']*)\'|(\S+))/', $attribute_string, $matches, PREG_SET_ORDER)) {
+      foreach ($matches as $match) {
+        $attributes[$match[1]] = $match[2] !== '' ? $match[2] : ($match[3] !== '' ? $match[3] : ($match[4] ?? ''));
+      }
+    }
+    return $attributes;
+  }
+
+}
